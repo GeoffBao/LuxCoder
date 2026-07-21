HLM1026/Proma

refactor(session): 抽取 @proma/session-core 作为会话处理唯一真源 (#988)
* refactor(session): extract @proma/session-core as single source of truth

把会话读取 / 快照去重 / 转录 / 渲染的纯逻辑从渲染层 SDKMessageRenderer 与主进程
agent-session-manager 抽到新包 @proma/session-core，并补齐渐进式读取原语
（outline / search / select / renderMarkdown + token 估算），为后续 proma CLI
提供与 Electron 共用的唯一真源。

- 渲染层与主进程改为复用 core（re-export shim），零行为变化
- groupIntoTurns 一族与 convertLegacyMessage 不再主进程/渲染层各存一份
- bun 单测 12/12：快照去重 / thinking 丢弃 / 工具折叠 ×N / legacy 归一 / search / select
- typecheck 全绿（session-core + electron）
- 真实 51MB 会话冒烟：14 turns，清洗后 ~33KB（约 1500× 压缩）

Co-Authored-By: Claude <noreply@anthropic.com>

* fix(session-core): 隔离 node:fs 到 /node 子入口，修复渲染层白屏

主 barrel re-export 了含 `import 'node:fs'` 的 read.ts，Vite 把 node:fs
externalize 后渲染层 import 即触发 "Module node:fs has been externalized"
运行时错误导致白屏。

- read.ts 改为纯函数（readSessionMessagesFromString / convertLegacyMessage），不再 import node:fs
- 文件 IO（readSessionMessages）移至 read-fs.ts，仅经子入口 @proma/session-core/node 暴露
- 主入口 '.' 全部浏览器安全；package.json exports 加 './node'

Co-Authored-By: Claude <noreply@anthropic.com>

---------

Co-authored-by: Claude <noreply@anthropic.com>

apps/electron/src/renderer/components/agent/thinking-tag-parser.ts
@@ -1,64 +1,9 @@
-import type { SDKContentBlock, SDKTextBlock } from '@proma/shared'
-
-const THINK_OPEN_TAG = '<think>'
-const THINK_CLOSE_TAG = '</think>'
-
-function appendTextBlock(blocks: SDKContentBlock[], text: string): void {
-  if (!text.trim()) return
-  blocks.push({ type: 'text', text })
-}
-
-function appendThinkingBlock(blocks: SDKContentBlock[], thinking: string): void {
-  const content = thinking.trim()
-  if (!content) return
-  blocks.push({ type: 'thinking', thinking: content })
-}
-
 /**
- * 兼容部分模型把思考内容包在 <think> 标签里的返回格式。
- * 未闭合的 <think> 在流式阶段按思考块处理，等闭合标签到达后会自然拆出后续正文。
+ * 该模块实现已迁移至 @proma/session-core（headless 核心，纯函数）。
+ * 保留此文件为 re-export shim，使既有 `from './thinking-tag-parser'` 导入方零改动。
  */
-export function parseThinkTagsFromText(text: string): SDKContentBlock[] {
-  const lowerText = text.toLowerCase()
-  const blocks: SDKContentBlock[] = []
-  let cursor = 0
-
-  while (cursor < text.length) {
-    const openIndex = lowerText.indexOf(THINK_OPEN_TAG, cursor)
-    if (openIndex === -1) {
-      appendTextBlock(blocks, text.slice(cursor))
-      break
-    }
-
-    appendTextBlock(blocks, text.slice(cursor, openIndex))
-
-    const contentStart = openIndex + THINK_OPEN_TAG.length
-    const closeIndex = lowerText.indexOf(THINK_CLOSE_TAG, contentStart)
-    if (closeIndex === -1) {
-      appendThinkingBlock(blocks, text.slice(contentStart))
-      break
-    }
-
-    appendThinkingBlock(blocks, text.slice(contentStart, closeIndex))
-    cursor = closeIndex + THINK_CLOSE_TAG.length
-  }
-
-  return blocks
-}
-
-export function splitThinkTagsInTextBlock(block: SDKTextBlock): SDKContentBlock[] {
-  if (!block.text.toLowerCase().includes(THINK_OPEN_TAG)) return [block]
-  return parseThinkTagsFromText(block.text)
-}
-
-export function normalizeThinkTagsInContentBlocks(blocks: SDKContentBlock[]): SDKContentBlock[] {
-  const normalized: SDKContentBlock[] = []
-  for (const block of blocks) {
-    if (block.type === 'text' && 'text' in block && typeof block.text === 'string') {
-      normalized.push(...splitThinkTagsInTextBlock(block as SDKTextBlock))
-    } else {
-      normalized.push(block)
-    }
-  }
-  return normalized
-}
+export {
+  normalizeThinkTagsInContentBlocks,
+  parseThinkTagsFromText,
+  splitThinkTagsInTextBlock,
+} from '@proma/session-core'

packages/session-core/src/read-fs.ts
@@ -1,0 +1,17 @@
+/**
+ * 文件 IO 层 — 仅供 Node 侧（proma CLI / Electron 主进程）使用。
+ *
+ * 本文件 import 'node:fs'，因此**不能**进入浏览器可达的主 barrel（'@proma/session-core'）。
+ * 它只通过子路径 '@proma/session-core/node' 暴露，避免 Vite 把 node:fs 打进渲染层 bundle。
+ */
+import { readFileSync } from 'node:fs'
+import type { SDKMessage } from '@proma/shared'
+import { readSessionMessagesFromString } from './read'
+
+/**
+ * 从磁盘读取一份会话 JSONL 并解析为 SDKMessage[]。
+ */
+export function readSessionMessages(filePath: string): SDKMessage[] {
+  const raw = readFileSync(filePath, 'utf-8')
+  return readSessionMessagesFromString(raw)
+}

packages/session-core/src/read.ts
@@ -1,0 +1,99 @@
+/**
+ * 读取 Proma 会话 JSONL → SDKMessage[]
+ *
+ * 逻辑迁移自 apps/electron 主进程 agent-session-manager.ts 的
+ * getAgentSessionSDKMessages / convertLegacyMessage，作为唯一真源由
+ * Electron 主进程与 proma CLI 共用。旧扁平格式（AgentMessage，带 role 字段）
+ * 在此统一归一为近似 SDKMessage，下游无需再区分「格式 A / 格式 B」。
+ */
+/**
+ * 解析会话 JSONL 文本为 SDKMessage[]（纯函数，浏览器安全，无文件 IO）。
+ *
+ * 注意：本文件刻意不 import 'node:fs'，以便被 Electron 渲染层（浏览器环境）
+ * 经主 barrel 引入。需要从磁盘读取的 readSessionMessages 在 './read-fs' 中，
+ * 仅通过 '@proma/session-core/node' 子路径暴露给 Node 侧（CLI / 主进程）。
+ */
+import type { AgentMessage, SDKMessage } from '@proma/shared'
+
+/**
+ * 将旧的 AgentMessage 转换为近似的 SDKMessage（向后兼容）。
+ * 不需要完美还原，只需在渲染/导出中可读即可。
+ */
+export function convertLegacyMessage(legacy: AgentMessage): SDKMessage {
+  if (legacy.role === 'user') {
+    return {
+      type: 'user',
+      message: {
+        content: [{ type: 'text', text: legacy.content }],
+      },
+      parent_tool_use_id: null,
+      _legacy: true,
+      _createdAt: legacy.createdAt,
+    } as unknown as SDKMessage
+  }
+
+  if (legacy.role === 'assistant') {
+    return {
+      type: 'assistant',
+      message: {
+        content: [{ type: 'text', text: legacy.content }],
+        model: legacy.model,
+      },
+      parent_tool_use_id: null,
+      _legacy: true,
+      _createdAt: legacy.createdAt,
+    } as unknown as SDKMessage
+  }
+
+  if (legacy.role === 'status') {
+    // 错误消息转换为 assistant error 格式
+    return {
+      type: 'assistant',
+      message: {
+        content: [{ type: 'text', text: legacy.content }],
+      },
+      parent_tool_use_id: null,
+      error: { message: legacy.content, errorType: legacy.errorCode },
+      _legacy: true,
+      _createdAt: legacy.createdAt,
+      _errorCode: legacy.errorCode,
+      _errorTitle: legacy.errorTitle,
+      _errorDetails: legacy.errorDetails,
+      _errorCanRetry: legacy.errorCanRetry,
+      _errorActions: legacy.errorActions,
+    } as unknown as SDKMessage
+  }
+
+  // 其他类型，作为 system 消息返回
+  return {
+    type: 'system',
+    subtype: 'init',
+    _legacy: true,
+    _createdAt: legacy.createdAt,
+  } as unknown as SDKMessage
+}
+
+/**
+ * 解析 JSONL 文本为 SDKMessage[]（纯函数，便于测试与非文件来源）。
+ * 损坏行（截断的 JSON）静默跳过，不中断整份会话解析。
+ */
+export function readSessionMessagesFromString(raw: string): SDKMessage[] {
+  const lines = raw.split('\n')
+  const messages: SDKMessage[] = []
+  for (const line of lines) {
+    if (!line.trim()) continue
+    let parsed: unknown
+    try {
+      parsed = JSON.parse(line)
+    } catch {
+      continue // 容错：跳过损坏行
+    }
+    // 旧格式检测：AgentMessage 有 `role` 字段，SDKMessage 有 `type` 字段
+    if (parsed && typeof parsed === 'object' && 'role' in parsed && !('type' in parsed)) {
+      messages.push(convertLegacyMessage(parsed as AgentMessage))
+    } else {
+      messages.push(parsed as SDKMessage)
+    }
+  }
+  return messages
+}

packages/session-core/src/thinking-tags.ts
@@ -1,0 +1,64 @@
+import type { SDKContentBlock, SDKTextBlock } from '@proma/shared'
+
+const THINK_OPEN_TAG = '<think>'
+const THINK_CLOSE_TAG = '</think>'
+
+function appendTextBlock(blocks: SDKContentBlock[], text: string): void {
+  if (!text.trim()) return
+  blocks.push({ type: 'text', text })
+}
+
+function appendThinkingBlock(blocks: SDKContentBlock[], thinking: string): void {
+  const content = thinking.trim()
+  if (!content) return
+  blocks.push({ type: 'thinking', thinking: content })
+}
+
+/**
+ * 兼容部分模型把思考内容包在 <think> 标签里的返回格式。
+ * 未闭合的 <think> 在流式阶段按思考块处理，等闭合标签到达后会自然拆出后续正文。
+ */
+export function parseThinkTagsFromText(text: string): SDKContentBlock[] {
+  const lowerText = text.toLowerCase()
+  const blocks: SDKContentBlock[] = []
+  let cursor = 0
+
+  while (cursor < text.length) {
+    const openIndex = lowerText.indexOf(THINK_OPEN_TAG, cursor)
+    if (openIndex === -1) {
+      appendTextBlock(blocks, text.slice(cursor))
+      break
+    }
+
+    appendTextBlock(blocks, text.slice(cursor, openIndex))
+
+    const contentStart = openIndex + THINK_OPEN_TAG.length
+    const closeIndex = lowerText.indexOf(THINK_CLOSE_TAG, contentStart)
+    if (closeIndex === -1) {
+      appendThinkingBlock(blocks, text.slice(contentStart))
+      break
+    }
+
+    appendThinkingBlock(blocks, text.slice(contentStart, closeIndex))
+    cursor = closeIndex + THINK_CLOSE_TAG.length
+  }
+
+  return blocks
+}
+
+export function splitThinkTagsInTextBlock(block: SDKTextBlock): SDKContentBlock[] {
+  if (!block.text.toLowerCase().includes(THINK_OPEN_TAG)) return [block]
+  return parseThinkTagsFromText(block.text)
+}
+
+export function normalizeThinkTagsInContentBlocks(blocks: SDKContentBlock[]): SDKContentBlock[] {
+  const normalized: SDKContentBlock[] = []
+  for (const block of blocks) {
+    if (block.type === 'text' && 'text' in block && typeof block.text === 'string') {
+      normalized.push(...splitThinkTagsInTextBlock(block as SDKTextBlock))
+    } else {
+      normalized.push(block)
+    }
+  }
+  return normalized
+}

packages/session-core/src/transcript.ts
@@ -1,0 +1,139 @@
+/**
+ * 转录层 — 把 MessageGroup[] 规整为带稳定下标的扁平 TranscriptTurn[]，
+ * 供 CLI 的 outline / search / select / export 复用。
+ *
+ * 关键：groupIntoTurns 会把同一回合的多行流式快照都保留在 assistantMessages 中
+ * （每行共享同一 message.id，是逐步增长的完整快照）。本层按 message.id 取最后一条
+ * （最完整）快照来消除「拼接单字 / 重复段落」，这是会话「快照去重」的落地实现。
+ */
+import type { SDKAssistantMessage, SDKContentBlock, SDKToolUseBlock } from '@proma/shared'
+import { type MessageGroup, extractUserText, getGroupPreview, stripScheduledRunMarker } from './group'
+import { estimateTokens } from './tokens'
+
+export type TurnRole = 'user' | 'assistant' | 'system'
+
+export interface TranscriptTurn {
+  /** 稳定下标：在 groupIntoTurns 输出中的 0 基位置。outline/search/export 共享。 */
+  index: number
+  role: TurnRole
+  model?: string
+  createdAt?: number
+  /** 可见正文（thinking 与 tool_result 已丢弃；assistant 多快照已去重）。 */
+  text: string
+  /** assistant 回合的工具调用摘要，已折叠连续重复为 "name args ×N"。 */
+  toolSummaries: string[]
+  /** 单行预览（<=200 字）。 */
+  preview: string
+  /** 近似 token 数（正文 + 工具摘要）。 */
+  tokens: number
+}
+
+/** 把工具 input 压缩成可读单行摘要：name key=value …（空值跳过，超 80 字符截断）。 */
+export function summarizeToolInput(name: string, input: unknown): string {
+  if (!input || typeof input !== 'object') return name
+  const parts: string[] = []
+  for (const [k, v] of Object.entries(input as Record<string, unknown>)) {
+    if (v === null || v === undefined || v === '' ) continue
+    if (Array.isArray(v) && v.length === 0) continue
+    if (typeof v === 'object' && !Array.isArray(v) && Object.keys(v as object).length === 0) continue
+    let sv = typeof v === 'string' ? v : JSON.stringify(v)
+    sv = sv.replace(/\s+/g, ' ')
+    if (sv.length > 80) sv = sv.slice(0, 77) + '...'
+    parts.push(`${k}=${sv}`)
+  }
+  return parts.length ? `${name} ${parts.join(' ')}` : name
+}
+
+/** 折叠连续相同的工具摘要为一条，末尾加 ×N。 */
+export function collapseToolSummaries(summaries: string[]): string[] {
+  const out: string[] = []
+  let i = 0
+  while (i < summaries.length) {
+    let j = i + 1
+    while (j < summaries.length && summaries[j] === summaries[i]) j++
+    const count = j - i
+    out.push(count === 1 ? summaries[i]! : `${summaries[i]} ×${count}`)
+    i = j
+  }
+  return out
+}
+
+/** 按 message.id 取每个逻辑消息的最后一条（最完整）快照，保留首次出现顺序。 */
+function dedupSnapshotsByMessageId(messages: SDKAssistantMessage[]): SDKAssistantMessage[] {
+  const lastById = new Map<string, SDKAssistantMessage>()
+  const order: string[] = []
+  let anon = 0
+  for (const m of messages) {
+    // message.id 在真实 JSONL 中存在（同一回合的多行快照共享它），但 SDK 类型未建模 → 安全读取。
+    const id = (m.message as unknown as { id?: string } | undefined)?.id ?? `__anon_${anon++}`
+    if (!lastById.has(id)) order.push(id)
+    lastById.set(id, m)
+  }
+  return order.map((id) => lastById.get(id)!)
+}
+
+/** 从去重后的 assistant 消息中抽取正文与工具摘要（thinking / tool_result 丢弃）。 */
+function buildAssistantContent(messages: SDKAssistantMessage[]): { text: string; toolSummaries: string[] } {
+  const texts: string[] = []
+  const rawToolSummaries: string[] = []
+  for (const m of dedupSnapshotsByMessageId(messages)) {
+    const blocks = m.message?.content
+    if (!Array.isArray(blocks)) continue
+    for (const block of blocks as SDKContentBlock[]) {
+      if (block.type === 'text' && 'text' in block) {
+        const t = (block as { text: string }).text.trim()
+        if (t) texts.push(t)
+      } else if (block.type === 'tool_use') {
+        const tu = block as SDKToolUseBlock
+        rawToolSummaries.push(summarizeToolInput(tu.name ?? 'tool', tu.input))
+      }
+      // thinking / 其他块：丢弃
+    }
+  }
+  return { text: texts.join('\n\n'), toolSummaries: collapseToolSummaries(rawToolSummaries) }
+}
+
+/**
+ * MessageGroup[] → TranscriptTurn[]（稳定下标）。
+ */
+export function toTranscript(groups: MessageGroup[]): TranscriptTurn[] {
+  return groups.map((group, index) => {
+    if (group.type === 'user') {
+      const text = stripScheduledRunMarker(extractUserText(group.message) ?? '')
+        .replace(/<attached_files>[\s\S]*?<\/attached_files>\n*/g, '')
+        .replace(/<quoted_file[^>]*>[\s\S]*?<\/quoted_file>\n*/g, '')
+        .trim()
+      return {
+        index,
+        role: 'user' as const,
+        createdAt: (group.message as unknown as { _createdAt?: number })._createdAt,
+        text,
+        toolSummaries: [],
+        preview: getGroupPreview(group),
+        tokens: estimateTokens(text),
+      }
+    }
+    if (group.type === 'system') {
+      const preview = getGroupPreview(group)
+      return {
+        index,
+        role: 'system' as const,
+        text: preview,
+        toolSummaries: [],
+        preview,
+        tokens: estimateTokens(preview),
+      }
+    }
+    const { text, toolSummaries } = buildAssistantContent(group.assistantMessages)
+    return {
+      index,
+      role: 'assistant' as const,
+      model: group.model,
+      createdAt: group.createdAt,
+      text,
+      toolSummaries,
+      preview: getGroupPreview(group),
+      tokens: estimateTokens(text + ' ' + toolSummaries.join(' ')),
+    }
+  })
+}

GeoffBao/LuxCoder

(cherry picked from commit 692ea0f5ab0942b924f3b016e42f6e922540d740)

apps/electron/src/renderer/components/agent/thinking-tag-parser.ts
@@ -1,64 +1,9 @@
-import type { SDKContentBlock, SDKTextBlock } from '@luxagents/shared'
-
-const THINK_OPEN_TAG = '<think>'
-const THINK_CLOSE_TAG = '</think>'
-
-function appendTextBlock(blocks: SDKContentBlock[], text: string): void {
-  if (!text.trim()) return
-  blocks.push({ type: 'text', text })
-}
-
-function appendThinkingBlock(blocks: SDKContentBlock[], thinking: string): void {
-  const content = thinking.trim()
-  if (!content) return
-  blocks.push({ type: 'thinking', thinking: content })
-}
-
 /**
- * 兼容部分模型把思考内容包在 <think> 标签里的返回格式。
- * 未闭合的 <think> 在流式阶段按思考块处理，等闭合标签到达后会自然拆出后续正文。
+ * 该模块实现已迁移至 @luxagents/session-core（headless 核心，纯函数）。
+ * 保留此文件为 re-export shim，使既有 `from './thinking-tag-parser'` 导入方零改动。
  */
-export function parseThinkTagsFromText(text: string): SDKContentBlock[] {
-  const lowerText = text.toLowerCase()
-  const blocks: SDKContentBlock[] = []
-  let cursor = 0
-
-  while (cursor < text.length) {
-    const openIndex = lowerText.indexOf(THINK_OPEN_TAG, cursor)
-    if (openIndex === -1) {
-      appendTextBlock(blocks, text.slice(cursor))
-      break
-    }
-
-    appendTextBlock(blocks, text.slice(cursor, openIndex))
-
-    const contentStart = openIndex + THINK_OPEN_TAG.length
-    const closeIndex = lowerText.indexOf(THINK_CLOSE_TAG, contentStart)
-    if (closeIndex === -1) {
-      appendThinkingBlock(blocks, text.slice(contentStart))
-      break
-    }
-
-    appendThinkingBlock(blocks, text.slice(contentStart, closeIndex))
-    cursor = closeIndex + THINK_CLOSE_TAG.length
-  }
-
-  return blocks
-}
-
-export function splitThinkTagsInTextBlock(block: SDKTextBlock): SDKContentBlock[] {
-  if (!block.text.toLowerCase().includes(THINK_OPEN_TAG)) return [block]
-  return parseThinkTagsFromText(block.text)
-}
-
-export function normalizeThinkTagsInContentBlocks(blocks: SDKContentBlock[]): SDKContentBlock[] {
-  const normalized: SDKContentBlock[] = []
-  for (const block of blocks) {
-    if (block.type === 'text' && 'text' in block && typeof block.text === 'string') {
-      normalized.push(...splitThinkTagsInTextBlock(block as SDKTextBlock))
-    } else {
-      normalized.push(block)
-    }
-  }
-  return normalized
-}
+export {
+  normalizeThinkTagsInContentBlocks,
+  parseThinkTagsFromText,
+  splitThinkTagsInTextBlock,
+} from '@luxagents/session-core'

packages/session-core/src/read-fs.ts
@@ -1,0 +1,17 @@
+/**
+ * 文件 IO 层 — 仅供 Node 侧（proma CLI / Electron 主进程）使用。
+ *
+ * 本文件 import 'node:fs'，因此**不能**进入浏览器可达的主 barrel（'@luxagents/session-core'）。
+ * 它只通过子路径 '@luxagents/session-core/node' 暴露，避免 Vite 把 node:fs 打进渲染层 bundle。
+ */
+import { readFileSync } from 'node:fs'
+import type { SDKMessage } from '@luxagents/shared'
+import { readSessionMessagesFromString } from './read'
+
+/**
+ * 从磁盘读取一份会话 JSONL 并解析为 SDKMessage[]。
+ */
+export function readSessionMessages(filePath: string): SDKMessage[] {
+  const raw = readFileSync(filePath, 'utf-8')
+  return readSessionMessagesFromString(raw)
+}

packages/session-core/src/read.ts
@@ -1,0 +1,99 @@
+/**
+ * 读取 Proma 会话 JSONL → SDKMessage[]
+ *
+ * 逻辑迁移自 apps/electron 主进程 agent-session-manager.ts 的
+ * getAgentSessionSDKMessages / convertLegacyMessage，作为唯一真源由
+ * Electron 主进程与 proma CLI 共用。旧扁平格式（AgentMessage，带 role 字段）
+ * 在此统一归一为近似 SDKMessage，下游无需再区分「格式 A / 格式 B」。
+ */
+/**
+ * 解析会话 JSONL 文本为 SDKMessage[]（纯函数，浏览器安全，无文件 IO）。
+ *
+ * 注意：本文件刻意不 import 'node:fs'，以便被 Electron 渲染层（浏览器环境）
+ * 经主 barrel 引入。需要从磁盘读取的 readSessionMessages 在 './read-fs' 中，
+ * 仅通过 '@luxagents/session-core/node' 子路径暴露给 Node 侧（CLI / 主进程）。
+ */
+import type { AgentMessage, SDKMessage } from '@luxagents/shared'
+
+/**
+ * 将旧的 AgentMessage 转换为近似的 SDKMessage（向后兼容）。
+ * 不需要完美还原，只需在渲染/导出中可读即可。
+ */
+export function convertLegacyMessage(legacy: AgentMessage): SDKMessage {
+  if (legacy.role === 'user') {
+    return {
+      type: 'user',
+      message: {
+        content: [{ type: 'text', text: legacy.content }],
+      },
+      parent_tool_use_id: null,
+      _legacy: true,
+      _createdAt: legacy.createdAt,
+    } as unknown as SDKMessage
+  }
+
+  if (legacy.role === 'assistant') {
+    return {
+      type: 'assistant',
+      message: {
+        content: [{ type: 'text', text: legacy.content }],
+        model: legacy.model,
+      },
+      parent_tool_use_id: null,
+      _legacy: true,
+      _createdAt: legacy.createdAt,
+    } as unknown as SDKMessage
+  }
+
+  if (legacy.role === 'status') {
+    // 错误消息转换为 assistant error 格式
+    return {
+      type: 'assistant',
+      message: {
+        content: [{ type: 'text', text: legacy.content }],
+      },
+      parent_tool_use_id: null,
+      error: { message: legacy.content, errorType: legacy.errorCode },
+      _legacy: true,
+      _createdAt: legacy.createdAt,
+      _errorCode: legacy.errorCode,
+      _errorTitle: legacy.errorTitle,
+      _errorDetails: legacy.errorDetails,
+      _errorCanRetry: legacy.errorCanRetry,
+      _errorActions: legacy.errorActions,
+    } as unknown as SDKMessage
+  }
+
+  // 其他类型，作为 system 消息返回
+  return {
+    type: 'system',
+    subtype: 'init',
+    _legacy: true,
+    _createdAt: legacy.createdAt,
+  } as unknown as SDKMessage
+}
+
+/**
+ * 解析 JSONL 文本为 SDKMessage[]（纯函数，便于测试与非文件来源）。
+ * 损坏行（截断的 JSON）静默跳过，不中断整份会话解析。
+ */
+export function readSessionMessagesFromString(raw: string): SDKMessage[] {
+  const lines = raw.split('\n')
+  const messages: SDKMessage[] = []
+  for (const line of lines) {
+    if (!line.trim()) continue
+    let parsed: unknown
+    try {
+      parsed = JSON.parse(line)
+    } catch {
+      continue // 容错：跳过损坏行
+    }
+    // 旧格式检测：AgentMessage 有 `role` 字段，SDKMessage 有 `type` 字段
+    if (parsed && typeof parsed === 'object' && 'role' in parsed && !('type' in parsed)) {
+      messages.push(convertLegacyMessage(parsed as AgentMessage))
+    } else {
+      messages.push(parsed as SDKMessage)
+    }
+  }
+  return messages
+}

packages/session-core/src/thinking-tags.ts
@@ -1,0 +1,64 @@
+import type { SDKContentBlock, SDKTextBlock } from '@luxagents/shared'
+
+const THINK_OPEN_TAG = '<think>'
+const THINK_CLOSE_TAG = '</think>'
+
+function appendTextBlock(blocks: SDKContentBlock[], text: string): void {
+  if (!text.trim()) return
+  blocks.push({ type: 'text', text })
+}
+
+function appendThinkingBlock(blocks: SDKContentBlock[], thinking: string): void {
+  const content = thinking.trim()
+  if (!content) return
+  blocks.push({ type: 'thinking', thinking: content })
+}
+
+/**
+ * 兼容部分模型把思考内容包在 <think> 标签里的返回格式。
+ * 未闭合的 <think> 在流式阶段按思考块处理，等闭合标签到达后会自然拆出后续正文。
+ */
+export function parseThinkTagsFromText(text: string): SDKContentBlock[] {
+  const lowerText = text.toLowerCase()
+  const blocks: SDKContentBlock[] = []
+  let cursor = 0
+
+  while (cursor < text.length) {
+    const openIndex = lowerText.indexOf(THINK_OPEN_TAG, cursor)
+    if (openIndex === -1) {
+      appendTextBlock(blocks, text.slice(cursor))
+      break
+    }
+
+    appendTextBlock(blocks, text.slice(cursor, openIndex))
+
+    const contentStart = openIndex + THINK_OPEN_TAG.length
+    const closeIndex = lowerText.indexOf(THINK_CLOSE_TAG, contentStart)
+    if (closeIndex === -1) {
+      appendThinkingBlock(blocks, text.slice(contentStart))
+      break
+    }
+
+    appendThinkingBlock(blocks, text.slice(contentStart, closeIndex))
+    cursor = closeIndex + THINK_CLOSE_TAG.length
+  }
+
+  return blocks
+}
+
+export function splitThinkTagsInTextBlock(block: SDKTextBlock): SDKContentBlock[] {
+  if (!block.text.toLowerCase().includes(THINK_OPEN_TAG)) return [block]
+  return parseThinkTagsFromText(block.text)
+}
+
+export function normalizeThinkTagsInContentBlocks(blocks: SDKContentBlock[]): SDKContentBlock[] {
+  const normalized: SDKContentBlock[] = []
+  for (const block of blocks) {
+    if (block.type === 'text' && 'text' in block && typeof block.text === 'string') {
+      normalized.push(...splitThinkTagsInTextBlock(block as SDKTextBlock))
+    } else {
+      normalized.push(block)
+    }
+  }
+  return normalized
+}

packages/session-core/src/transcript.ts
@@ -1,0 +1,139 @@
+/**
+ * 转录层 — 把 MessageGroup[] 规整为带稳定下标的扁平 TranscriptTurn[]，
+ * 供 CLI 的 outline / search / select / export 复用。
+ *
+ * 关键：groupIntoTurns 会把同一回合的多行流式快照都保留在 assistantMessages 中
+ * （每行共享同一 message.id，是逐步增长的完整快照）。本层按 message.id 取最后一条
+ * （最完整）快照来消除「拼接单字 / 重复段落」，这是会话「快照去重」的落地实现。
+ */
+import type { SDKAssistantMessage, SDKContentBlock, SDKToolUseBlock } from '@luxagents/shared'
+import { type MessageGroup, extractUserText, getGroupPreview, stripScheduledRunMarker } from './group'
+import { estimateTokens } from './tokens'
+
+export type TurnRole = 'user' | 'assistant' | 'system'
+
+export interface TranscriptTurn {
+  /** 稳定下标：在 groupIntoTurns 输出中的 0 基位置。outline/search/export 共享。 */
+  index: number
+  role: TurnRole
+  model?: string
+  createdAt?: number
+  /** 可见正文（thinking 与 tool_result 已丢弃；assistant 多快照已去重）。 */
+  text: string
+  /** assistant 回合的工具调用摘要，已折叠连续重复为 "name args ×N"。 */
+  toolSummaries: string[]
+  /** 单行预览（<=200 字）。 */
+  preview: string
+  /** 近似 token 数（正文 + 工具摘要）。 */
+  tokens: number
+}
+
+/** 把工具 input 压缩成可读单行摘要：name key=value …（空值跳过，超 80 字符截断）。 */
+export function summarizeToolInput(name: string, input: unknown): string {
+  if (!input || typeof input !== 'object') return name
+  const parts: string[] = []
+  for (const [k, v] of Object.entries(input as Record<string, unknown>)) {
+    if (v === null || v === undefined || v === '' ) continue
+    if (Array.isArray(v) && v.length === 0) continue
+    if (typeof v === 'object' && !Array.isArray(v) && Object.keys(v as object).length === 0) continue
+    let sv = typeof v === 'string' ? v : JSON.stringify(v)
+    sv = sv.replace(/\s+/g, ' ')
+    if (sv.length > 80) sv = sv.slice(0, 77) + '...'
+    parts.push(`${k}=${sv}`)
+  }
+  return parts.length ? `${name} ${parts.join(' ')}` : name
+}
+
+/** 折叠连续相同的工具摘要为一条，末尾加 ×N。 */
+export function collapseToolSummaries(summaries: string[]): string[] {
+  const out: string[] = []
+  let i = 0
+  while (i < summaries.length) {
+    let j = i + 1
+    while (j < summaries.length && summaries[j] === summaries[i]) j++
+    const count = j - i
+    out.push(count === 1 ? summaries[i]! : `${summaries[i]} ×${count}`)
+    i = j
+  }
+  return out
+}
+
+/** 按 message.id 取每个逻辑消息的最后一条（最完整）快照，保留首次出现顺序。 */
+function dedupSnapshotsByMessageId(messages: SDKAssistantMessage[]): SDKAssistantMessage[] {
+  const lastById = new Map<string, SDKAssistantMessage>()
+  const order: string[] = []
+  let anon = 0
+  for (const m of messages) {
+    // message.id 在真实 JSONL 中存在（同一回合的多行快照共享它），但 SDK 类型未建模 → 安全读取。
+    const id = (m.message as unknown as { id?: string } | undefined)?.id ?? `__anon_${anon++}`
+    if (!lastById.has(id)) order.push(id)
+    lastById.set(id, m)
+  }
+  return order.map((id) => lastById.get(id)!)
+}
+
+/** 从去重后的 assistant 消息中抽取正文与工具摘要（thinking / tool_result 丢弃）。 */
+function buildAssistantContent(messages: SDKAssistantMessage[]): { text: string; toolSummaries: string[] } {
+  const texts: string[] = []
+  const rawToolSummaries: string[] = []
+  for (const m of dedupSnapshotsByMessageId(messages)) {
+    const blocks = m.message?.content
+    if (!Array.isArray(blocks)) continue
+    for (const block of blocks as SDKContentBlock[]) {
+      if (block.type === 'text' && 'text' in block) {
+        const t = (block as { text: string }).text.trim()
+        if (t) texts.push(t)
+      } else if (block.type === 'tool_use') {
+        const tu = block as SDKToolUseBlock
+        rawToolSummaries.push(summarizeToolInput(tu.name ?? 'tool', tu.input))
+      }
+      // thinking / 其他块：丢弃
+    }
+  }
+  return { text: texts.join('\n\n'), toolSummaries: collapseToolSummaries(rawToolSummaries) }
+}
+
+/**
+ * MessageGroup[] → TranscriptTurn[]（稳定下标）。
+ */
+export function toTranscript(groups: MessageGroup[]): TranscriptTurn[] {
+  return groups.map((group, index) => {
+    if (group.type === 'user') {
+      const text = stripScheduledRunMarker(extractUserText(group.message) ?? '')
+        .replace(/<attached_files>[\s\S]*?<\/attached_files>\n*/g, '')
+        .replace(/<quoted_file[^>]*>[\s\S]*?<\/quoted_file>\n*/g, '')
+        .trim()
+      return {
+        index,
+        role: 'user' as const,
+        createdAt: (group.message as unknown as { _createdAt?: number })._createdAt,
+        text,
+        toolSummaries: [],
+        preview: getGroupPreview(group),
+        tokens: estimateTokens(text),
+      }
+    }
+    if (group.type === 'system') {
+      const preview = getGroupPreview(group)
+      return {
+        index,
+        role: 'system' as const,
+        text: preview,
+        toolSummaries: [],
+        preview,
+        tokens: estimateTokens(preview),
+      }
+    }
+    const { text, toolSummaries } = buildAssistantContent(group.assistantMessages)
+    return {
+      index,
+      role: 'assistant' as const,
+      model: group.model,
+      createdAt: group.createdAt,
+      text,
+      toolSummaries,
+      preview: getGroupPreview(group),
+      tokens: estimateTokens(text + ' ' + toolSummaries.join(' ')),
+    }
+  })
+}