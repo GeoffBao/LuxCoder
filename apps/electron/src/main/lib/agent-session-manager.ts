jacky1991/Proma

Remove agent auto approval mode (#1068)

apps/electron/src/main/lib/agent-session-manager.ts
@@ -37,6 +37,7 @@ import type {
   AgentSessionReferenceSearchInput,
   AgentSessionReferenceSearchResult,
 } from '@proma/shared'
+import { migratePermissionMode } from '@proma/shared'
 import { getConversationMessages } from './conversation-manager'
 // 旧格式 → SDKMessage 的转换逻辑下沉到 @proma/session-core 作为唯一真源，避免主进程与渲染层各存一份。
 import { convertLegacyMessage } from '@proma/session-core'
@@ -112,13 +113,33 @@ function normalizePersistedSDKMessage(parsed: unknown): SDKMessage {
   return parsed as SDKMessage
 }
 
+function migrateLegacyPermissionMode(index: AgentSessionsIndex): boolean {
+  let changed = false
+  for (const session of index.sessions) {
+    const rawMode = session.permissionMode as string | undefined
+    if (!rawMode) continue
+    const nextMode = migratePermissionMode(rawMode)
+    if (nextMode !== rawMode) {
+      session.permissionMode = nextMode
+      changed = true
+    }
+  }
+  return changed
+}
+
 /**
  * 读取会话索引文件
  */
 function readIndex(): AgentSessionsIndex {
   const indexPath = getAgentSessionsIndexPath()
   const data = readJsonFileSafe<AgentSessionsIndex>(indexPath)
-  if (data) return data
+  if (data) {
+    if (migrateLegacyPermissionMode(data)) {
+      writeIndex(data)
+      console.log('[Agent 会话] 已迁移历史权限模式 auto → bypassPermissions')
+    }
+    return data
+  }
   return { version: INDEX_VERSION, sessions: [] }
 }
 

apps/electron/src/main/lib/automation-agent-tools.ts
@@ -7,7 +7,6 @@
 
 import {
   type Automation,
-  type AutomationPermissionMode,
   type AutomationScheduleType,
   type CreateAutomationInput,
   type UpdateAutomationInput,
@@ -45,10 +44,6 @@ function validScheduleType(v: unknown): v is AutomationScheduleType {
   return v === 'interval' || v === 'daily' || v === 'weekly' || v === 'monthly' || v === 'once'
 }
 
-function validPermissionMode(v: unknown): v is AutomationPermissionMode {
-  return v === 'auto' || v === 'bypassPermissions'
-}
-
 function isFiniteInt(v: unknown): v is number {
   return typeof v === 'number' && Number.isFinite(v) && Number.isInteger(v)
 }
@@ -82,9 +77,6 @@ function validateScheduleFields(input: Partial<CreateAutomationInput | UpdateAut
   if (input.maxRuns !== undefined && (!isFiniteInt(input.maxRuns) || input.maxRuns < 1)) {
     throw new Error(`非法的 maxRuns: ${String(input.maxRuns)}（应为 ≥1 的整数）`)
   }
-  if (input.permissionMode !== undefined && !validPermissionMode(input.permissionMode)) {
-    throw new Error(`非法的 permissionMode: ${String(input.permissionMode)}`)
-  }
   if (input.sessionMode !== undefined && input.sessionMode !== 'daily' && input.sessionMode !== 'reuse') {
     throw new Error(`非法的 sessionMode: ${String(input.sessionMode)}`)
   }
@@ -104,7 +96,6 @@ function summarizeAutomation(a: Automation, includeHistory: boolean): Record<str
     maxRuns: a.maxRuns,
     runCount: a.runCount ?? 0,
     completedAt: a.completedAt,
-    permissionMode: a.permissionMode,
     sessionMode: a.sessionMode,
     workspaceId: a.workspaceId,
     sourceSessionId: a.sourceSessionId,
@@ -136,7 +127,6 @@ function getCurrentAutomationId(ctx: AutomationAgentToolContext): string | undef
 
 function buildAutomationSchemas(z: ZodModule['z']) {
   const scheduleType = z.enum(['interval', 'daily', 'weekly', 'monthly', 'once'])
-  const permissionMode = z.enum(['auto', 'bypassPermissions'])
   const sessionMode = z.enum(['daily', 'reuse'])
   return {
     list: {
@@ -157,7 +147,6 @@ function buildAutomationSchemas(z: ZodModule['z']) {
       scheduledAt: z.number().int().positive().optional().describe('一次性任务的绝对触发时间（毫秒时间戳）；scheduleType=once 时必填。用于"在某个具体时间点跑一次"，如 N 小时/天后或某个日期时刻'),
       maxRuns: z.number().int().min(1).optional().describe('最大运行次数上限（按实际执行次数计，成功+失败都算）；达到后任务自动停用。不传=不限次。可与任意 scheduleType 叠加，如 interval+maxRuns=3 表示"每隔一段时间跑，共跑 3 次就停"'),
       active: z.boolean().optional().describe('创建后是否启用，默认 true'),
-      permissionMode: permissionMode.optional().describe('无人值守权限模式，默认 bypassPermissions；高风险任务可用 auto'),
       sessionMode: sessionMode.optional().describe('会话模式：daily=同一自然日内的触发复用同一个子会话，跨日新建（默认）；reuse=始终复用同一个子会话（保留长期上下文，token 成本更高）'),
     },
     update: {
@@ -172,7 +161,6 @@ function buildAutomationSchemas(z: ZodModule['z']) {
       scheduledAt: z.number().int().positive().optional().describe('新的一次性触发时间（毫秒时间戳），scheduleType=once 时使用'),
       maxRuns: z.number().int().min(1).optional().describe('新的最大运行次数上限（按实际执行次数计）；改动会重置已执行次数计数'),
       active: z.boolean().optional().describe('启用或暂停任务'),
-      permissionMode: permissionMode.optional().describe('新的无人值守权限模式'),
       sessionMode: sessionMode.optional().describe('新的会话模式：daily=同一自然日内复用，跨日新建；reuse=始终复用同一个子会话'),
     },
     delete: {
@@ -242,7 +230,6 @@ export async function injectAutomationMcpServer(
             channelId: ctx.channelId,
             modelId: ctx.modelId,
             workspaceId: ctx.workspaceId,
-            permissionMode: args.permissionMode,
             sessionMode: args.sessionMode,
             sourceSessionId: ctx.sessionId,
             active: args.active ?? true,
@@ -270,7 +257,7 @@ export async function injectAutomationMcpServer(
       ),
       sdk.tool(
         'update_automation',
-        '修改 Proma 定时任务，包括名称、执行提示词、频率、启用状态和权限模式。定时任务自动执行中可以省略 id 来修改当前任务。',
+        '修改 Proma 定时任务，包括名称、执行提示词、频率和启用状态。定时任务自动执行中可以省略 id 来修改当前任务。',
         schemas.update,
         async (args) => {
           const id = args.id?.trim() || getCurrentAutomationId(ctx)
@@ -287,7 +274,6 @@ export async function injectAutomationMcpServer(
             scheduledAt: args.scheduledAt,
             maxRuns: args.maxRuns,
             active: args.active,
-            permissionMode: args.permissionMode,
             sessionMode: args.sessionMode,
           }
           if (input.name !== undefined) assertNonBlank(input.name, 'name')

GeoffBao/LuxCoder

(cherry picked from commit aa99c2472ff792da6e3a448adbce2ff71c5c9433)

apps/electron/src/main/lib/agent-session-manager.ts
@@ -37,6 +37,7 @@ import type {
   AgentSessionReferenceSearchInput,
   AgentSessionReferenceSearchResult,
 } from '@luxagents/shared'
+import { migratePermissionMode } from '@luxagents/shared'
 import { getConversationMessages } from './conversation-manager'
 // 旧格式 → SDKMessage 的转换逻辑下沉到 @luxagents/session-core 作为唯一真源，避免主进程与渲染层各存一份。
 import { convertLegacyMessage } from '@luxagents/session-core'
@@ -112,13 +113,33 @@ function normalizePersistedSDKMessage(parsed: unknown): SDKMessage {
   return parsed as SDKMessage
 }
 
+function migrateLegacyPermissionMode(index: AgentSessionsIndex): boolean {
+  let changed = false
+  for (const session of index.sessions) {
+    const rawMode = session.permissionMode as string | undefined
+    if (!rawMode) continue
+    const nextMode = migratePermissionMode(rawMode)
+    if (nextMode !== rawMode) {
+      session.permissionMode = nextMode
+      changed = true
+    }
+  }
+  return changed
+}
+
 /**
  * 读取会话索引文件
  */
 function readIndex(): AgentSessionsIndex {
   const indexPath = getAgentSessionsIndexPath()
   const data = readJsonFileSafe<AgentSessionsIndex>(indexPath)
-  if (data) return data
+  if (data) {
+    if (migrateLegacyPermissionMode(data)) {
+      writeIndex(data)
+      console.log('[Agent 会话] 已迁移历史权限模式 auto → bypassPermissions')
+    }
+    return data
+  }
   return { version: INDEX_VERSION, sessions: [] }
 }
 

apps/electron/src/main/lib/automation-agent-tools.ts
@@ -7,7 +7,6 @@
 
 import {
   type Automation,
-  type AutomationPermissionMode,
   type AutomationScheduleType,
   type CreateAutomationInput,
   type UpdateAutomationInput,
@@ -45,10 +44,6 @@ function validScheduleType(v: unknown): v is AutomationScheduleType {
   return v === 'interval' || v === 'daily' || v === 'weekly' || v === 'monthly' || v === 'once'
 }
 
-function validPermissionMode(v: unknown): v is AutomationPermissionMode {
-  return v === 'auto' || v === 'bypassPermissions'
-}
-
 function isFiniteInt(v: unknown): v is number {
   return typeof v === 'number' && Number.isFinite(v) && Number.isInteger(v)
 }
@@ -82,9 +77,6 @@ function validateScheduleFields(input: Partial<CreateAutomationInput | UpdateAut
   if (input.maxRuns !== undefined && (!isFiniteInt(input.maxRuns) || input.maxRuns < 1)) {
     throw new Error(`非法的 maxRuns: ${String(input.maxRuns)}（应为 ≥1 的整数）`)
   }
-  if (input.permissionMode !== undefined && !validPermissionMode(input.permissionMode)) {
-    throw new Error(`非法的 permissionMode: ${String(input.permissionMode)}`)
-  }
   if (input.sessionMode !== undefined && input.sessionMode !== 'daily' && input.sessionMode !== 'reuse') {
     throw new Error(`非法的 sessionMode: ${String(input.sessionMode)}`)
   }
@@ -104,7 +96,6 @@ function summarizeAutomation(a: Automation, includeHistory: boolean): Record<str
     maxRuns: a.maxRuns,
     runCount: a.runCount ?? 0,
     completedAt: a.completedAt,
-    permissionMode: a.permissionMode,
     sessionMode: a.sessionMode,
     workspaceId: a.workspaceId,
     sourceSessionId: a.sourceSessionId,
@@ -136,7 +127,6 @@ function getCurrentAutomationId(ctx: AutomationAgentToolContext): string | undef
 
 function buildAutomationSchemas(z: ZodModule['z']) {
   const scheduleType = z.enum(['interval', 'daily', 'weekly', 'monthly', 'once'])
-  const permissionMode = z.enum(['auto', 'bypassPermissions'])
   const sessionMode = z.enum(['daily', 'reuse'])
   return {
     list: {
@@ -157,7 +147,6 @@ function buildAutomationSchemas(z: ZodModule['z']) {
       scheduledAt: z.number().int().positive().optional().describe('一次性任务的绝对触发时间（毫秒时间戳）；scheduleType=once 时必填。用于"在某个具体时间点跑一次"，如 N 小时/天后或某个日期时刻'),
       maxRuns: z.number().int().min(1).optional().describe('最大运行次数上限（按实际执行次数计，成功+失败都算）；达到后任务自动停用。不传=不限次。可与任意 scheduleType 叠加，如 interval+maxRuns=3 表示"每隔一段时间跑，共跑 3 次就停"'),
       active: z.boolean().optional().describe('创建后是否启用，默认 true'),
-      permissionMode: permissionMode.optional().describe('无人值守权限模式，默认 bypassPermissions；高风险任务可用 auto'),
       sessionMode: sessionMode.optional().describe('会话模式：daily=同一自然日内的触发复用同一个子会话，跨日新建（默认）；reuse=始终复用同一个子会话（保留长期上下文，token 成本更高）'),
     },
     update: {
@@ -172,7 +161,6 @@ function buildAutomationSchemas(z: ZodModule['z']) {
       scheduledAt: z.number().int().positive().optional().describe('新的一次性触发时间（毫秒时间戳），scheduleType=once 时使用'),
       maxRuns: z.number().int().min(1).optional().describe('新的最大运行次数上限（按实际执行次数计）；改动会重置已执行次数计数'),
       active: z.boolean().optional().describe('启用或暂停任务'),
-      permissionMode: permissionMode.optional().describe('新的无人值守权限模式'),
       sessionMode: sessionMode.optional().describe('新的会话模式：daily=同一自然日内复用，跨日新建；reuse=始终复用同一个子会话'),
     },
     delete: {
@@ -242,7 +230,6 @@ export async function injectAutomationMcpServer(
             channelId: ctx.channelId,
             modelId: ctx.modelId,
             workspaceId: ctx.workspaceId,
-            permissionMode: args.permissionMode,
             sessionMode: args.sessionMode,
             sourceSessionId: ctx.sessionId,
             active: args.active ?? true,
@@ -270,7 +257,7 @@ export async function injectAutomationMcpServer(
       ),
       sdk.tool(
         'update_automation',
-        '修改 LuxAgents 定时任务，包括名称、执行提示词、频率、启用状态和权限模式。定时任务自动执行中可以省略 id 来修改当前任务。',
+        '修改 LuxAgents 定时任务，包括名称、执行提示词、频率和启用状态。定时任务自动执行中可以省略 id 来修改当前任务。',
         schemas.update,
         async (args) => {
           const id = args.id?.trim() || getCurrentAutomationId(ctx)
@@ -287,7 +274,6 @@ export async function injectAutomationMcpServer(
             scheduledAt: args.scheduledAt,
             maxRuns: args.maxRuns,
             active: args.active,
-            permissionMode: args.permissionMode,
             sessionMode: args.sessionMode,
           }
           if (input.name !== undefined) assertNonBlank(input.name, 'name')