wafo210715/Proma

refactor(channel): deduplicate zhipu credential parsing and unify plan quota cache
- Extract parseZhipuTeamCredentials to @proma/shared, removing duplicate
  implementations from channel-manager.ts and ChannelForm.tsx
- Unify plan quota cache into channel-plan-quota.ts shared module with
  in-flight request deduplication, used by both ContextUsageBadge and
  ChannelPlanQuotaBadge
- Fix ChannelPlanQuotaBadge useEffect dep to use stable keys (channel.id)
  instead of the channel object reference
- Fix indentation in testZhipuCodingTeam
- Guard inferProviderFromBaseUrl with try/catch in getChannelPlanQuota

Co-Authored-By: Claude <noreply@anthropic.com>

apps/electron/src/main/lib/channel-manager.ts
@@ -24,7 +24,7 @@ import type {
   FetchModelsResult,
   ProviderType,
 } from '@proma/shared'
-import { extractZhipuCodingTeamApiToken, PROVIDER_DEFAULT_URLS } from '@proma/shared'
+import { extractZhipuCodingTeamApiToken, parseZhipuTeamCredentials, PROVIDER_DEFAULT_URLS } from '@proma/shared'
 import { getFetchFn } from './proxy-fetch'
 import { getEffectiveProxyUrl } from './proxy-settings-service'
 import {
@@ -994,12 +994,6 @@ interface ZhipuQuotaResponse {
   }
 }
 
-interface ZhipuTeamQuotaCredentials {
-  apiKey: string
-  organization?: string
-  project?: string
-}
-
 function createZhipuQuotaUrl(baseUrl: string, query?: Record<string, string>): string {
   let requestUrl = 'https://bigmodel.cn/api/monitor/usage/quota/limit'
   try {
@@ -1034,53 +1028,6 @@ function createZhipuTeamQuotaUrl(): string {
   return url.toString()
 }
 
-function normalizeZhipuCredentialKey(key: string): string {
-  return key.trim().toLowerCase().replace(/[_-]/g, '')
-}
-
-function parseZhipuTeamQuotaCredentials(apiKey: string): ZhipuTeamQuotaCredentials | null {
-  const trimmed = apiKey.trim()
-  if (!trimmed) return null
-
-  const pick = (record: Record<string, unknown>): ZhipuTeamQuotaCredentials | null => {
-    const normalized = new Map<string, string>()
-    for (const [key, value] of Object.entries(record)) {
-      if (typeof value === 'string' && value.trim()) {
-        normalized.set(normalizeZhipuCredentialKey(key), value.trim())
-      }
-    }
-    const apiKey = normalized.get('apikey')
-      ?? normalized.get('apitoken')
-      ?? normalized.get('token')
-      ?? normalized.get('authorization')
-      ?? normalized.get('auth')
-      ?? normalized.get('bearer')
-    const organization = normalized.get('bigmodelorganization') ?? normalized.get('organization') ?? normalized.get('org')
-    const project = normalized.get('bigmodelproject') ?? normalized.get('project')
-    if (!apiKey) return null
-    return { apiKey, organization, project }
-  }
-
-  if (trimmed.startsWith('{')) {
-    try {
-      const parsed = JSON.parse(trimmed) as unknown
-      if (parsed && typeof parsed === 'object' && !Array.isArray(parsed)) {
-        return pick(parsed as Record<string, unknown>)
-      }
-    } catch {
-      return null
-    }
-  }
-
-  const entries: Record<string, string> = {}
-  for (const part of trimmed.split(/[;\n]+/)) {
-    const index = part.indexOf('=')
-    if (index <= 0) continue
-    entries[part.slice(0, index).trim()] = part.slice(index + 1).trim()
-  }
-  return pick(entries)
-}
-
 async function fetchZhipuQuota(
   apiKey: string,
   requestUrl: string,
@@ -1108,7 +1055,7 @@ async function fetchZhipuQuota(
 }
 
 async function fetchZhipuTeamQuota(
-  credentials: ZhipuTeamQuotaCredentials,
+  credentials: import('@proma/shared').ZhipuTeamCredentials,
   proxyUrl?: string,
 ): Promise<ZhipuQuotaResponse | { error: string }> {
   const fetchFn = getFetchFn(proxyUrl)
@@ -1148,7 +1095,7 @@ async function testZhipuCodingTeam(
   baseUrl: string,
   proxyUrl?: string,
 ): Promise<ChannelTestResult> {
-  const teamCredentials = parseZhipuTeamQuotaCredentials(apiKey)
+  const teamCredentials = parseZhipuTeamCredentials(apiKey)
   if (teamCredentials) {
     const response = await fetchZhipuTeamQuota(teamCredentials, proxyUrl)
     if ('error' in response) {
@@ -1166,10 +1113,10 @@ async function testZhipuCodingTeam(
 
   const authToken = extractZhipuCodingTeamApiToken(apiKey)
   if (!authToken) {
-      return {
-        success: false,
-        message: '请填写 API Token；组织 ID 和项目 ID 可选',
-      }
+    return {
+      success: false,
+      message: '请填写 API Token；组织 ID 和项目 ID 可选',
+    }
   }
 
   return await testAnthropicCompatible(baseUrl, apiKey, proxyUrl, 'zhipu-coding-team')
@@ -1254,7 +1201,7 @@ async function queryZhipuPlanQuota(
   provider: ProviderType = 'zhipu-coding',
 ): Promise<ChannelPlanQuotaResult> {
   if (provider === 'zhipu-coding-team') {
-    const teamCredentials = parseZhipuTeamQuotaCredentials(apiKey)
+    const teamCredentials = parseZhipuTeamCredentials(apiKey)
     if (!teamCredentials) {
       return createUnsupportedPlanQuota(
         'zhipu-coding-team',
@@ -1299,7 +1246,12 @@ export async function getChannelPlanQuota(channelId: string): Promise<ChannelPla
     return createUnsupportedPlanQuota('custom', '渠道不存在')
   }
 
-  const provider = inferProviderFromBaseUrl(channel.provider, channel.baseUrl)
+  let provider: ProviderType
+  try {
+    provider = inferProviderFromBaseUrl(channel.provider, channel.baseUrl)
+  } catch {
+    provider = channel.provider
+  }
   const proxyUrl = await getEffectiveProxyUrl()
   let apiKey: string
   try {

apps/electron/src/renderer/lib/channel-plan-quota.ts
@@ -1,4 +1,4 @@
-import type { Channel, ProviderType } from '@proma/shared'
+import type { Channel, ChannelPlanQuotaResult, ProviderType } from '@proma/shared'
 
 const PLAN_QUOTA_PROVIDERS = new Set<ProviderType>([
   'deepseek',
@@ -13,4 +13,52 @@ export function supportsChannelPlanQuota(channel: Pick<Channel, 'provider' | 'ba
   if (!channel) return false
   if (PLAN_QUOTA_PROVIDERS.has(channel.provider)) return true
   return channel.baseUrl.includes('api.kimi.com/coding')
+}
+
+const PLAN_QUOTA_CACHE_MS = 60 * 1000
+const PLAN_QUOTA_ERROR_CACHE_MS = 15 * 1000
+
+const quotaCache = new Map<string, ChannelPlanQuotaResult>()
+const inflightRequests = new Map<string, Promise<ChannelPlanQuotaResult>>()
+
+function getCacheTtl(result: ChannelPlanQuotaResult): number {
+  return result.supported ? PLAN_QUOTA_CACHE_MS : PLAN_QUOTA_ERROR_CACHE_MS
+}
+
+export function getCachedPlanQuota(channelId: string): ChannelPlanQuotaResult | null {
+  const cached = quotaCache.get(channelId)
+  if (!cached) return null
+  if (Date.now() - cached.updatedAt >= getCacheTtl(cached)) return null
+  return cached
+}
+
+export async function fetchChannelPlanQuota(channelId: string): Promise<ChannelPlanQuotaResult> {
+  const cached = getCachedPlanQuota(channelId)
+  if (cached) return cached
+
+  const inflight = inflightRequests.get(channelId)
+  if (inflight) return inflight
+
+  const request = window.electronAPI.getChannelPlanQuota(channelId)
+    .then((result) => {
+      quotaCache.set(channelId, result)
+      return result
+    })
+    .catch((error: unknown) => {
+      const result: ChannelPlanQuotaResult = {
+        supported: false,
+        provider: 'custom',
+        windows: [],
+        updatedAt: Date.now(),
+        message: error instanceof Error ? error.message : '订阅额度查询失败',
+      }
+      quotaCache.set(channelId, result)
+      return result
+    })
+    .finally(() => {
+      inflightRequests.delete(channelId)
+    })
+
+  inflightRequests.set(channelId, request)
+  return request
 }

GeoffBao/LuxCoder

(cherry picked from commit 78ed46b753ff426a789ec090733fc991fdffc739)
Co-authored-by: Cursor <cursoragent@cursor.com>

apps/electron/src/main/lib/channel-manager.ts
@@ -24,7 +24,7 @@ import type {
   FetchModelsResult,
   ProviderType,
 } from '@luxagents/shared'
-import { extractZhipuCodingTeamApiToken, PROVIDER_DEFAULT_URLS } from '@luxagents/shared'
+import { extractZhipuCodingTeamApiToken, parseZhipuTeamCredentials, PROVIDER_DEFAULT_URLS } from '@luxagents/shared'
 import { getFetchFn } from './proxy-fetch'
 import { getEffectiveProxyUrl } from './proxy-settings-service'
 import {
@@ -994,12 +994,6 @@ interface ZhipuQuotaResponse {
   }
 }
 
-interface ZhipuTeamQuotaCredentials {
-  apiKey: string
-  organization?: string
-  project?: string
-}
-
 function createZhipuQuotaUrl(baseUrl: string, query?: Record<string, string>): string {
   let requestUrl = 'https://bigmodel.cn/api/monitor/usage/quota/limit'
   try {
@@ -1034,53 +1028,6 @@ function createZhipuTeamQuotaUrl(): string {
   return url.toString()
 }
 
-function normalizeZhipuCredentialKey(key: string): string {
-  return key.trim().toLowerCase().replace(/[_-]/g, '')
-}
-
-function parseZhipuTeamQuotaCredentials(apiKey: string): ZhipuTeamQuotaCredentials | null {
-  const trimmed = apiKey.trim()
-  if (!trimmed) return null
-
-  const pick = (record: Record<string, unknown>): ZhipuTeamQuotaCredentials | null => {
-    const normalized = new Map<string, string>()
-    for (const [key, value] of Object.entries(record)) {
-      if (typeof value === 'string' && value.trim()) {
-        normalized.set(normalizeZhipuCredentialKey(key), value.trim())
-      }
-    }
-    const apiKey = normalized.get('apikey')
-      ?? normalized.get('apitoken')
-      ?? normalized.get('token')
-      ?? normalized.get('authorization')
-      ?? normalized.get('auth')
-      ?? normalized.get('bearer')
-    const organization = normalized.get('bigmodelorganization') ?? normalized.get('organization') ?? normalized.get('org')
-    const project = normalized.get('bigmodelproject') ?? normalized.get('project')
-    if (!apiKey) return null
-    return { apiKey, organization, project }
-  }
-
-  if (trimmed.startsWith('{')) {
-    try {
-      const parsed = JSON.parse(trimmed) as unknown
-      if (parsed && typeof parsed === 'object' && !Array.isArray(parsed)) {
-        return pick(parsed as Record<string, unknown>)
-      }
-    } catch {
-      return null
-    }
-  }
-
-  const entries: Record<string, string> = {}
-  for (const part of trimmed.split(/[;\n]+/)) {
-    const index = part.indexOf('=')
-    if (index <= 0) continue
-    entries[part.slice(0, index).trim()] = part.slice(index + 1).trim()
-  }
-  return pick(entries)
-}
-
 async function fetchZhipuQuota(
   apiKey: string,
   requestUrl: string,
@@ -1108,7 +1055,7 @@ async function fetchZhipuQuota(
 }
 
 async function fetchZhipuTeamQuota(
-  credentials: ZhipuTeamQuotaCredentials,
+  credentials: import('@luxagents/shared').ZhipuTeamCredentials,
   proxyUrl?: string,
 ): Promise<ZhipuQuotaResponse | { error: string }> {
   const fetchFn = getFetchFn(proxyUrl)
@@ -1148,7 +1095,7 @@ async function testZhipuCodingTeam(
   baseUrl: string,
   proxyUrl?: string,
 ): Promise<ChannelTestResult> {
-  const teamCredentials = parseZhipuTeamQuotaCredentials(apiKey)
+  const teamCredentials = parseZhipuTeamCredentials(apiKey)
   if (teamCredentials) {
     const response = await fetchZhipuTeamQuota(teamCredentials, proxyUrl)
     if ('error' in response) {
@@ -1166,10 +1113,10 @@ async function testZhipuCodingTeam(
 
   const authToken = extractZhipuCodingTeamApiToken(apiKey)
   if (!authToken) {
-      return {
-        success: false,
-        message: '请填写 API Token；组织 ID 和项目 ID 可选',
-      }
+    return {
+      success: false,
+      message: '请填写 API Token；组织 ID 和项目 ID 可选',
+    }
   }
 
   return await testAnthropicCompatible(baseUrl, apiKey, proxyUrl, 'zhipu-coding-team')
@@ -1254,7 +1201,7 @@ async function queryZhipuPlanQuota(
   provider: ProviderType = 'zhipu-coding',
 ): Promise<ChannelPlanQuotaResult> {
   if (provider === 'zhipu-coding-team') {
-    const teamCredentials = parseZhipuTeamQuotaCredentials(apiKey)
+    const teamCredentials = parseZhipuTeamCredentials(apiKey)
     if (!teamCredentials) {
       return createUnsupportedPlanQuota(
         'zhipu-coding-team',
@@ -1299,7 +1246,12 @@ export async function getChannelPlanQuota(channelId: string): Promise<ChannelPla
     return createUnsupportedPlanQuota('custom', '渠道不存在')
   }
 
-  const provider = inferProviderFromBaseUrl(channel.provider, channel.baseUrl)
+  let provider: ProviderType
+  try {
+    provider = inferProviderFromBaseUrl(channel.provider, channel.baseUrl)
+  } catch {
+    provider = channel.provider
+  }
   const proxyUrl = await getEffectiveProxyUrl()
   let apiKey: string
   try {

apps/electron/src/renderer/lib/channel-plan-quota.ts
@@ -1,4 +1,4 @@
-import type { Channel, ProviderType } from '@luxagents/shared'
+import type { Channel, ChannelPlanQuotaResult, ProviderType } from '@luxagents/shared'
 
 const PLAN_QUOTA_PROVIDERS = new Set<ProviderType>([
   'deepseek',
@@ -13,4 +13,52 @@ export function supportsChannelPlanQuota(channel: Pick<Channel, 'provider' | 'ba
   if (!channel) return false
   if (PLAN_QUOTA_PROVIDERS.has(channel.provider)) return true
   return channel.baseUrl.includes('api.kimi.com/coding')
+}
+
+const PLAN_QUOTA_CACHE_MS = 60 * 1000
+const PLAN_QUOTA_ERROR_CACHE_MS = 15 * 1000
+
+const quotaCache = new Map<string, ChannelPlanQuotaResult>()
+const inflightRequests = new Map<string, Promise<ChannelPlanQuotaResult>>()
+
+function getCacheTtl(result: ChannelPlanQuotaResult): number {
+  return result.supported ? PLAN_QUOTA_CACHE_MS : PLAN_QUOTA_ERROR_CACHE_MS
+}
+
+export function getCachedPlanQuota(channelId: string): ChannelPlanQuotaResult | null {
+  const cached = quotaCache.get(channelId)
+  if (!cached) return null
+  if (Date.now() - cached.updatedAt >= getCacheTtl(cached)) return null
+  return cached
+}
+
+export async function fetchChannelPlanQuota(channelId: string): Promise<ChannelPlanQuotaResult> {
+  const cached = getCachedPlanQuota(channelId)
+  if (cached) return cached
+
+  const inflight = inflightRequests.get(channelId)
+  if (inflight) return inflight
+
+  const request = window.electronAPI.getChannelPlanQuota(channelId)
+    .then((result) => {
+      quotaCache.set(channelId, result)
+      return result
+    })
+    .catch((error: unknown) => {
+      const result: ChannelPlanQuotaResult = {
+        supported: false,
+        provider: 'custom',
+        windows: [],
+        updatedAt: Date.now(),
+        message: error instanceof Error ? error.message : '订阅额度查询失败',
+      }
+      quotaCache.set(channelId, result)
+      return result
+    })
+    .finally(() => {
+      inflightRequests.delete(channelId)
+    })
+
+  inflightRequests.set(channelId, request)
+  return request
 }