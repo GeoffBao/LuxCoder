jacky1991/Proma

[codex] Fix compatible provider request URLs (#982)
* fix compatible provider request URLs

* fix(channel): 为 custom/anthropic-compatible 渠道补齐 v1→v2 配置迁移

PR #982 改变了 custom / anthropic-compatible 两类通用兼容渠道的 baseUrl
语义（从 Base URL 自动补端点 → 完整请求地址原样使用），但缺少对存量渠道
的迁移，已配置过的用户升级后会向缺少端点后缀的地址发请求而失败。

- 新增 migrateCompatibleChannelBaseUrl，把存量 baseUrl 补全为旧版本实际
  请求过的完整 URL（custom→/chat/completions，anthropic-compatible→/v1/messages），
  保证升级后运行时行为不变；带幂等守卫（空值/已是完整端点/其他 provider 跳过）
- channel-manager CONFIG_VERSION 升至 2，readConfig 首次读到旧版本时自动
  迁移并回写 channels.json
- 补 resolve* 函数与迁移函数的单元测试；修正 PR 遗留的过时测试断言
  （anthropic-compatible 已不再走 normalizeAnthropicProviderUrl 的 versioned 分支）

Co-Authored-By: Claude <noreply@anthropic.com>

---------

Co-authored-by: Claude <noreply@anthropic.com>

apps/electron/src/main/lib/channel-manager.ts
@@ -24,12 +24,20 @@ import type {
 import { PROVIDER_DEFAULT_URLS } from '@proma/shared'
 import { getFetchFn } from './proxy-fetch'
 import { getEffectiveProxyUrl } from './proxy-settings-service'
-import { normalizeBaseUrl, normalizeAnthropicProviderUrl, getPromaUserAgent } from '@proma/core'
+import {
+  getPromaUserAgent,
+  migrateCompatibleChannelBaseUrl,
+  normalizeBaseUrl,
+  resolveAnthropicMessagesUrl,
+  resolveAnthropicModelsUrl,
+  resolveOpenAIChatCompletionsUrl,
+  resolveOpenAIModelsUrl,
+} from '@proma/core'
 import { normalizeHttpResponse, normalizeRequestError } from './channel-test-error'
 import pkg from '../../../package.json' with { type: 'json' }
 
 /** 当前配置版本 */
-const CONFIG_VERSION = 1
+const CONFIG_VERSION = 2
 /** 连接测试 / 模型拉取的统一超时时间 */
 const CHANNEL_TEST_TIMEOUT_MS = 15_000
 
@@ -42,7 +50,43 @@ function withTimeout(init: RequestInit): RequestInit {
 }
 
 /**
+ * 将渠道配置迁移到最新版本。
+ *
+ * v1 → v2：custom / anthropic-compatible 两类通用兼容渠道的 baseUrl 语义从「Base URL（运行时
+ * 自动补端点后缀）」改为「完整请求地址（原样使用）」。把存量 baseUrl 一次性补全为旧版本实际
+ * 请求过的完整端点，使升级后的运行时行为与升级前保持一致。详见 migrateCompatibleChannelBaseUrl。
+ *
+ * @returns 迁移后的配置；`changed` 标记是否发生实际变更（决定是否需要回写文件）
+ */
+function migrateConfig(config: ChannelsConfig): { config: ChannelsConfig; changed: boolean } {
+  const version = config.version ?? 1
+  if (version >= CONFIG_VERSION) {
+    return { config, changed: false }
+  }
+
+  let mutated = false
+  const channels = config.channels.map((channel) => {
+    if (channel.provider !== 'custom' && channel.provider !== 'anthropic-compatible') {
+      return channel
+    }
+    const migratedUrl = migrateCompatibleChannelBaseUrl(channel.baseUrl, channel.provider)
+    if (migratedUrl === channel.baseUrl) {
+      return channel
+    }
+    mutated = true
+    console.log(
+      `[渠道管理] v${version}→v${CONFIG_VERSION} 迁移渠道 ${channel.name} (${channel.provider}) Base URL: ${channel.baseUrl} → ${migratedUrl}`,
+    )
+    return { ...channel, baseUrl: migratedUrl }
+  })
+
+  return { config: { version: CONFIG_VERSION, channels }, changed: true }
+}
+
+/**
  * 读取渠道配置文件
+ *
+ * 读取时自动将旧版本配置迁移到 CONFIG_VERSION，并在发生变更时回写。
  */
 function readConfig(): ChannelsConfig {
   const configPath = getChannelsPath()
@@ -53,7 +97,13 @@ function readConfig(): ChannelsConfig {
 
   try {
     const raw = readFileSync(configPath, 'utf-8')
-    return JSON.parse(raw) as ChannelsConfig
+    const parsed = JSON.parse(raw) as ChannelsConfig
+    const { config, changed } = migrateConfig(parsed)
+    if (changed) {
+      writeConfig(config)
+      console.log('[渠道管理] 渠道配置已迁移并持久化')
+    }
+    return config
   } catch (error) {
     console.error('[渠道管理] 读取配置文件失败:', error)
     return { version: CONFIG_VERSION, channels: [] }
@@ -294,7 +344,7 @@ export async function testChannel(channelId: string): Promise<ChannelTestResult>
       case 'doubao':
       case 'qwen':
       case 'custom':
-        return await testOpenAICompatible(channel.baseUrl, apiKey, proxyUrl)
+        return await testOpenAICompatible(channel.baseUrl, apiKey, proxyUrl, channel.provider)
       case 'google':
         return await testGoogle(channel.baseUrl, apiKey, proxyUrl)
       default:
@@ -308,8 +358,8 @@ export async function testChannel(channelId: string): Promise<ChannelTestResult>
 /**
  * 测试 Anthropic 兼容 API 连接（Anthropic / DeepSeek / Kimi API / Kimi Coding Plan / MiniMax）
  *
- * DeepSeek / Kimi 等以 /anthropic 为协议根路径的供应商，实际端点位于 /anthropic/v1/messages，
- * 由 normalizeAnthropicProviderUrl 统一按需补 /v1。
+ * DeepSeek / Kimi 等内置供应商会按协议根路径补全端点。
+ * Anthropic 兼容格式使用用户填写的完整请求地址。
  * Kimi Coding Plan 必须发送 Proma User-Agent，否则返回 403。
  */
 async function testAnthropicCompatible(
@@ -318,7 +368,7 @@ async function testAnthropicCompatible(
   proxyUrl?: string,
   provider: ProviderType = 'anthropic',
 ): Promise<ChannelTestResult> {
-  const url = normalizeAnthropicProviderUrl(baseUrl, provider)
+  const url = resolveAnthropicMessagesUrl(baseUrl, provider)
   const fetchFn = getFetchFn(proxyUrl)
 
   let testModel: string
@@ -366,7 +416,7 @@ async function testAnthropicCompatible(
     headers.Authorization = `Bearer ${apiKey}`
   }
 
-  const response = await fetchFn(`${url}/messages`, withTimeout({
+  const response = await fetchFn(url, withTimeout({
     method: 'POST',
     headers,
     body: JSON.stringify({
@@ -382,11 +432,34 @@ async function testAnthropicCompatible(
 /**
  * 测试 OpenAI 兼容 API 连接（OpenAI / Custom）
  */
-async function testOpenAICompatible(baseUrl: string, apiKey: string, proxyUrl?: string): Promise<ChannelTestResult> {
-  const url = normalizeBaseUrl(baseUrl)
+async function testOpenAICompatible(
+  baseUrl: string,
+  apiKey: string,
+  proxyUrl?: string,
+  provider: ProviderType = 'openai',
+): Promise<ChannelTestResult> {
+  if (provider === 'custom') {
+    const url = resolveOpenAIChatCompletionsUrl(baseUrl, provider)
+    const fetchFn = getFetchFn(proxyUrl)
+    const response = await fetchFn(url, withTimeout({
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${apiKey}`,
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        model: 'gpt-4o-mini',
+        messages: [{ role: 'user', content: 'hi' }],
+        max_tokens: 1,
+      }),
+    }))
+    return normalizeHttpResponse(response)
+  }
+
+  const url = resolveOpenAIModelsUrl(baseUrl)
   const fetchFn = getFetchFn(proxyUrl)
 
-  const response = await fetchFn(`${url}/models`, withTimeout({
+  const response = await fetchFn(url, withTimeout({
     method: 'GET',
     headers: {
       Authorization: `Bearer ${apiKey}`,
@@ -439,7 +512,7 @@ export async function testChannelDirect(input: FetchModelsInput): Promise<Channe
       case 'doubao':
       case 'qwen':
       case 'custom':
-        return await testOpenAICompatible(input.baseUrl, input.apiKey, proxyUrl)
+        return await testOpenAICompatible(input.baseUrl, input.apiKey, proxyUrl, input.provider)
       case 'google':
         return await testGoogle(input.baseUrl, input.apiKey, proxyUrl)
       default:
@@ -479,7 +552,7 @@ export async function fetchModels(input: FetchModelsInput): Promise<FetchModelsR
       case 'doubao':
       case 'qwen':
       case 'custom':
-        return await fetchOpenAICompatibleModels(input.baseUrl, input.apiKey, proxyUrl)
+        return await fetchOpenAICompatibleModels(input.baseUrl, input.apiKey, proxyUrl, input.provider)
       case 'google':
         return await fetchGoogleModels(input.baseUrl, input.apiKey, proxyUrl)
       default:
@@ -504,8 +577,8 @@ interface AnthropicModelItem {
 /**
  * 从 Anthropic 兼容 API 拉取模型列表（Anthropic / DeepSeek / Kimi API / Kimi Coding Plan / MiniMax）
  *
- * DeepSeek / Kimi 等以 /anthropic 为协议根路径的供应商，实际端点位于 /anthropic/v1/messages，
- * 由 normalizeAnthropicProviderUrl 统一按需补 /v1。
+ * DeepSeek / Kimi 等内置供应商会按协议根路径补全模型端点。
+ * Anthropic 兼容格式使用完整请求地址，不再推导模型端点。
  * Kimi Coding Plan 必须发送 Proma User-Agent。
  * 文档: https://docs.anthropic.com/en/api/models-list
  */
@@ -515,7 +588,11 @@ async function fetchAnthropicCompatibleModels(
   proxyUrl?: string,
   provider: ProviderType = 'anthropic',
 ): Promise<FetchModelsResult> {
-  const url = normalizeAnthropicProviderUrl(baseUrl, provider)
+  if (provider === 'anthropic-compatible') {
+    return { success: false, message: 'Anthropic 兼容格式使用完整请求地址，请手动添加模型', models: [] }
+  }
+
+  const url = resolveAnthropicModelsUrl(baseUrl, provider)
   const fetchFn = getFetchFn(proxyUrl)
 
   const headers: Record<string, string> = {
@@ -534,7 +611,7 @@ async function fetchAnthropicCompatibleModels(
     headers.Authorization = `Bearer ${apiKey}`
   }
 
-  const response = await fetchFn(`${url}/models`, withTimeout({
+  const response = await fetchFn(url, withTimeout({
     method: 'GET',
     headers,
   }))
@@ -574,11 +651,20 @@ interface OpenAIModelItem {
  * API: GET {baseUrl}/models
  * 通用 OpenAI 兼容格式，适用于大部分第三方供应商。
  */
-async function fetchOpenAICompatibleModels(baseUrl: string, apiKey: string, proxyUrl?: string): Promise<FetchModelsResult> {
-  const url = normalizeBaseUrl(baseUrl)
+async function fetchOpenAICompatibleModels(
+  baseUrl: string,
+  apiKey: string,
+  proxyUrl?: string,
+  provider: ProviderType = 'openai',
+): Promise<FetchModelsResult> {
+  if (provider === 'custom') {
+    return { success: false, message: 'OpenAI 兼容格式使用完整请求地址，请手动添加模型', models: [] }
+  }
+
+  const url = resolveOpenAIModelsUrl(baseUrl)
   const fetchFn = getFetchFn(proxyUrl)
 
-  const response = await fetchFn(`${url}/models`, withTimeout({
+  const response = await fetchFn(url, withTimeout({
     method: 'GET',
     headers: {
       Authorization: `Bearer ${apiKey}`,

apps/electron/src/renderer/components/settings/ChannelForm.tsx
@@ -42,7 +42,7 @@ import type {
   FetchModelsResult,
   ProviderType,
 } from '@proma/shared'
-import { normalizeAnthropicProviderUrl } from '@proma/core'
+import { resolveAnthropicMessagesUrl, resolveOpenAIChatCompletionsUrl } from '@proma/core'
 import { getProviderLogo } from '@/lib/model-logo'
 import { ScrollArea } from '@/components/ui/scroll-area'
 import {
@@ -81,26 +81,6 @@ const PROVIDER_SELECT_OPTIONS = PROVIDER_OPTIONS.map((p) => ({
   icon: getProviderLogo(p),
 }))
 
-/** 各供应商的 Chat 端点路径，用于 Base URL 预览 */
-const PROVIDER_CHAT_PATHS: Record<ProviderType, string> = {
-  anthropic: '/v1/messages',
-  'anthropic-compatible': '/v1/messages',
-  openai: '/chat/completions',
-  deepseek: '/messages',
-  google: '/v1beta/models/{model}:generateContent',
-  'kimi-api': '/messages',
-  'kimi-coding': '/messages',
-  zhipu: '/chat/completions',
-  'zhipu-coding': '/messages',
-  minimax: '/v1/messages',
-  doubao: '/chat/completions',
-  qwen: '/chat/completions',
-  'qwen-anthropic': '/v1/messages',
-  xiaomi: '/v1/messages',
-  'xiaomi-token-plan': '/v1/messages',
-  custom: '/chat/completions',
-}
-
 /** 走 Anthropic 协议的供应商集合（共用 /v1/messages 端点） */
 const ANTHROPIC_PROTOCOL_PROVIDERS: ReadonlySet<ProviderType> = new Set<ProviderType>([
   'anthropic',
@@ -118,15 +98,26 @@ const ANTHROPIC_PROTOCOL_PROVIDERS: ReadonlySet<ProviderType> = new Set<Provider
 /**
  * 生成 API 端点预览 URL
  *
- * Anthropic 协议供应商：复用 normalizeAnthropicProviderUrl 计算 base，再拼 /messages，
- * 与运行时 channel-manager / AnthropicAdapter 的规范化逻辑保持一致。
+ * 与运行时 channel-manager / ProviderAdapter 的端点解析逻辑保持一致。
  */
 function buildPreviewUrl(baseUrl: string, provider: ProviderType): string {
   if (ANTHROPIC_PROTOCOL_PROVIDERS.has(provider)) {
-    return `${normalizeAnthropicProviderUrl(baseUrl, provider)}/messages`
+    return resolveAnthropicMessagesUrl(baseUrl, provider)
   }
-  const trimmed = baseUrl.trim().replace(/\/+$/, '')
-  return `${trimmed}${PROVIDER_CHAT_PATHS[provider]}`
+  if (provider === 'google') {
+    return `${baseUrl.trim().replace(/\/+$/, '')}/v1beta/models/{model}:generateContent`
+  }
+  return resolveOpenAIChatCompletionsUrl(baseUrl, provider)
+}
+
+function getUrlInputLabel(provider: ProviderType): string {
+  return provider === 'custom' || provider === 'anthropic-compatible' ? '请求地址' : 'Base URL'
+}
+
+function getUrlInputPlaceholder(provider: ProviderType): string {
+  if (provider === 'custom') return 'https://api.example.com/v1/chat/completions'
+  if (provider === 'anthropic-compatible') return 'https://api.example.com/v1/messages'
+  return 'https://api.example.com'
 }
 
 /** auto-save 防抖延迟 */
@@ -542,10 +533,10 @@ export function ChannelForm({ channel, onSaved, onAgentEligibilityChange, onCanc
             required
           />
           <SettingsInput
-            label="Base URL"
+            label={getUrlInputLabel(provider)}
             value={baseUrl}
             onChange={setBaseUrl}
-            placeholder="https://api.example.com"
+            placeholder={getUrlInputPlaceholder(provider)}
             description={baseUrl.trim() ? `预览：${buildPreviewUrl(baseUrl, provider)}` : undefined}
           />
           {/* API Key + 测试连接同行 */}

packages/core/src/providers/openai-adapter.ts
@@ -10,6 +10,7 @@
  * - 认证：Authorization: Bearer
  */
 
+import type { ProviderType } from '@proma/shared'
 import type {
   ProviderAdapter,
   ProviderRequest,
@@ -20,7 +21,7 @@ import type {
   ToolDefinition,
   ContinuationMessage,
 } from './types.ts'
-import { normalizeBaseUrl } from './url-utils.ts'
+import { resolveOpenAIChatCompletionsUrl } from './url-utils.ts'
 
 // ===== OpenAI 特有类型 =====
 
@@ -185,10 +186,14 @@ function appendContinuationMessages(
 // ===== 适配器实现 =====
 
 export class OpenAIAdapter implements ProviderAdapter {
-  readonly providerType = 'openai' as const
+  readonly providerType: ProviderType
+
+  constructor(providerType: ProviderType = 'openai') {
+    this.providerType = providerType
+  }
 
   buildStreamRequest(input: StreamRequestInput): ProviderRequest {
-    const url = normalizeBaseUrl(input.baseUrl)
+    const url = resolveOpenAIChatCompletionsUrl(input.baseUrl, this.providerType)
     const messages = toOpenAIMessages(input)
 
     const bodyObj: Record<string, unknown> = {
@@ -208,7 +213,7 @@ export class OpenAIAdapter implements ProviderAdapter {
     }
 
     return {
-      url: `${url}/chat/completions`,
+      url,
       headers: {
         'Authorization': `Bearer ${input.apiKey}`,
         'content-type': 'application/json',
@@ -267,10 +272,10 @@ export class OpenAIAdapter implements ProviderAdapter {
   }
 
   buildTitleRequest(input: TitleRequestInput): ProviderRequest {
-    const url = normalizeBaseUrl(input.baseUrl)
+    const url = resolveOpenAIChatCompletionsUrl(input.baseUrl, this.providerType)
 
     return {
-      url: `${url}/chat/completions`,
+      url,
       headers: {
         'Authorization': `Bearer ${input.apiKey}`,
         'content-type': 'application/json',

GeoffBao/LuxCoder

(cherry picked from commit ce55e01e747aa1b35bbc30920ecab0f9b2e406fb)

apps/electron/src/main/lib/channel-manager.ts
@@ -24,12 +24,20 @@ import type {
 import { PROVIDER_DEFAULT_URLS } from '@luxagents/shared'
 import { getFetchFn } from './proxy-fetch'
 import { getEffectiveProxyUrl } from './proxy-settings-service'
-import { normalizeBaseUrl, normalizeAnthropicProviderUrl, getAppUserAgent } from '@luxagents/core'
+import {
+  migrateCompatibleChannelBaseUrl,
+  normalizeBaseUrl,
+  resolveAnthropicMessagesUrl,
+  resolveAnthropicModelsUrl,
+  resolveOpenAIChatCompletionsUrl,
+  resolveOpenAIModelsUrl,
+  getAppUserAgent,
+} from '@luxagents/core'
 import { normalizeHttpResponse, normalizeRequestError } from './channel-test-error'
 import pkg from '../../../package.json' with { type: 'json' }
 
 /** 当前配置版本 */
-const CONFIG_VERSION = 1
+const CONFIG_VERSION = 2
 /** 连接测试 / 模型拉取的统一超时时间 */
 const CHANNEL_TEST_TIMEOUT_MS = 15_000
 
@@ -42,7 +50,43 @@ function withTimeout(init: RequestInit): RequestInit {
 }
 
 /**
+ * 将渠道配置迁移到最新版本。
+ *
+ * v1 → v2：custom / anthropic-compatible 两类通用兼容渠道的 baseUrl 语义从「Base URL（运行时
+ * 自动补端点后缀）」改为「完整请求地址（原样使用）」。把存量 baseUrl 一次性补全为旧版本实际
+ * 请求过的完整端点，使升级后的运行时行为与升级前保持一致。详见 migrateCompatibleChannelBaseUrl。
+ *
+ * @returns 迁移后的配置；`changed` 标记是否发生实际变更（决定是否需要回写文件）
+ */
+function migrateConfig(config: ChannelsConfig): { config: ChannelsConfig; changed: boolean } {
+  const version = config.version ?? 1
+  if (version >= CONFIG_VERSION) {
+    return { config, changed: false }
+  }
+
+  let mutated = false
+  const channels = config.channels.map((channel) => {
+    if (channel.provider !== 'custom' && channel.provider !== 'anthropic-compatible') {
+      return channel
+    }
+    const migratedUrl = migrateCompatibleChannelBaseUrl(channel.baseUrl, channel.provider)
+    if (migratedUrl === channel.baseUrl) {
+      return channel
+    }
+    mutated = true
+    console.log(
+      `[渠道管理] v${version}→v${CONFIG_VERSION} 迁移渠道 ${channel.name} (${channel.provider}) Base URL: ${channel.baseUrl} → ${migratedUrl}`,
+    )
+    return { ...channel, baseUrl: migratedUrl }
+  })
+
+  return { config: { version: CONFIG_VERSION, channels }, changed: true }
+}
+
+/**
  * 读取渠道配置文件
+ *
+ * 读取时自动将旧版本配置迁移到 CONFIG_VERSION，并在发生变更时回写。
  */
 function readConfig(): ChannelsConfig {
   const configPath = getChannelsPath()
@@ -53,7 +97,13 @@ function readConfig(): ChannelsConfig {
 
   try {
     const raw = readFileSync(configPath, 'utf-8')
-    return JSON.parse(raw) as ChannelsConfig
+    const parsed = JSON.parse(raw) as ChannelsConfig
+    const { config, changed } = migrateConfig(parsed)
+    if (changed) {
+      writeConfig(config)
+      console.log('[渠道管理] 渠道配置已迁移并持久化')
+    }
+    return config
   } catch (error) {
     console.error('[渠道管理] 读取配置文件失败:', error)
     return { version: CONFIG_VERSION, channels: [] }
@@ -294,7 +344,7 @@ export async function testChannel(channelId: string): Promise<ChannelTestResult>
       case 'doubao':
       case 'qwen':
       case 'custom':
-        return await testOpenAICompatible(channel.baseUrl, apiKey, proxyUrl)
+        return await testOpenAICompatible(channel.baseUrl, apiKey, proxyUrl, channel.provider)
       case 'google':
         return await testGoogle(channel.baseUrl, apiKey, proxyUrl)
       default:
@@ -308,8 +358,8 @@ export async function testChannel(channelId: string): Promise<ChannelTestResult>
 /**
  * 测试 Anthropic 兼容 API 连接（Anthropic / DeepSeek / Kimi API / Kimi Coding Plan / MiniMax）
  *
- * DeepSeek / Kimi 等以 /anthropic 为协议根路径的供应商，实际端点位于 /anthropic/v1/messages，
- * 由 normalizeAnthropicProviderUrl 统一按需补 /v1。
+ * DeepSeek / Kimi 等内置供应商会按协议根路径补全端点。
+ * Anthropic 兼容格式使用用户填写的完整请求地址。
  * Kimi Coding Plan 必须发送 LuxAgents User-Agent，否则返回 403。
  */
 async function testAnthropicCompatible(
@@ -318,7 +368,7 @@ async function testAnthropicCompatible(
   proxyUrl?: string,
   provider: ProviderType = 'anthropic',
 ): Promise<ChannelTestResult> {
-  const url = normalizeAnthropicProviderUrl(baseUrl, provider)
+  const url = resolveAnthropicMessagesUrl(baseUrl, provider)
   const fetchFn = getFetchFn(proxyUrl)
 
   let testModel: string
@@ -366,7 +416,7 @@ async function testAnthropicCompatible(
     headers.Authorization = `Bearer ${apiKey}`
   }
 
-  const response = await fetchFn(`${url}/messages`, withTimeout({
+  const response = await fetchFn(url, withTimeout({
     method: 'POST',
     headers,
     body: JSON.stringify({
@@ -382,11 +432,34 @@ async function testAnthropicCompatible(
 /**
  * 测试 OpenAI 兼容 API 连接（OpenAI / Custom）
  */
-async function testOpenAICompatible(baseUrl: string, apiKey: string, proxyUrl?: string): Promise<ChannelTestResult> {
-  const url = normalizeBaseUrl(baseUrl)
+async function testOpenAICompatible(
+  baseUrl: string,
+  apiKey: string,
+  proxyUrl?: string,
+  provider: ProviderType = 'openai',
+): Promise<ChannelTestResult> {
+  if (provider === 'custom') {
+    const url = resolveOpenAIChatCompletionsUrl(baseUrl, provider)
+    const fetchFn = getFetchFn(proxyUrl)
+    const response = await fetchFn(url, withTimeout({
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${apiKey}`,
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        model: 'gpt-4o-mini',
+        messages: [{ role: 'user', content: 'hi' }],
+        max_tokens: 1,
+      }),
+    }))
+    return normalizeHttpResponse(response)
+  }
+
+  const url = resolveOpenAIModelsUrl(baseUrl)
   const fetchFn = getFetchFn(proxyUrl)
 
-  const response = await fetchFn(`${url}/models`, withTimeout({
+  const response = await fetchFn(url, withTimeout({
     method: 'GET',
     headers: {
       Authorization: `Bearer ${apiKey}`,
@@ -439,7 +512,7 @@ export async function testChannelDirect(input: FetchModelsInput): Promise<Channe
       case 'doubao':
       case 'qwen':
       case 'custom':
-        return await testOpenAICompatible(input.baseUrl, input.apiKey, proxyUrl)
+        return await testOpenAICompatible(input.baseUrl, input.apiKey, proxyUrl, input.provider)
       case 'google':
         return await testGoogle(input.baseUrl, input.apiKey, proxyUrl)
       default:
@@ -479,7 +552,7 @@ export async function fetchModels(input: FetchModelsInput): Promise<FetchModelsR
       case 'doubao':
       case 'qwen':
       case 'custom':
-        return await fetchOpenAICompatibleModels(input.baseUrl, input.apiKey, proxyUrl)
+        return await fetchOpenAICompatibleModels(input.baseUrl, input.apiKey, proxyUrl, input.provider)
       case 'google':
         return await fetchGoogleModels(input.baseUrl, input.apiKey, proxyUrl)
       default:
@@ -504,8 +577,8 @@ interface AnthropicModelItem {
 /**
  * 从 Anthropic 兼容 API 拉取模型列表（Anthropic / DeepSeek / Kimi API / Kimi Coding Plan / MiniMax）
  *
- * DeepSeek / Kimi 等以 /anthropic 为协议根路径的供应商，实际端点位于 /anthropic/v1/messages，
- * 由 normalizeAnthropicProviderUrl 统一按需补 /v1。
+ * DeepSeek / Kimi 等内置供应商会按协议根路径补全模型端点。
+ * Anthropic 兼容格式使用完整请求地址，不再推导模型端点。
  * Kimi Coding Plan 必须发送 LuxAgents User-Agent。
  * 文档: https://docs.anthropic.com/en/api/models-list
  */
@@ -515,7 +588,11 @@ async function fetchAnthropicCompatibleModels(
   proxyUrl?: string,
   provider: ProviderType = 'anthropic',
 ): Promise<FetchModelsResult> {
-  const url = normalizeAnthropicProviderUrl(baseUrl, provider)
+  if (provider === 'anthropic-compatible') {
+    return { success: false, message: 'Anthropic 兼容格式使用完整请求地址，请手动添加模型', models: [] }
+  }
+
+  const url = resolveAnthropicModelsUrl(baseUrl, provider)
   const fetchFn = getFetchFn(proxyUrl)
 
   const headers: Record<string, string> = {
@@ -534,7 +611,7 @@ async function fetchAnthropicCompatibleModels(
     headers.Authorization = `Bearer ${apiKey}`
   }
 
-  const response = await fetchFn(`${url}/models`, withTimeout({
+  const response = await fetchFn(url, withTimeout({
     method: 'GET',
     headers,
   }))
@@ -574,11 +651,20 @@ interface OpenAIModelItem {
  * API: GET {baseUrl}/models
  * 通用 OpenAI 兼容格式，适用于大部分第三方供应商。
  */
-async function fetchOpenAICompatibleModels(baseUrl: string, apiKey: string, proxyUrl?: string): Promise<FetchModelsResult> {
-  const url = normalizeBaseUrl(baseUrl)
+async function fetchOpenAICompatibleModels(
+  baseUrl: string,
+  apiKey: string,
+  proxyUrl?: string,
+  provider: ProviderType = 'openai',
+): Promise<FetchModelsResult> {
+  if (provider === 'custom') {
+    return { success: false, message: 'OpenAI 兼容格式使用完整请求地址，请手动添加模型', models: [] }
+  }
+
+  const url = resolveOpenAIModelsUrl(baseUrl)
   const fetchFn = getFetchFn(proxyUrl)
 
-  const response = await fetchFn(`${url}/models`, withTimeout({
+  const response = await fetchFn(url, withTimeout({
     method: 'GET',
     headers: {
       Authorization: `Bearer ${apiKey}`,

apps/electron/src/renderer/components/settings/ChannelForm.tsx
@@ -42,7 +42,7 @@ import type {
   FetchModelsResult,
   ProviderType,
 } from '@luxagents/shared'
-import { normalizeAnthropicProviderUrl } from '@luxagents/core'
+import { resolveAnthropicMessagesUrl, resolveOpenAIChatCompletionsUrl } from '@luxagents/core'
 import { getProviderLogo } from '@/lib/model-logo'
 import { ScrollArea } from '@/components/ui/scroll-area'
 import {
@@ -81,26 +81,6 @@ const PROVIDER_SELECT_OPTIONS = PROVIDER_OPTIONS.map((p) => ({
   icon: getProviderLogo(p),
 }))
 
-/** 各供应商的 Chat 端点路径，用于 Base URL 预览 */
-const PROVIDER_CHAT_PATHS: Record<ProviderType, string> = {
-  anthropic: '/v1/messages',
-  'anthropic-compatible': '/v1/messages',
-  openai: '/chat/completions',
-  deepseek: '/messages',
-  google: '/v1beta/models/{model}:generateContent',
-  'kimi-api': '/messages',
-  'kimi-coding': '/messages',
-  zhipu: '/chat/completions',
-  'zhipu-coding': '/messages',
-  minimax: '/v1/messages',
-  doubao: '/chat/completions',
-  qwen: '/chat/completions',
-  'qwen-anthropic': '/v1/messages',
-  xiaomi: '/v1/messages',
-  'xiaomi-token-plan': '/v1/messages',
-  custom: '/chat/completions',
-}
-
 /** 走 Anthropic 协议的供应商集合（共用 /v1/messages 端点） */
 const ANTHROPIC_PROTOCOL_PROVIDERS: ReadonlySet<ProviderType> = new Set<ProviderType>([
   'anthropic',
@@ -118,15 +98,26 @@ const ANTHROPIC_PROTOCOL_PROVIDERS: ReadonlySet<ProviderType> = new Set<Provider
 /**
  * 生成 API 端点预览 URL
  *
- * Anthropic 协议供应商：复用 normalizeAnthropicProviderUrl 计算 base，再拼 /messages，
- * 与运行时 channel-manager / AnthropicAdapter 的规范化逻辑保持一致。
+ * 与运行时 channel-manager / ProviderAdapter 的端点解析逻辑保持一致。
  */
 function buildPreviewUrl(baseUrl: string, provider: ProviderType): string {
   if (ANTHROPIC_PROTOCOL_PROVIDERS.has(provider)) {
-    return `${normalizeAnthropicProviderUrl(baseUrl, provider)}/messages`
+    return resolveAnthropicMessagesUrl(baseUrl, provider)
   }
-  const trimmed = baseUrl.trim().replace(/\/+$/, '')
-  return `${trimmed}${PROVIDER_CHAT_PATHS[provider]}`
+  if (provider === 'google') {
+    return `${baseUrl.trim().replace(/\/+$/, '')}/v1beta/models/{model}:generateContent`
+  }
+  return resolveOpenAIChatCompletionsUrl(baseUrl, provider)
+}
+
+function getUrlInputLabel(provider: ProviderType): string {
+  return provider === 'custom' || provider === 'anthropic-compatible' ? '请求地址' : 'Base URL'
+}
+
+function getUrlInputPlaceholder(provider: ProviderType): string {
+  if (provider === 'custom') return 'https://api.example.com/v1/chat/completions'
+  if (provider === 'anthropic-compatible') return 'https://api.example.com/v1/messages'
+  return 'https://api.example.com'
 }
 
 /** auto-save 防抖延迟 */
@@ -542,10 +533,10 @@ export function ChannelForm({ channel, onSaved, onAgentEligibilityChange, onCanc
             required
           />
           <SettingsInput
-            label="Base URL"
+            label={getUrlInputLabel(provider)}
             value={baseUrl}
             onChange={setBaseUrl}
-            placeholder="https://api.example.com"
+            placeholder={getUrlInputPlaceholder(provider)}
             description={baseUrl.trim() ? `预览：${buildPreviewUrl(baseUrl, provider)}` : undefined}
           />
           {/* API Key + 测试连接同行 */}

packages/core/src/providers/openai-adapter.ts
@@ -10,6 +10,7 @@
  * - 认证：Authorization: Bearer
  */
 
+import type { ProviderType } from '@luxagents/shared'
 import type {
   ProviderAdapter,
   ProviderRequest,
@@ -20,7 +21,7 @@ import type {
   ToolDefinition,
   ContinuationMessage,
 } from './types.ts'
-import { normalizeBaseUrl } from './url-utils.ts'
+import { resolveOpenAIChatCompletionsUrl } from './url-utils.ts'
 
 // ===== OpenAI 特有类型 =====
 
@@ -185,10 +186,14 @@ function appendContinuationMessages(
 // ===== 适配器实现 =====
 
 export class OpenAIAdapter implements ProviderAdapter {
-  readonly providerType = 'openai' as const
+  readonly providerType: ProviderType
+
+  constructor(providerType: ProviderType = 'openai') {
+    this.providerType = providerType
+  }
 
   buildStreamRequest(input: StreamRequestInput): ProviderRequest {
-    const url = normalizeBaseUrl(input.baseUrl)
+    const url = resolveOpenAIChatCompletionsUrl(input.baseUrl, this.providerType)
     const messages = toOpenAIMessages(input)
 
     const bodyObj: Record<string, unknown> = {
@@ -208,7 +213,7 @@ export class OpenAIAdapter implements ProviderAdapter {
     }
 
     return {
-      url: `${url}/chat/completions`,
+      url,
       headers: {
         'Authorization': `Bearer ${input.apiKey}`,
         'content-type': 'application/json',
@@ -267,10 +272,10 @@ export class OpenAIAdapter implements ProviderAdapter {
   }
 
   buildTitleRequest(input: TitleRequestInput): ProviderRequest {
-    const url = normalizeBaseUrl(input.baseUrl)
+    const url = resolveOpenAIChatCompletionsUrl(input.baseUrl, this.providerType)
 
     return {
-      url: `${url}/chat/completions`,
+      url,
       headers: {
         'Authorization': `Bearer ${input.apiKey}`,
         'content-type': 'application/json',