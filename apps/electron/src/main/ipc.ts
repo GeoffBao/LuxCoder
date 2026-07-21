jacky1991/Proma

feat(agent): show provider plan quota

apps/electron/src/main/ipc.ts
@@ -127,6 +127,7 @@ import {
   testChannel,
   testChannelDirect,
   fetchModels,
+  getChannelPlanQuota,
 } from './lib/channel-manager'
 import {
   listConversations,
@@ -1152,6 +1153,14 @@ export function registerIpcHandlers(): void {
     }
   )
 
+  // 查询订阅 Plan 额度（用于 Agent Context 圆环 hover 信息）
+  ipcMain.handle(
+    CHANNEL_IPC_CHANNELS.GET_PLAN_QUOTA,
+    async (_, channelId: string): Promise<import('@proma/shared').ChannelPlanQuotaResult> => {
+      return getChannelPlanQuota(channelId)
+    }
+  )
+
   // ===== 对话管理相关 =====
 
   // 获取对话列表
@@ -1756,8 +1765,8 @@ export function registerIpcHandlers(): void {
   // 创建 Agent 会话
   ipcMain.handle(
     AGENT_IPC_CHANNELS.CREATE_SESSION,
-    async (_, title?: string, channelId?: string, workspaceId?: string): Promise<AgentSessionMeta> => {
-      const session = createAgentSession(title, channelId, workspaceId)
+    async (_, title?: string, channelId?: string, workspaceId?: string, modelId?: string): Promise<AgentSessionMeta> => {
+      const session = createAgentSession(title, channelId, workspaceId, modelId)
       feishuBridgeManager.ensureSessionMirror(session).catch((error) => {
         console.error('[飞书 Session 镜像] 新会话建群失败:', error)
       })
@@ -1778,6 +1787,14 @@ export function registerIpcHandlers(): void {
     AGENT_IPC_CHANNELS.UPDATE_TITLE,
     async (_, id: string, title: string): Promise<AgentSessionMeta> => {
       return updateAgentSessionMeta(id, { title })
+    }
+  )
+
+  // 更新 Agent 会话模型选择
+  ipcMain.handle(
+    AGENT_IPC_CHANNELS.UPDATE_SESSION_MODEL,
+    async (_, id: string, channelId?: string, modelId?: string): Promise<AgentSessionMeta> => {
+      return updateAgentSessionMeta(id, { channelId, modelId })
     }
   )
 

apps/electron/src/main/lib/agent-sdk-auth-env.test.ts
@@ -2,7 +2,7 @@ import { describe, expect, test } from 'bun:test'
 import { applyAgentSdkAuthEnv, usesAgentSdkBearerWithUserAgent } from './agent-sdk-auth-env'
 
 describe('Agent SDK 认证环境变量', () => {
-  test.each(['kimi-coding', 'zhipu-coding', 'xiaomi-token-plan'] as const)(
+  test.each(['kimi-coding', 'zhipu-coding', 'zhipu-coding-team', 'xiaomi-token-plan'] as const)(
     'Given %s When 写入 SDK 认证 env Then 使用 Bearer 与 Proma User-Agent',
     (provider) => {
       const env: Record<string, string | undefined> = {}
@@ -15,6 +15,20 @@ describe('Agent SDK 认证环境变量', () => {
       expect(env.ANTHROPIC_API_KEY).toBeUndefined()
     },
   )
+
+  test('Given 智谱团队版 JSON 凭证 When 写入 SDK 认证 env Then 只使用 apiKey 调模型', () => {
+    const env: Record<string, string | undefined> = {}
+
+    applyAgentSdkAuthEnv(
+      env,
+      'zhipu-coding-team',
+      '{"apiKey":"model-key","organization":"org","project":"proj"}',
+      'Proma/test',
+    )
+
+    expect(env.ANTHROPIC_AUTH_TOKEN).toBe('model-key')
+    expect(env.ANTHROPIC_CUSTOM_HEADERS).toBe('User-Agent: Proma/test')
+  })
 
   test('Given 普通 Anthropic 渠道 When 写入 SDK 认证 env Then 使用 API Key', () => {
     const env: Record<string, string | undefined> = {}

apps/electron/src/main/lib/agent-sdk-auth-env.ts
@@ -1,8 +1,9 @@
-import type { ProviderType } from '@proma/shared'
+import { extractZhipuCodingTeamApiToken, type ProviderType } from '@proma/shared'
 
 export function usesAgentSdkBearerWithUserAgent(provider: ProviderType): boolean {
   return provider === 'kimi-coding'
     || provider === 'zhipu-coding'
+    || provider === 'zhipu-coding-team'
     || provider === 'xiaomi-token-plan'
 }
 
@@ -13,7 +14,9 @@ export function applyAgentSdkAuthEnv(
   userAgent: string,
 ): void {
   if (usesAgentSdkBearerWithUserAgent(provider)) {
-    target.ANTHROPIC_AUTH_TOKEN = apiKey
+    target.ANTHROPIC_AUTH_TOKEN = provider === 'zhipu-coding-team'
+      ? extractZhipuCodingTeamApiToken(apiKey)
+      : apiKey
     target.ANTHROPIC_CUSTOM_HEADERS = `User-Agent: ${userAgent}`
     return
   }

apps/electron/src/renderer/components/agent/ContextUsageBadge.tsx
@@ -15,6 +15,7 @@ import { Button } from '@/components/ui/button'
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
 import { inputToolbarButtonClass } from '@/components/ai-elements/input-toolbar-styles'
 import { cn } from '@/lib/utils'
+import type { ChannelPlanQuotaResult, ChannelPlanQuotaWindow } from '@proma/shared'
 
 /** 压缩阈值比例（SDK 在 ~77.5% 窗口大小时自动压缩） */
 const COMPACT_THRESHOLD_RATIO = 0.775
@@ -22,6 +23,10 @@ const COMPACT_THRESHOLD_RATIO = 0.775
 const WARNING_RATIO = 0.80
 /** Popover hover 关闭延迟（ms），与 AgentThinkingPopover 一致 */
 const HOVER_CLOSE_DELAY = 150
+/** 订阅额度缓存时间，避免频繁 hover 时重复打远端 API */
+const PLAN_QUOTA_CACHE_MS = 4 * 60 * 1000
+const PLAN_QUOTA_ERROR_CACHE_MS = 15 * 1000
+const UNSUPPORTED_PLAN_QUOTA_MESSAGE = '当前渠道不支持订阅 Plan 额度查询'
 
 interface ContextUsageBadgeProps {
   inputTokens?: number
@@ -40,6 +45,8 @@ interface ContextUsageBadgeProps {
    * 避免新会话尚未发消息时仍显示上一个会话的 token 数。
    */
   sessionId?: string
+  /** 当前 Agent 渠道 ID，用于 hover 时查询订阅 Plan 剩余额度 */
+  channelId?: string | null
 }
 
 /** 格式化 token 数为可读字符串（如 1234 → "1.2k"） */
@@ -118,6 +125,41 @@ function DetailRow({ label, value, emphasized }: DetailRowProps): React.ReactEle
   )
 }
 
+function formatResetTime(timestamp?: number): string | undefined {
+  if (!timestamp) return undefined
+  return new Intl.DateTimeFormat(undefined, {
+    month: 'numeric',
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+  }).format(new Date(timestamp))
+}
+
+function PlanQuotaRow({ quotaWindow }: { quotaWindow: ChannelPlanQuotaWindow }): React.ReactElement {
+  const resetText = formatResetTime(quotaWindow.resetAt)
+  const value = `${quotaWindow.remainingLabel ?? `${quotaWindow.remainingPercent}%`} 剩余${resetText ? ` · ${resetText}` : ''}`
+  return (
+    <div className="space-y-1">
+      <DetailRow
+        label={quotaWindow.label}
+        value={value}
+        emphasized={quotaWindow.remainingPercent <= 20}
+      />
+      {quotaWindow.showProgress !== false ? (
+        <div className="h-1 overflow-hidden rounded-full bg-foreground/10">
+          <div
+            className={cn(
+              'h-full rounded-full',
+              quotaWindow.remainingPercent <= 20 ? 'bg-amber-500' : 'bg-foreground/60',
+            )}
+            style={{ width: `${Math.max(0, Math.min(100, quotaWindow.remainingPercent))}%` }}
+          />
+        </div>
+      ) : null}
+    </div>
+  )
+}
+
 export function ContextUsageBadge({
   inputTokens,
   outputTokens,
@@ -129,6 +171,7 @@ export function ContextUsageBadge({
   isProcessing,
   onCompact,
   sessionId,
+  channelId,
 }: ContextUsageBadgeProps): React.ReactElement | null {
   // 保留最近一次有效的 token 值，避免切换会话时闪烁消失
   const stableRef = React.useRef<{
@@ -152,6 +195,10 @@ export function ContextUsageBadge({
 
   const [open, setOpen] = React.useState(false)
   const closeTimerRef = React.useRef<number | null>(null)
+  const [quota, setQuota] = React.useState<ChannelPlanQuotaResult | null>(null)
+  const [quotaLoading, setQuotaLoading] = React.useState(false)
+  const quotaChannelRef = React.useRef<string | null>(null)
+  const quotaResultRef = React.useRef<ChannelPlanQuotaResult | null>(null)
 
   const cancelClose = React.useCallback(() => {
     if (closeTimerRef.current != null) {
@@ -166,6 +213,54 @@ export function ContextUsageBadge({
   }, [cancelClose])
 
   React.useEffect(() => cancelClose, [cancelClose])
+
+  React.useEffect(() => {
+    if (!open || !channelId) return
+
+    const cachedForChannel = quotaChannelRef.current === channelId
+    const cachedQuota = cachedForChannel ? quotaResultRef.current : null
+    const cacheTtl = cachedQuota?.supported ? PLAN_QUOTA_CACHE_MS : PLAN_QUOTA_ERROR_CACHE_MS
+    if (cachedQuota && Date.now() - cachedQuota.updatedAt < cacheTtl) {
+      setQuota(cachedQuota)
+      return
+    }
+
+    let cancelled = false
+    setQuotaLoading(true)
+    if (!cachedForChannel) {
+      setQuota(null)
+    }
+
+    window.electronAPI.getChannelPlanQuota(channelId)
+      .then((result) => {
+        if (cancelled) return
+        quotaChannelRef.current = channelId
+        quotaResultRef.current = result
+        setQuota(result)
+      })
+      .catch((error: unknown) => {
+        if (cancelled) return
+        quotaChannelRef.current = channelId
+        const errorResult: ChannelPlanQuotaResult = {
+          supported: false,
+          provider: 'custom',
+          windows: [],
+          updatedAt: Date.now(),
+          message: error instanceof Error ? error.message : '订阅额度查询失败',
+        }
+        quotaResultRef.current = errorResult
+        setQuota(errorResult)
+      })
+      .finally(() => {
+        if (!cancelled) {
+          setQuotaLoading(false)
+        }
+      })
+
+    return () => {
+      cancelled = true
+    }
+  }, [open, channelId])
 
   // 压缩中 → 按钮位置显示 spinner
   if (isCompacting) {
@@ -228,6 +323,13 @@ export function ContextUsageBadge({
     setOpen(false)
   }
 
+  const shouldShowPlanQuota = quotaLoading
+    || (quota != null && (
+      quota.supported
+      || quota.windows.length > 0
+      || quota.message !== UNSUPPORTED_PLAN_QUOTA_MESSAGE
+    ))
+
   return (
     <Popover open={open} onOpenChange={setOpen}>
       <PopoverTrigger asChild>
@@ -285,6 +387,28 @@ export function ContextUsageBadge({
             <div className="text-[11px] text-center text-foreground/50 pt-0.5">
               数据{ageText}
             </div>
+          ) : null}
+
+          {shouldShowPlanQuota ? (
+            <>
+              <div className="h-px bg-border my-0.5" />
+              <div className="text-[11px] font-medium text-foreground/70">
+                订阅额度{quota?.planName ? ` · ${quota.planName}` : ''}
+              </div>
+              {quotaLoading ? (
+                <div className="text-[11px] text-foreground/50">读取中...</div>
+              ) : quota?.supported && quota.windows.length > 0 ? (
+                <div className="flex flex-col gap-1.5">
+                  {quota.windows.map((quotaWindow) => (
+                    <PlanQuotaRow key={`${quotaWindow.type}-${quotaWindow.label}`} quotaWindow={quotaWindow} />
+                  ))}
+                </div>
+              ) : (
+                <div className="text-[11px] text-foreground/50">
+                  {quota?.message ?? '订阅额度查询失败'}
+                </div>
+              )}
+            </>
           ) : null}
 
           <div className="h-px bg-border my-0.5" />

apps/electron/src/renderer/lib/channel-plan-quota.ts
@@ -1,0 +1,16 @@
+import type { Channel, ProviderType } from '@proma/shared'
+
+const PLAN_QUOTA_PROVIDERS = new Set<ProviderType>([
+  'deepseek',
+  'kimi-coding',
+  'minimax',
+  'zhipu',
+  'zhipu-coding',
+  'zhipu-coding-team',
+])
+
+export function supportsChannelPlanQuota(channel: Pick<Channel, 'provider' | 'baseUrl'> | null | undefined): boolean {
+  if (!channel) return false
+  if (PLAN_QUOTA_PROVIDERS.has(channel.provider)) return true
+  return channel.baseUrl.includes('api.kimi.com/coding')
+}

GeoffBao/LuxCoder

(cherry picked from commit 7f423b07ea5c76b869c1e5962ac6f44b76cdc9b7)

apps/electron/src/main/ipc.ts
@@ -128,6 +128,7 @@ import {
   testChannel,
   testChannelDirect,
   fetchModels,
+  getChannelPlanQuota,
 } from './lib/channel-manager'
 import {
   listConversations,
@@ -1156,6 +1157,14 @@ export function registerIpcHandlers(): void {
     }
   )
 
+  // 查询订阅 Plan 额度（用于 Agent Context 圆环 hover 信息）
+  ipcMain.handle(
+    CHANNEL_IPC_CHANNELS.GET_PLAN_QUOTA,
+    async (_, channelId: string): Promise<import('@luxagents/shared').ChannelPlanQuotaResult> => {
+      return getChannelPlanQuota(channelId)
+    }
+  )
+
   // ===== 对话管理相关 =====
 
   // 获取对话列表
@@ -1760,8 +1769,8 @@ export function registerIpcHandlers(): void {
   // 创建 Agent 会话
   ipcMain.handle(
     AGENT_IPC_CHANNELS.CREATE_SESSION,
-    async (_, title?: string, channelId?: string, workspaceId?: string): Promise<AgentSessionMeta> => {
-      const session = createAgentSession(title, channelId, workspaceId)
+    async (_, title?: string, channelId?: string, workspaceId?: string, modelId?: string): Promise<AgentSessionMeta> => {
+      const session = createAgentSession(title, channelId, workspaceId, modelId)
       feishuBridgeManager.ensureSessionMirror(session).catch((error) => {
         console.error('[飞书 Session 镜像] 新会话建群失败:', error)
       })
@@ -1782,6 +1791,14 @@ export function registerIpcHandlers(): void {
     AGENT_IPC_CHANNELS.UPDATE_TITLE,
     async (_, id: string, title: string): Promise<AgentSessionMeta> => {
       return updateAgentSessionMeta(id, { title })
+    }
+  )
+
+  // 更新 Agent 会话模型选择
+  ipcMain.handle(
+    AGENT_IPC_CHANNELS.UPDATE_SESSION_MODEL,
+    async (_, id: string, channelId?: string, modelId?: string): Promise<AgentSessionMeta> => {
+      return updateAgentSessionMeta(id, { channelId, modelId })
     }
   )
 

apps/electron/src/main/lib/agent-sdk-auth-env.test.ts
@@ -2,7 +2,7 @@ import { describe, expect, test } from 'bun:test'
 import { applyAgentSdkAuthEnv, usesAgentSdkBearerWithUserAgent } from './agent-sdk-auth-env'
 
 describe('Agent SDK 认证环境变量', () => {
-  test.each(['kimi-coding', 'zhipu-coding', 'xiaomi-token-plan'] as const)(
+  test.each(['kimi-coding', 'zhipu-coding', 'zhipu-coding-team', 'xiaomi-token-plan'] as const)(
     'Given %s When 写入 SDK 认证 env Then 使用 Bearer 与 LuxAgents User-Agent',
     (provider) => {
       const env: Record<string, string | undefined> = {}
@@ -15,6 +15,20 @@ describe('Agent SDK 认证环境变量', () => {
       expect(env.ANTHROPIC_API_KEY).toBeUndefined()
     },
   )
+
+  test('Given 智谱团队版 JSON 凭证 When 写入 SDK 认证 env Then 只使用 apiKey 调模型', () => {
+    const env: Record<string, string | undefined> = {}
+
+    applyAgentSdkAuthEnv(
+      env,
+      'zhipu-coding-team',
+      '{"apiKey":"model-key","organization":"org","project":"proj"}',
+      'LuxAgents/test',
+    )
+
+    expect(env.ANTHROPIC_AUTH_TOKEN).toBe('model-key')
+    expect(env.ANTHROPIC_CUSTOM_HEADERS).toBe('User-Agent: LuxAgents/test')
+  })
 
   test('Given 普通 Anthropic 渠道 When 写入 SDK 认证 env Then 使用 API Key', () => {
     const env: Record<string, string | undefined> = {}

apps/electron/src/main/lib/agent-sdk-auth-env.ts
@@ -1,8 +1,9 @@
-import type { ProviderType } from '@luxagents/shared'
+import { extractZhipuCodingTeamApiToken, type ProviderType } from '@luxagents/shared'
 
 export function usesAgentSdkBearerWithUserAgent(provider: ProviderType): boolean {
   return provider === 'kimi-coding'
     || provider === 'zhipu-coding'
+    || provider === 'zhipu-coding-team'
     || provider === 'xiaomi-token-plan'
 }
 
@@ -13,7 +14,9 @@ export function applyAgentSdkAuthEnv(
   userAgent: string,
 ): void {
   if (usesAgentSdkBearerWithUserAgent(provider)) {
-    target.ANTHROPIC_AUTH_TOKEN = apiKey
+    target.ANTHROPIC_AUTH_TOKEN = provider === 'zhipu-coding-team'
+      ? extractZhipuCodingTeamApiToken(apiKey)
+      : apiKey
     target.ANTHROPIC_CUSTOM_HEADERS = `User-Agent: ${userAgent}`
     return
   }

apps/electron/src/renderer/components/agent/ContextUsageBadge.tsx
@@ -15,6 +15,7 @@ import { Button } from '@/components/ui/button'
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
 import { inputToolbarButtonClass } from '@/components/ai-elements/input-toolbar-styles'
 import { cn } from '@/lib/utils'
+import type { ChannelPlanQuotaResult, ChannelPlanQuotaWindow } from '@luxagents/shared'
 
 /** 压缩阈值比例（SDK 在 ~77.5% 窗口大小时自动压缩） */
 const COMPACT_THRESHOLD_RATIO = 0.775
@@ -22,6 +23,10 @@ const COMPACT_THRESHOLD_RATIO = 0.775
 const WARNING_RATIO = 0.80
 /** Popover hover 关闭延迟（ms），与 AgentThinkingPopover 一致 */
 const HOVER_CLOSE_DELAY = 150
+/** 订阅额度缓存时间，避免频繁 hover 时重复打远端 API */
+const PLAN_QUOTA_CACHE_MS = 4 * 60 * 1000
+const PLAN_QUOTA_ERROR_CACHE_MS = 15 * 1000
+const UNSUPPORTED_PLAN_QUOTA_MESSAGE = '当前渠道不支持订阅 Plan 额度查询'
 
 interface ContextUsageBadgeProps {
   inputTokens?: number
@@ -40,6 +45,8 @@ interface ContextUsageBadgeProps {
    * 避免新会话尚未发消息时仍显示上一个会话的 token 数。
    */
   sessionId?: string
+  /** 当前 Agent 渠道 ID，用于 hover 时查询订阅 Plan 剩余额度 */
+  channelId?: string | null
 }
 
 /** 格式化 token 数为可读字符串（如 1234 → "1.2k"） */
@@ -118,6 +125,41 @@ function DetailRow({ label, value, emphasized }: DetailRowProps): React.ReactEle
   )
 }
 
+function formatResetTime(timestamp?: number): string | undefined {
+  if (!timestamp) return undefined
+  return new Intl.DateTimeFormat(undefined, {
+    month: 'numeric',
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+  }).format(new Date(timestamp))
+}
+
+function PlanQuotaRow({ quotaWindow }: { quotaWindow: ChannelPlanQuotaWindow }): React.ReactElement {
+  const resetText = formatResetTime(quotaWindow.resetAt)
+  const value = `${quotaWindow.remainingLabel ?? `${quotaWindow.remainingPercent}%`} 剩余${resetText ? ` · ${resetText}` : ''}`
+  return (
+    <div className="space-y-1">
+      <DetailRow
+        label={quotaWindow.label}
+        value={value}
+        emphasized={quotaWindow.remainingPercent <= 20}
+      />
+      {quotaWindow.showProgress !== false ? (
+        <div className="h-1 overflow-hidden rounded-full bg-foreground/10">
+          <div
+            className={cn(
+              'h-full rounded-full',
+              quotaWindow.remainingPercent <= 20 ? 'bg-amber-500' : 'bg-foreground/60',
+            )}
+            style={{ width: `${Math.max(0, Math.min(100, quotaWindow.remainingPercent))}%` }}
+          />
+        </div>
+      ) : null}
+    </div>
+  )
+}
+
 export function ContextUsageBadge({
   inputTokens,
   outputTokens,
@@ -129,6 +171,7 @@ export function ContextUsageBadge({
   isProcessing,
   onCompact,
   sessionId,
+  channelId,
 }: ContextUsageBadgeProps): React.ReactElement | null {
   // 保留最近一次有效的 token 值，避免切换会话时闪烁消失
   const stableRef = React.useRef<{
@@ -152,6 +195,10 @@ export function ContextUsageBadge({
 
   const [open, setOpen] = React.useState(false)
   const closeTimerRef = React.useRef<number | null>(null)
+  const [quota, setQuota] = React.useState<ChannelPlanQuotaResult | null>(null)
+  const [quotaLoading, setQuotaLoading] = React.useState(false)
+  const quotaChannelRef = React.useRef<string | null>(null)
+  const quotaResultRef = React.useRef<ChannelPlanQuotaResult | null>(null)
 
   const cancelClose = React.useCallback(() => {
     if (closeTimerRef.current != null) {
@@ -166,6 +213,54 @@ export function ContextUsageBadge({
   }, [cancelClose])
 
   React.useEffect(() => cancelClose, [cancelClose])
+
+  React.useEffect(() => {
+    if (!open || !channelId) return
+
+    const cachedForChannel = quotaChannelRef.current === channelId
+    const cachedQuota = cachedForChannel ? quotaResultRef.current : null
+    const cacheTtl = cachedQuota?.supported ? PLAN_QUOTA_CACHE_MS : PLAN_QUOTA_ERROR_CACHE_MS
+    if (cachedQuota && Date.now() - cachedQuota.updatedAt < cacheTtl) {
+      setQuota(cachedQuota)
+      return
+    }
+
+    let cancelled = false
+    setQuotaLoading(true)
+    if (!cachedForChannel) {
+      setQuota(null)
+    }
+
+    window.electronAPI.getChannelPlanQuota(channelId)
+      .then((result) => {
+        if (cancelled) return
+        quotaChannelRef.current = channelId
+        quotaResultRef.current = result
+        setQuota(result)
+      })
+      .catch((error: unknown) => {
+        if (cancelled) return
+        quotaChannelRef.current = channelId
+        const errorResult: ChannelPlanQuotaResult = {
+          supported: false,
+          provider: 'custom',
+          windows: [],
+          updatedAt: Date.now(),
+          message: error instanceof Error ? error.message : '订阅额度查询失败',
+        }
+        quotaResultRef.current = errorResult
+        setQuota(errorResult)
+      })
+      .finally(() => {
+        if (!cancelled) {
+          setQuotaLoading(false)
+        }
+      })
+
+    return () => {
+      cancelled = true
+    }
+  }, [open, channelId])
 
   // 压缩中 → 按钮位置显示 spinner
   if (isCompacting) {
@@ -228,6 +323,13 @@ export function ContextUsageBadge({
     setOpen(false)
   }
 
+  const shouldShowPlanQuota = quotaLoading
+    || (quota != null && (
+      quota.supported
+      || quota.windows.length > 0
+      || quota.message !== UNSUPPORTED_PLAN_QUOTA_MESSAGE
+    ))
+
   return (
     <Popover open={open} onOpenChange={setOpen}>
       <PopoverTrigger asChild>
@@ -285,6 +387,28 @@ export function ContextUsageBadge({
             <div className="text-[11px] text-center text-foreground/50 pt-0.5">
               数据{ageText}
             </div>
+          ) : null}
+
+          {shouldShowPlanQuota ? (
+            <>
+              <div className="h-px bg-border my-0.5" />
+              <div className="text-[11px] font-medium text-foreground/70">
+                订阅额度{quota?.planName ? ` · ${quota.planName}` : ''}
+              </div>
+              {quotaLoading ? (
+                <div className="text-[11px] text-foreground/50">读取中...</div>
+              ) : quota?.supported && quota.windows.length > 0 ? (
+                <div className="flex flex-col gap-1.5">
+                  {quota.windows.map((quotaWindow) => (
+                    <PlanQuotaRow key={`${quotaWindow.type}-${quotaWindow.label}`} quotaWindow={quotaWindow} />
+                  ))}
+                </div>
+              ) : (
+                <div className="text-[11px] text-foreground/50">
+                  {quota?.message ?? '订阅额度查询失败'}
+                </div>
+              )}
+            </>
           ) : null}
 
           <div className="h-px bg-border my-0.5" />

apps/electron/src/renderer/lib/channel-plan-quota.ts
@@ -1,0 +1,16 @@
+import type { Channel, ProviderType } from '@luxagents/shared'
+
+const PLAN_QUOTA_PROVIDERS = new Set<ProviderType>([
+  'deepseek',
+  'kimi-coding',
+  'minimax',
+  'zhipu',
+  'zhipu-coding',
+  'zhipu-coding-team',
+])
+
+export function supportsChannelPlanQuota(channel: Pick<Channel, 'provider' | 'baseUrl'> | null | undefined): boolean {
+  if (!channel) return false
+  if (PLAN_QUOTA_PROVIDERS.has(channel.provider)) return true
+  return channel.baseUrl.includes('api.kimi.com/coding')
+}