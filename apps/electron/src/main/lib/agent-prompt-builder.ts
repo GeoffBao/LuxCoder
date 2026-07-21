wafo210715/Proma

feat: 协作子会话触发策略更积极——对抗式审查 + 多样性探索 (#1074)
核心改动：
- SubAgent 委派策略从「先相信直觉，再派 SubAgent」翻转为「善用 SubAgent 拓宽探索边界」
- 触发条件从 3 个扩展到 5 个（新增多方案对比、盲区探测）
- 「想一步就派」：Agent 首次推理后自我检查方向是否唯一/是否有盲区，任一为是就 spawn
- 新增「对抗式协作模式」：父 Agent 完成方案后派子 Agent 独立审查（只提建议不改文件）
- 新增「多样性探索模式」：方向不唯一时并行派子 Agent 各探一个方向（只调研不改文件）
- SKILL.md v1.0.1→v1.1.0：更新 frontmatter/工作流/BDD 场景

Co-authored-by: Claude <noreply@anthropic.com>

apps/electron/src/main/lib/agent-prompt-builder.ts
@@ -64,14 +64,21 @@ export function buildSystemPrompt(ctx: SystemPromptContext): string {
   if (ctx.deepSeekSubagentModel === DEEPSEEK_SUBAGENT_MODEL_ID) {
     sections.push(`## SubAgent 委派策略
 
-**先相信直觉，再派 SubAgent。**
+**善用 SubAgent 拓宽探索边界。**
 
-你的第一反应通常是对的，当直觉路径走不通、结果与预期反复不符，或需要充分验证时，再创建 SubAgent 做深度探索和交叉验证。
+想一步做初始方向判断后，如果发现方向不唯一（多个可行方案），或者可能有盲区（假设未验证、边缘情况未覆盖），就立刻 spawn SubAgent 做多角度探索或验证。SubAgent 是你的拓展器，用来并行拓宽探索范围，不是碰壁后的备用方案。
 
-只在以下场景考虑使用 Agent 工具创建临时 SubAgent：
-- 直觉路径尝试后结果与预期不符，或陷入反复
-- 需要并行探索 1 个以上独立子系统
-- 需要独立/对抗性视角（如安全审计、咨询、设计、调研等场景）
+典型触发条件（满足任意一条即可 spawn）：
+- **多方案对比**：问题有多个可行方案，方向不唯一，需要并行探索对比优劣
+- **对抗性审查**：已有方案需要独立视角挑战假设、探测盲区和边缘情况
+- **并行探索**：需要同时探索 1 个以上独立子系统或模块
+- **盲区探测**：对当前路径的假设合理性不确定，或担心边缘情况未覆盖
+- **路径遇阻**：直觉路径尝试后结果与预期不符，或陷入反复
+
+注意分级：
+- 简单搜索、短调研、单文件定位 — 用 SDK SubAgent（更轻量）
+- 多方案对比、对抗性审查、跨模块并行探索 — 用 Agent 工具创建临时 SubAgent
+- 长耗时、需要用户观察进展或保留完整记录 — 用 Proma collaboration 创建真实子会话
 
 Proma 没有预定义内置 SubAgent。临时 SubAgent 固定路由到 \`${DEEPSEEK_SUBAGENT_MODEL_ID}\`，不要通过 \`model\` 参数指定模型，也不要使用 haiku/sonnet/opus 等 Claude 模型别名。
 
@@ -79,27 +86,41 @@ Proma 没有预定义内置 SubAgent。临时 SubAgent 固定路由到 \`${DEEPS
   } else if (claudeAvailable) {
     sections.push(`## SubAgent 委派策略
 
-**先相信直觉，再派 SubAgent。**
+**善用 SubAgent 拓宽探索边界。**
 
-你的第一反应通常是对的，当直觉路径走不通、结果与预期反复不符，或需要充分验证时，再创建 SubAgent 做深度探索和交叉验证。
+想一步做初始方向判断后，如果发现方向不唯一（多个可行方案），或者可能有盲区（假设未验证、边缘情况未覆盖），就立刻 spawn SubAgent 做多角度探索或验证。SubAgent 是你的拓展器，用来并行拓宽探索范围，不是碰壁后的备用方案。
 
-只在以下场景考虑使用 Agent 工具创建临时 SubAgent：
-- 直觉路径尝试后结果与预期不符，或陷入反复
-- 需要并行探索 1 个以上独立子系统
-- 需要独立/对抗性视角（如安全审计、咨询、设计、调研等场景）
+典型触发条件（满足任意一条即可 spawn）：
+- **多方案对比**：问题有多个可行方案，方向不唯一，需要并行探索对比优劣
+- **对抗性审查**：已有方案需要独立视角挑战假设、探测盲区和边缘情况
+- **并行探索**：需要同时探索 1 个以上独立子系统或模块
+- **盲区探测**：对当前路径的假设合理性不确定，或担心边缘情况未覆盖
+- **路径遇阻**：直觉路径尝试后结果与预期不符，或陷入反复
+
+注意分级：
+- 简单搜索、短调研、单文件定位 — 用 SDK SubAgent（更轻量）
+- 多方案对比、对抗性审查、跨模块并行探索 — 用 Agent 工具创建临时 SubAgent
+- 长耗时、需要用户观察进展或保留完整记录 — 用 Proma collaboration 创建真实子会话
 
 代码审查请使用 SDK 自带的 \`/code-review\` 或 \`/simplify\` Skill`)
   } else {
     sections.push(`## SubAgent 委派策略
 
-**先相信直觉，再派 SubAgent。**
+**善用 SubAgent 拓宽探索边界。**
 
-你的第一反应通常是对的，当直觉路径走不通、结果与预期反复不符，或需要充分验证时，再创建 SubAgent 做深度探索和交叉验证。
+想一步做初始方向判断后，如果发现方向不唯一（多个可行方案），或者可能有盲区（假设未验证、边缘情况未覆盖），就立刻 spawn SubAgent 做多角度探索或验证。SubAgent 是你的拓展器，用来并行拓宽探索范围，不是碰壁后的备用方案。
 
-只在以下场景考虑使用 Agent 工具创建临时 SubAgent：
-- 直觉路径尝试后结果与预期不符，或陷入反复
-- 需要并行探索 1 个以上独立子系统
-- 需要独立/对抗性视角（如安全审计、咨询、设计、调研等场景）
+典型触发条件（满足任意一条即可 spawn）：
+- **多方案对比**：问题有多个可行方案，方向不唯一，需要并行探索对比优劣
+- **对抗性审查**：已有方案需要独立视角挑战假设、探测盲区和边缘情况
+- **并行探索**：需要同时探索 1 个以上独立子系统或模块
+- **盲区探测**：对当前路径的假设合理性不确定，或担心边缘情况未覆盖
+- **路径遇阻**：直觉路径尝试后结果与预期不符，或陷入反复
+
+注意分级：
+- 简单搜索、短调研、单文件定位 — 用 SDK SubAgent（更轻量）
+- 多方案对比、对抗性审查、跨模块并行探索 — 用 Agent 工具创建临时 SubAgent
+- 长耗时、需要用户观察进展或保留完整记录 — 用 Proma collaboration 创建真实子会话
 
 Proma 没有预定义内置 SubAgent。临时 SubAgent 继承当前主模型，不要通过 \`model\` 参数指定 haiku/sonnet/opus 等 Claude 模型别名，否则会导致调用失败。`)
   }
@@ -130,7 +151,29 @@ Proma 提供内置 \`collaboration\` 工具，可以创建真实可见的协作�
 - 需要非阻塞查看状态或按 ID 读取结果时，使用 \`collaboration.list_delegations\` 和 \`collaboration.get_delegation_results\`
 - 委派说明必须自包含：目标、范围、约束、输出格式和必要上下文都写进 task
 - 第一版只允许一级协作，子会话不能再创建新的子会话
-- 父 Agent 必须在合适时机调用 \`collaboration.wait_for_delegations\` 收敛结果，并把关键发现整合给用户`)
+- 父 Agent 必须在合适时机调用 \`collaboration.wait_for_delegations\` 收敛结果，并把关键发现整合给用户
+
+### 对抗式协作模式
+
+当需要对已有方案做独立审查和压力测试时，采用对抗式协作：
+
+1. **父 Agent 完成方案**：先基于自己的分析形成方案、设计或实现
+2. **派子 Agent 做对抗性审查**：创建子 Agent 时，不告知自己的具体实现思路，只说明审查目标和产出。让子 Agent 以独立视角挑战假设、寻找盲区、评估风险和边缘情况
+3. **子 Agent 只提建议不改文件**：对抗性子 Agent 只返回审查报告，包含：发现的问题和风险、改进建议、边缘情况覆盖、替代方案评估。不要直接修改文件
+4. **父 Agent 综合判断**：审查结果回来后逐条评估，决定采纳、调整还是忽略。所有实际修改由父 Agent 执行
+
+触发条件：方案有一定复杂度（如核心算法、安全机制、数据一致性）、对正确性要求高、或父 Agent 对某些假设不确定时。
+
+### 多样性探索模式
+
+当问题有多种可行方案、方向不唯一时，采用多样性探索：
+
+1. **识别多个方向**：父 Agent 先识别出 2-3 个合理方向，确保每个方向有独立的假设或技术路径
+2. **并行派子 Agent 探索**：为每个方向派一个子 Agent，各自独立做深度调研、原型验证或可行性分析。方向少的用 \`delegate_agent\` 逐个创建，方向已经清晰的用 \`delegate_agents\` 批量创建
+3. **子 Agent 只调研不改文件**：每个子 Agent 探索后产出方案报告（优缺点、风险、实施路径、推荐与否），不做代码修改
+4. **父 Agent 汇总对比**：收集所有方向结果后，做对比分析，向用户呈现各方案优势和取舍，供用户决策
+
+触发条件：解决方案的架构选型不确定、有多种合理的技术路径可走、或父 Agent 意识到自己的初始偏好可能影响客观判断时。`)
   }
 
   // 工作区信息

GeoffBao/LuxCoder

(cherry picked from commit 42c574a6a51fe42d63f6b9bea2688102903ea61c)

apps/electron/src/main/lib/agent-prompt-builder.ts
@@ -66,14 +66,21 @@ export function buildSystemPrompt(ctx: SystemPromptContext): string {
   if (ctx.deepSeekSubagentModel === DEEPSEEK_SUBAGENT_MODEL_ID) {
     sections.push(`## SubAgent 委派策略
 
-**先相信直觉，再派 SubAgent。**
+**善用 SubAgent 拓宽探索边界。**
 
-你的第一反应通常是对的，当直觉路径走不通、结果与预期反复不符，或需要充分验证时，再创建 SubAgent 做深度探索和交叉验证。
+想一步做初始方向判断后，如果发现方向不唯一（多个可行方案），或者可能有盲区（假设未验证、边缘情况未覆盖），就立刻 spawn SubAgent 做多角度探索或验证。SubAgent 是你的拓展器，用来并行拓宽探索范围，不是碰壁后的备用方案。
 
-只在以下场景考虑使用 Agent 工具创建临时 SubAgent：
-- 直觉路径尝试后结果与预期不符，或陷入反复
-- 需要并行探索 1 个以上独立子系统
-- 需要独立/对抗性视角（如安全审计、咨询、设计、调研等场景）
+典型触发条件（满足任意一条即可 spawn）：
+- **多方案对比**：问题有多个可行方案，方向不唯一，需要并行探索对比优劣
+- **对抗性审查**：已有方案需要独立视角挑战假设、探测盲区和边缘情况
+- **并行探索**：需要同时探索 1 个以上独立子系统或模块
+- **盲区探测**：对当前路径的假设合理性不确定，或担心边缘情况未覆盖
+- **路径遇阻**：直觉路径尝试后结果与预期不符，或陷入反复
+
+注意分级：
+- 简单搜索、短调研、单文件定位 — 用 SDK SubAgent（更轻量）
+- 多方案对比、对抗性审查、跨模块并行探索 — 用 Agent 工具创建临时 SubAgent
+- 长耗时、需要用户观察进展或保留完整记录 — 用 LuxAgents collaboration 创建真实子会话
 
 LuxAgents 没有预定义内置 SubAgent。临时 SubAgent 固定路由到 \`${DEEPSEEK_SUBAGENT_MODEL_ID}\`，不要通过 \`model\` 参数指定模型，也不要使用 haiku/sonnet/opus 等 Claude 模型别名。
 
@@ -81,27 +88,41 @@ LuxAgents 没有预定义内置 SubAgent。临时 SubAgent 固定路由到 \`${D
   } else if (claudeAvailable) {
     sections.push(`## SubAgent 委派策略
 
-**先相信直觉，再派 SubAgent。**
+**善用 SubAgent 拓宽探索边界。**
 
-你的第一反应通常是对的，当直觉路径走不通、结果与预期反复不符，或需要充分验证时，再创建 SubAgent 做深度探索和交叉验证。
+想一步做初始方向判断后，如果发现方向不唯一（多个可行方案），或者可能有盲区（假设未验证、边缘情况未覆盖），就立刻 spawn SubAgent 做多角度探索或验证。SubAgent 是你的拓展器，用来并行拓宽探索范围，不是碰壁后的备用方案。
 
-只在以下场景考虑使用 Agent 工具创建临时 SubAgent：
-- 直觉路径尝试后结果与预期不符，或陷入反复
-- 需要并行探索 1 个以上独立子系统
-- 需要独立/对抗性视角（如安全审计、咨询、设计、调研等场景）
+典型触发条件（满足任意一条即可 spawn）：
+- **多方案对比**：问题有多个可行方案，方向不唯一，需要并行探索对比优劣
+- **对抗性审查**：已有方案需要独立视角挑战假设、探测盲区和边缘情况
+- **并行探索**：需要同时探索 1 个以上独立子系统或模块
+- **盲区探测**：对当前路径的假设合理性不确定，或担心边缘情况未覆盖
+- **路径遇阻**：直觉路径尝试后结果与预期不符，或陷入反复
+
+注意分级：
+- 简单搜索、短调研、单文件定位 — 用 SDK SubAgent（更轻量）
+- 多方案对比、对抗性审查、跨模块并行探索 — 用 Agent 工具创建临时 SubAgent
+- 长耗时、需要用户观察进展或保留完整记录 — 用 LuxAgents collaboration 创建真实子会话
 
 代码审查请使用 SDK 自带的 \`/code-review\` 或 \`/simplify\` Skill`)
   } else {
     sections.push(`## SubAgent 委派策略
 
-**先相信直觉，再派 SubAgent。**
+**善用 SubAgent 拓宽探索边界。**
 
-你的第一反应通常是对的，当直觉路径走不通、结果与预期反复不符，或需要充分验证时，再创建 SubAgent 做深度探索和交叉验证。
+想一步做初始方向判断后，如果发现方向不唯一（多个可行方案），或者可能有盲区（假设未验证、边缘情况未覆盖），就立刻 spawn SubAgent 做多角度探索或验证。SubAgent 是你的拓展器，用来并行拓宽探索范围，不是碰壁后的备用方案。
 
-只在以下场景考虑使用 Agent 工具创建临时 SubAgent：
-- 直觉路径尝试后结果与预期不符，或陷入反复
-- 需要并行探索 1 个以上独立子系统
-- 需要独立/对抗性视角（如安全审计、咨询、设计、调研等场景）
+典型触发条件（满足任意一条即可 spawn）：
+- **多方案对比**：问题有多个可行方案，方向不唯一，需要并行探索对比优劣
+- **对抗性审查**：已有方案需要独立视角挑战假设、探测盲区和边缘情况
+- **并行探索**：需要同时探索 1 个以上独立子系统或模块
+- **盲区探测**：对当前路径的假设合理性不确定，或担心边缘情况未覆盖
+- **路径遇阻**：直觉路径尝试后结果与预期不符，或陷入反复
+
+注意分级：
+- 简单搜索、短调研、单文件定位 — 用 SDK SubAgent（更轻量）
+- 多方案对比、对抗性审查、跨模块并行探索 — 用 Agent 工具创建临时 SubAgent
+- 长耗时、需要用户观察进展或保留完整记录 — 用 LuxAgents collaboration 创建真实子会话
 
 LuxAgents 没有预定义内置 SubAgent。临时 SubAgent 继承当前主模型，不要通过 \`model\` 参数指定 haiku/sonnet/opus 等 Claude 模型别名，否则会导致调用失败。`)
   }
@@ -132,7 +153,29 @@ LuxAgents 提供内置 \`collaboration\` 工具，可以创建真实可见的协
 - 需要非阻塞查看状态或按 ID 读取结果时，使用 \`collaboration.list_delegations\` 和 \`collaboration.get_delegation_results\`
 - 委派说明必须自包含：目标、范围、约束、输出格式和必要上下文都写进 task
 - 第一版只允许一级协作，子会话不能再创建新的子会话
-- 父 Agent 必须在合适时机调用 \`collaboration.wait_for_delegations\` 收敛结果，并把关键发现整合给用户`)
+- 父 Agent 必须在合适时机调用 \`collaboration.wait_for_delegations\` 收敛结果，并把关键发现整合给用户
+
+### 对抗式协作模式
+
+当需要对已有方案做独立审查和压力测试时，采用对抗式协作：
+
+1. **父 Agent 完成方案**：先基于自己的分析形成方案、设计或实现
+2. **派子 Agent 做对抗性审查**：创建子 Agent 时，不告知自己的具体实现思路，只说明审查目标和产出。让子 Agent 以独立视角挑战假设、寻找盲区、评估风险和边缘情况
+3. **子 Agent 只提建议不改文件**：对抗性子 Agent 只返回审查报告，包含：发现的问题和风险、改进建议、边缘情况覆盖、替代方案评估。不要直接修改文件
+4. **父 Agent 综合判断**：审查结果回来后逐条评估，决定采纳、调整还是忽略。所有实际修改由父 Agent 执行
+
+触发条件：方案有一定复杂度（如核心算法、安全机制、数据一致性）、对正确性要求高、或父 Agent 对某些假设不确定时。
+
+### 多样性探索模式
+
+当问题有多种可行方案、方向不唯一时，采用多样性探索：
+
+1. **识别多个方向**：父 Agent 先识别出 2-3 个合理方向，确保每个方向有独立的假设或技术路径
+2. **并行派子 Agent 探索**：为每个方向派一个子 Agent，各自独立做深度调研、原型验证或可行性分析。方向少的用 \`delegate_agent\` 逐个创建，方向已经清晰的用 \`delegate_agents\` 批量创建
+3. **子 Agent 只调研不改文件**：每个子 Agent 探索后产出方案报告（优缺点、风险、实施路径、推荐与否），不做代码修改
+4. **父 Agent 汇总对比**：收集所有方向结果后，做对比分析，向用户呈现各方案优势和取舍，供用户决策
+
+触发条件：解决方案的架构选型不确定、有多种合理的技术路径可走、或父 Agent 意识到自己的初始偏好可能影响客观判断时。`)
   }
 
   // 工作区信息