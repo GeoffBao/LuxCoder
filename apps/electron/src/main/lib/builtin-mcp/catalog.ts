jacky1991/Proma

feat(mcp): 内置 MCP 单一事实源（default-mcp.json）+ 修复 nano-banana 命名漂移 (#1022)
* feat(mcp): single-source builtin MCP names via default-mcp.json

新增 builtin-mcp/default-mcp.json 作为内置 MCP 的唯一事实源（id 保持历史值、
name 为下划线安全的运行时真实名），baseline.ts 统一加载并提供
getBuiltinMcpName / RESERVED_BUILTIN_KEYS，编译进包随版本覆盖升级。

memory / nano-banana 注入器改为从 baseline 取真实 server 名：

修复 nano-banana 命名漂移 —— SDK 会把 createSdkMcpServer 的 server 名里的
连字符规范化成下划线（'nano-banana' 实际注册为 mcp__nano_banana__*），此前
注入器硬编码连字符名，源名与运行名不一致。改为从 default-mcp.json 取
下划线安全名后，源名 == 运行名。

Co-Authored-By: Claude <noreply@anthropic.com>

* feat(mcp): drive builtin MCP catalog from single source, add default-disabled

catalog.ts 改为从 baseline（default-mcp.json）读取内置 MCP 元数据，删除
硬编码的 BUILTIN_MCP_CATALOG 数组，消除 catalog 与注入器双处手写漂移。

settings.ts 引入 DEFAULT_DISABLED_IDS 机制：nano-banana / mem 默认关闭，
需用户在能力列表手动开启（这两个依赖额外 API Key 才有意义）。新增
isBuiltinMcpDefaultDisabled，并为默认关闭项启用 builtinMcpEnabledIds 白名单。
AppSettings 相应新增 builtinMcpEnabledIds 字段。

Co-Authored-By: Claude <noreply@anthropic.com>

---------

Co-authored-by: Claude <noreply@anthropic.com>

apps/electron/src/main/lib/builtin-mcp/catalog.ts
@@ -2,93 +2,38 @@
  * Proma 内置 MCP 能力目录
  *
  * 这里只维护可展示的元数据和可用性判断，不负责运行时注入。
- * 这样前端能力摘要可以安全读取内置 MCP 列表，而不会引入 Agent 编排层循环依赖。
+ * 元数据本身来自 default-mcp.json（经 baseline 加载），本文件只在其上叠加
+ * 运行时可用性判断（API Key、工作区、登录态等）。这样前端能力摘要可以安全读取
+ * 内置 MCP 列表，而不会引入 Agent 编排层循环依赖。
  */
 
-import type { BuiltinMcpServerSummary, McpToolSummary } from '@proma/shared'
+import type { BuiltinMcpServerSummary } from '@proma/shared'
 import { getToolCredentials, getToolState } from '../chat-tool-config'
 import { getMemoryConfig } from '../memory-service'
-import { isBuiltinMcpUserEnabled } from './settings'
-
-interface BuiltinMcpCatalogItem {
-  id: string
-  name: string
-  displayName: string
-  description: string
-  category: BuiltinMcpServerSummary['category']
-  tools: McpToolSummary[]
-}
+import { getBuiltinMcpDefinitions, type BuiltinMcpDefinition } from './baseline'
+import { isBuiltinMcpDefaultDisabled, isBuiltinMcpUserEnabled } from './settings'
 
 interface BuiltinMcpListContext {
   workspaceSlug?: string
 }
 
-const BUILTIN_MCP_CATALOG: BuiltinMcpCatalogItem[] = [
-  {
-    id: 'automation',
-    name: 'automation',
-    displayName: '定时任务',
-    description: '创建、查看、更新、删除和立即运行 Proma 持久化定时任务。',
-    category: 'automation',
-    tools: [
-      { name: 'list_automations', description: '列出 Proma 定时任务。', readOnly: true },
-      { name: 'get_automation', description: '读取单个定时任务详情和运行记录。', readOnly: true },
-      { name: 'create_automation', description: '创建持久化定时任务。' },
-      { name: 'update_automation', description: '更新定时任务配置。' },
-      { name: 'delete_automation', description: '删除定时任务。' },
-      { name: 'run_automation_now', description: '立即运行一次定时任务。' },
-    ],
-  },
-  {
-    id: 'collaboration',
-    name: 'collaboration',
-    displayName: '协作子 Agent',
-    description: '创建、等待、读取和停止真实可见的 Proma 协作子 Agent 会话。',
-    category: 'collaboration',
-    tools: [
-      { name: 'list_available_agent_models', description: '列出当前渠道下可用于协作子 Agent 的模型。', readOnly: true },
-      { name: 'delegate_agent', description: '创建单个协作子 Agent 会话。' },
-      { name: 'delegate_agents', description: '批量创建协作子 Agent 会话。' },
-      { name: 'wait_for_delegations', description: '等待一组协作子会话完成。', readOnly: true },
-      { name: 'list_delegations', description: '列出当前父会话创建的子会话。', readOnly: true },
-      { name: 'get_delegation_results', description: '按委派 ID 读取子会话结果摘要。', readOnly: true },
-      { name: 'stop_delegation', description: '停止单个协作子会话。' },
-      { name: 'stop_delegations', description: '批量停止协作子会话。' },
-    ],
-  },
-  {
-    id: 'mem',
-    name: 'mem',
-    displayName: '记忆',
-    description: '通过 MemOS Cloud 检索和写入长期记忆。',
-    category: 'memory',
-    tools: [
-      { name: 'recall_memory', description: '检索用户长期记忆。', readOnly: true },
-      { name: 'add_memory', description: '写入一条长期记忆。' },
-    ],
-  },
-  {
-    id: 'nano-banana',
-    name: 'nano-banana',
-    displayName: 'Nano Banana 生图',
-    description: '通过 Gemini Image Generation 为 Agent 提供图片生成和编辑能力。',
-    category: 'media',
-    tools: [
-      { name: 'generate_image', description: '生成或编辑图片。' },
-    ],
-  },
-]
-
 function resolveAvailability(
-  item: BuiltinMcpCatalogItem,
+  item: BuiltinMcpDefinition,
   ctx: BuiltinMcpListContext,
 ): Pick<BuiltinMcpServerSummary, 'enabled' | 'available' | 'availabilityReason'> {
+  // 基础设施型（如 proma-cloud）：登录后始终注入，不受用户开关影响
+  if (item.toggleable === false) {
+    return { enabled: true, available: true }
+  }
+
   const userEnabled = isBuiltinMcpUserEnabled(item.id)
   if (!userEnabled) {
     return {
       enabled: false,
       available: false,
-      availabilityReason: '已手动关闭',
+      availabilityReason: isBuiltinMcpDefaultDisabled(item.id)
+        ? '默认关闭，可手动开启'
+        : '已手动关闭',
     }
   }
 
@@ -130,8 +75,14 @@ function resolveAvailability(
 }
 
 export function listBuiltinMcpServers(ctx: BuiltinMcpListContext = {}): BuiltinMcpServerSummary[] {
-  return BUILTIN_MCP_CATALOG.map((item) => ({
-    ...item,
+  return getBuiltinMcpDefinitions().map((item) => ({
+    id: item.id,
+    name: item.name,
+    displayName: item.displayName,
+    description: item.description,
+    category: item.category,
+    tools: item.tools,
+    toggleable: item.toggleable,
     ...resolveAvailability(item, ctx),
   }))
 }

GeoffBao/LuxCoder

(cherry picked from commit a006471229547b8b92a64c50723d09475e0057d9)

apps/electron/src/main/lib/builtin-mcp/catalog.ts
@@ -2,93 +2,38 @@
  * LuxAgents 内置 MCP 能力目录
  *
  * 这里只维护可展示的元数据和可用性判断，不负责运行时注入。
- * 这样前端能力摘要可以安全读取内置 MCP 列表，而不会引入 Agent 编排层循环依赖。
+ * 元数据本身来自 default-mcp.json（经 baseline 加载），本文件只在其上叠加
+ * 运行时可用性判断（API Key、工作区、登录态等）。这样前端能力摘要可以安全读取
+ * 内置 MCP 列表，而不会引入 Agent 编排层循环依赖。
  */
 
-import type { BuiltinMcpServerSummary, McpToolSummary } from '@luxagents/shared'
+import type { BuiltinMcpServerSummary } from '@luxagents/shared'
 import { getToolCredentials, getToolState } from '../chat-tool-config'
 import { getMemoryConfig } from '../memory-service'
-import { isBuiltinMcpUserEnabled } from './settings'
-
-interface BuiltinMcpCatalogItem {
-  id: string
-  name: string
-  displayName: string
-  description: string
-  category: BuiltinMcpServerSummary['category']
-  tools: McpToolSummary[]
-}
+import { getBuiltinMcpDefinitions, type BuiltinMcpDefinition } from './baseline'
+import { isBuiltinMcpDefaultDisabled, isBuiltinMcpUserEnabled } from './settings'
 
 interface BuiltinMcpListContext {
   workspaceSlug?: string
 }
 
-const BUILTIN_MCP_CATALOG: BuiltinMcpCatalogItem[] = [
-  {
-    id: 'automation',
-    name: 'automation',
-    displayName: '定时任务',
-    description: '创建、查看、更新、删除和立即运行 LuxAgents 持久化定时任务。',
-    category: 'automation',
-    tools: [
-      { name: 'list_automations', description: '列出 LuxAgents 定时任务。', readOnly: true },
-      { name: 'get_automation', description: '读取单个定时任务详情和运行记录。', readOnly: true },
-      { name: 'create_automation', description: '创建持久化定时任务。' },
-      { name: 'update_automation', description: '更新定时任务配置。' },
-      { name: 'delete_automation', description: '删除定时任务。' },
-      { name: 'run_automation_now', description: '立即运行一次定时任务。' },
-    ],
-  },
-  {
-    id: 'collaboration',
-    name: 'collaboration',
-    displayName: '协作子 Agent',
-    description: '创建、等待、读取和停止真实可见的 LuxAgents 协作子 Agent 会话。',
-    category: 'collaboration',
-    tools: [
-      { name: 'list_available_agent_models', description: '列出当前渠道下可用于协作子 Agent 的模型。', readOnly: true },
-      { name: 'delegate_agent', description: '创建单个协作子 Agent 会话。' },
-      { name: 'delegate_agents', description: '批量创建协作子 Agent 会话。' },
-      { name: 'wait_for_delegations', description: '等待一组协作子会话完成。', readOnly: true },
-      { name: 'list_delegations', description: '列出当前父会话创建的子会话。', readOnly: true },
-      { name: 'get_delegation_results', description: '按委派 ID 读取子会话结果摘要。', readOnly: true },
-      { name: 'stop_delegation', description: '停止单个协作子会话。' },
-      { name: 'stop_delegations', description: '批量停止协作子会话。' },
-    ],
-  },
-  {
-    id: 'mem',
-    name: 'mem',
-    displayName: '记忆',
-    description: '通过 MemOS Cloud 检索和写入长期记忆。',
-    category: 'memory',
-    tools: [
-      { name: 'recall_memory', description: '检索用户长期记忆。', readOnly: true },
-      { name: 'add_memory', description: '写入一条长期记忆。' },
-    ],
-  },
-  {
-    id: 'nano-banana',
-    name: 'nano-banana',
-    displayName: 'Nano Banana 生图',
-    description: '通过 Gemini Image Generation 为 Agent 提供图片生成和编辑能力。',
-    category: 'media',
-    tools: [
-      { name: 'generate_image', description: '生成或编辑图片。' },
-    ],
-  },
-]
-
 function resolveAvailability(
-  item: BuiltinMcpCatalogItem,
+  item: BuiltinMcpDefinition,
   ctx: BuiltinMcpListContext,
 ): Pick<BuiltinMcpServerSummary, 'enabled' | 'available' | 'availabilityReason'> {
+  // 基础设施型（如 luxagents-cloud）：登录后始终注入，不受用户开关影响
+  if (item.toggleable === false) {
+    return { enabled: true, available: true }
+  }
+
   const userEnabled = isBuiltinMcpUserEnabled(item.id)
   if (!userEnabled) {
     return {
       enabled: false,
       available: false,
-      availabilityReason: '已手动关闭',
+      availabilityReason: isBuiltinMcpDefaultDisabled(item.id)
+        ? '默认关闭，可手动开启'
+        : '已手动关闭',
     }
   }
 
@@ -130,8 +75,14 @@ function resolveAvailability(
 }
 
 export function listBuiltinMcpServers(ctx: BuiltinMcpListContext = {}): BuiltinMcpServerSummary[] {
-  return BUILTIN_MCP_CATALOG.map((item) => ({
-    ...item,
+  return getBuiltinMcpDefinitions().map((item) => ({
+    id: item.id,
+    name: item.name,
+    displayName: item.displayName,
+    description: item.description,
+    category: item.category,
+    tools: item.tools,
+    toggleable: item.toggleable,
     ...resolveAvailability(item, ctx),
   }))
 }