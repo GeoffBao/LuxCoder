shanyu910/Proma

feat(agent): add Codex fast mode to Pi runtime (#1190)
* feat(agent): add Codex fast mode to Pi runtime

* fix(agent): account for Codex fast mode priority tier

* fix(agent): preserve Codex fast mode service tier

apps/electron/src/main/lib/adapters/pi-agent-adapter.ts
@@ -23,6 +23,7 @@ import type {
   SDKUserMessageInput,
   TypedError,
 } from '@proma/shared'
+import { isCodexFastModeSupportedModel } from '@proma/shared'
 import {
   THINKING_SIGNATURE_ERROR_MESSAGE,
   THINKING_SIGNATURE_ERROR_TITLE,
@@ -48,6 +49,7 @@ import {
   type AgentRuntimeGuard,
 } from '../agent-runtime-guards'
 import { createPromaAgentsFilesOverride } from './pi-resource-loader-overrides'
+import { createCodexFastModeExtension, withCodexFastModeServiceTier } from './pi-codex-fast-mode'
 import { mergeRuntimeEnv, type AgentRuntimeEnv } from '../agent-runtime-env'
 import {
   convertPiMessage,
@@ -106,6 +108,8 @@ export interface PiAgentQueryOptions extends AgentQueryInput {
   runtimeEnv?: AgentRuntimeEnv
   /** 手动压缩请求：走 pi 原生 session.compact()，而非把 /compact 当普通 prompt 发给模型 */
   compactRequest?: boolean
+  /** ChatGPT Codex Fast Mode；仅 openai-codex 的受支持模型实际注入 priority service tier。 */
+  codexFastMode?: boolean
 }
 
 interface ActivePiSession {
@@ -1284,6 +1288,9 @@ export class PiAgentAdapter implements AgentProviderAdapter {
 
     try {
       const sdk = await import('@earendil-works/pi-coding-agent')
+      const piAi = input.codexFastMode && input.provider === 'openai-codex'
+        ? await import('@earendil-works/pi-ai/compat')
+        : undefined
       restorePiProxyEnv = applyPiProxySettingsForQuery(sdk, input)
       if (active.abortRequested) throw createAbortError()
 
@@ -1324,6 +1331,9 @@ export class PiAgentAdapter implements AgentProviderAdapter {
         additionalSkillPaths: input.additionalSkillPaths ?? [],
         skillsOverride: createPromaSkillsOverride(input.additionalSkillPaths),
         agentsFilesOverride: createPromaAgentsFilesOverride(),
+        ...(input.codexFastMode && input.provider === 'openai-codex' && {
+          extensionFactories: [createCodexFastModeExtension()],
+        }),
         systemPromptOverride: () => input.systemPrompt,
       })
       await resourceLoader.reload()
@@ -1349,6 +1359,31 @@ export class PiAgentAdapter implements AgentProviderAdapter {
         customTools,
       })
       session.agent.toolExecution = 'sequential'
+      if (piAi && input.codexFastMode && input.provider === 'openai-codex' && isCodexFastModeSupportedModel(input.model)) {
+        // Pi 的通用 streamSimple 会丢弃 provider 专属 serviceTier；这里直接走
+        // provider stream，确保 request body 与 usage.cost 都使用 priority tier。
+        session.agent.streamFn = async (requestModel, context, options) => {
+          const auth = await registry.getApiKeyAndHeaders(requestModel)
+          if (!auth.ok) throw new Error(auth.error)
+
+          const env = auth.env || options?.env ? { ...(auth.env ?? {}), ...(options?.env ?? {}) } : undefined
+          const retrySettings = settingsManager.getProviderRetrySettings()
+          const configuredTimeoutMs = settingsManager.getHttpIdleTimeoutMs()
+          const timeoutMs = options?.timeoutMs ?? retrySettings.timeoutMs ?? (configuredTimeoutMs === 0 ? 2_147_483_647 : configuredTimeoutMs)
+          const websocketConnectTimeoutMs = options?.websocketConnectTimeoutMs ?? settingsManager.getWebSocketConnectTimeoutMs()
+
+          return piAi.stream(requestModel, context, withCodexFastModeServiceTier({
+            ...options,
+            apiKey: auth.apiKey,
+            env,
+            timeoutMs,
+            websocketConnectTimeoutMs,
+            maxRetries: options?.maxRetries ?? retrySettings.maxRetries,
+            maxRetryDelayMs: options?.maxRetryDelayMs ?? retrySettings.maxRetryDelayMs,
+            headers: { ...auth.headers, ...options?.headers },
+          }))
+        }
+      }
       installRuntimeGuardHooks(session, runtimeGuard)
       active.session = session
       resolveActiveReady(active, session)

apps/electron/src/main/lib/adapters/pi-codex-fast-mode.ts
@@ -1,0 +1,45 @@
+import type { ExtensionAPI } from '@earendil-works/pi-coding-agent'
+import { isCodexFastModeSupportedModel } from '@proma/shared'
+
+type ProviderPayload = Record<string, unknown>
+
+export const CODEX_FAST_MODE_SERVICE_TIER = 'priority'
+
+function isProviderPayload(payload: unknown): payload is ProviderPayload {
+  return typeof payload === 'object' && payload !== null && !Array.isArray(payload)
+}
+
+/**
+ * 为符合条件的 Codex Responses 请求附加 OpenAI priority service tier。
+ *
+ * Pi 的扩展钩子位于实际请求 payload 构建之后，因此能同时覆盖首轮、
+ * tool continuation、队列续轮与恢复会话后的全部 provider request。
+ */
+export function injectCodexFastMode(payload: unknown): unknown {
+  if (!isProviderPayload(payload)) return payload
+  const modelId = typeof payload.model === 'string' ? payload.model : undefined
+  if (!isCodexFastModeSupportedModel(modelId)) return payload
+
+  // Fast Mode 是明确的用户选择，必须覆盖先加载的用户扩展写入的 tier，
+  // 避免 UI 显示 Fast、实际请求却退化为 default/flex。
+  return { ...payload, service_tier: CODEX_FAST_MODE_SERVICE_TIER }
+}
+
+/**
+ * Pi Agent 的公开 streamFn 类型只暴露通用 SimpleStreamOptions，未包含 provider
+ * 专属 serviceTier。运行时仍会把此字段完整传给 Codex Responses provider；保留它可
+ * 让 Pi 按 priority tier 正确计算 usage.cost 与 Proma 预算守卫。
+ */
+export function withCodexFastModeServiceTier<T extends object | undefined>(options: T): T & { serviceTier: typeof CODEX_FAST_MODE_SERVICE_TIER } {
+  return { ...options, serviceTier: CODEX_FAST_MODE_SERVICE_TIER } as T & { serviceTier: typeof CODEX_FAST_MODE_SERVICE_TIER }
+}
+
+/** Pi 内联扩展：Proma 不依赖用户安装第三方 Pi extension。 */
+export function createCodexFastModeExtension(): (pi: ExtensionAPI) => void {
+  return (pi) => {
+    pi.on('before_provider_request', (event) => {
+      const updatedPayload = injectCodexFastMode(event.payload)
+      return updatedPayload === event.payload ? undefined : updatedPayload
+    })
+  }
+}

apps/electron/src/renderer/components/agent/AgentView.tsx
@@ -114,7 +114,7 @@ import { draftSessionIdsAtom } from '@/atoms/draft-session-atoms'
 import { sendWithCmdEnterAtom } from '@/atoms/shortcut-atoms'
 import { useOpenPreview } from '@/components/diff/preview-opener'
 import type { AgentRuntime, AgentSendInput, AgentPendingFile, FileDialogLargeFile, ModelOption, SDKMessage, SDKUserMessage, ProviderType } from '@proma/shared'
-import { inferAgentSdkContextWindow, inferContextWindow, MAX_ATTACHMENT_SIZE } from '@proma/shared'
+import { inferAgentSdkContextWindow, inferContextWindow, isCodexFastModeSupportedModel, MAX_ATTACHMENT_SIZE } from '@proma/shared'
 import { fileToBase64, formatFileNames, getFileParentPath } from '@/lib/file-utils'
 import { buildQuotedSelectionBlock } from '@/lib/quoted-selection'
 import { createClipboardPendingFile, createClipboardTextDraft, makeUniqueAttachmentName } from '@/lib/clipboard-text-attachment'
@@ -654,6 +654,11 @@ export function AgentView({ sessionId }: { sessionId: string }): React.ReactElem
     () => globalChannels.find((c) => c.id === agentChannelId)?.provider,
     [globalChannels, agentChannelId],
   )
+  const isCodexFastModeAvailable = hasSessionMeta
+    && sessionAgentRuntime === 'pi'
+    && agentChannelProvider === 'openai-codex'
+    && isCodexFastModeSupportedModel(agentModelId ?? undefined)
+  const codexFastModeEnabled = isCodexFastModeAvailable && sessionMeta?.codexFastMode === true
 
   // 检查 Agent 渠道列表中是否存在可用的模型（渠道 enabled + 模型 enabled）
   const hasAvailableModel = React.useMemo(() => {
@@ -1840,6 +1845,25 @@ export function AgentView({ sessionId }: { sessionId: string }): React.ReactElem
     streaming,
   ])
 
+  const handleCodexFastModeChange = React.useCallback(async (): Promise<void> => {
+    if (!isCodexFastModeAvailable || streaming || backgroundWaiting || !sessionMeta) return
+
+    const previousSessionMeta = sessionMeta
+    const nextEnabled = !codexFastModeEnabled
+    setAgentSessions((prev) => prev.map((item) => (
+      item.id === sessionId ? { ...item, codexFastMode: nextEnabled, updatedAt: Date.now() } : item
+    )))
+
+    try {
+      const updated = await window.electronAPI.updateSessionCodexFastMode(sessionId, nextEnabled)
+      setAgentSessions((prev) => prev.map((item) => item.id === sessionId ? updated : item))
+    } catch (error) {
+      console.error('[AgentView] 切换 Codex Fast Mode 失败:', error)
+      setAgentSessions((prev) => prev.map((item) => item.id === sessionId ? previousSessionMeta : item))
+      toast.error('快速模式切换失败', { description: getErrorMessage(error) })
+    }
+  }, [backgroundWaiting, codexFastModeEnabled, isCodexFastModeAvailable, sessionId, sessionMeta, setAgentSessions, streaming])
+
   /** 构建 externalSelectedModel 给 ModelSelector */
   const computedSelectedModel = React.useMemo(() => {
     if (!agentChannelId || !agentModelId) return null
@@ -2508,6 +2532,33 @@ export function AgentView({ sessionId }: { sessionId: string }): React.ReactElem
         />
       ),
     },
+    ...(isCodexFastModeAvailable ? [{
+      key: 'codex-fast-mode',
+      node: (
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button
+              type="button"
+              variant="ghost"
+              className={cn(
+                'h-8 min-w-10 rounded-md px-2 text-xs font-medium transition-transform active:scale-[0.96]',
+                codexFastModeEnabled
+                  ? 'bg-primary/10 text-primary hover:bg-primary/15 hover:text-primary'
+                  : 'text-foreground/60 hover:bg-muted/50 hover:text-foreground',
+              )}
+              onClick={handleCodexFastModeChange}
+              disabled={streaming || backgroundWaiting}
+              aria-pressed={codexFastModeEnabled}
+            >
+              {codexFastModeEnabled ? 'Fast' : '标准'}
+            </Button>
+          </TooltipTrigger>
+          <TooltipContent side="top">
+            <p>{codexFastModeEnabled ? '已启用快速模式：优先响应，消耗更多额度' : '开启快速模式：优先响应，消耗更多额度'}</p>
+          </TooltipContent>
+        </Tooltip>
+      ),
+    }] : []),
     {
       key: 'runtime',
       node: (
@@ -2610,6 +2661,9 @@ export function AgentView({ sessionId }: { sessionId: string }): React.ReactElem
     agentChannelId,
     planQuotaChannelId,
     planQuotaChannelUpdatedAt,
+    isCodexFastModeAvailable,
+    codexFastModeEnabled,
+    handleCodexFastModeChange,
     agentModelId,
     handleModelSelect,
     sessionAgentRuntime,

GeoffBao/LuxCoder

(cherry picked from commit 67b27fc6f78d231c479e970bc65ff2c6e8fe511f)

apps/electron/src/main/lib/adapters/pi-agent-adapter.ts
@@ -23,6 +23,7 @@ import type {
   SDKUserMessageInput,
   TypedError,
 } from '@luxagents/shared'
+import { isCodexFastModeSupportedModel } from '@luxagents/shared'
 import {
   THINKING_SIGNATURE_ERROR_MESSAGE,
   THINKING_SIGNATURE_ERROR_TITLE,
@@ -48,6 +49,7 @@ import {
   type AgentRuntimeGuard,
 } from '../agent-runtime-guards'
 import { createPromaAgentsFilesOverride } from './pi-resource-loader-overrides'
+import { createCodexFastModeExtension, withCodexFastModeServiceTier } from './pi-codex-fast-mode'
 import { mergeRuntimeEnv, type AgentRuntimeEnv } from '../agent-runtime-env'
 import {
   convertPiMessage,
@@ -106,6 +108,8 @@ export interface PiAgentQueryOptions extends AgentQueryInput {
   runtimeEnv?: AgentRuntimeEnv
   /** 手动压缩请求：走 pi 原生 session.compact()，而非把 /compact 当普通 prompt 发给模型 */
   compactRequest?: boolean
+  /** ChatGPT Codex Fast Mode；仅 openai-codex 的受支持模型实际注入 priority service tier。 */
+  codexFastMode?: boolean
 }
 
 interface ActivePiSession {
@@ -1284,6 +1288,9 @@ export class PiAgentAdapter implements AgentProviderAdapter {
 
     try {
       const sdk = await import('@earendil-works/pi-coding-agent')
+      const piAi = input.codexFastMode && input.provider === 'openai-codex'
+        ? await import('@earendil-works/pi-ai/compat')
+        : undefined
       restorePiProxyEnv = applyPiProxySettingsForQuery(sdk, input)
       if (active.abortRequested) throw createAbortError()
 
@@ -1324,6 +1331,9 @@ export class PiAgentAdapter implements AgentProviderAdapter {
         additionalSkillPaths: input.additionalSkillPaths ?? [],
         skillsOverride: createPromaSkillsOverride(input.additionalSkillPaths),
         agentsFilesOverride: createPromaAgentsFilesOverride(),
+        ...(input.codexFastMode && input.provider === 'openai-codex' && {
+          extensionFactories: [createCodexFastModeExtension()],
+        }),
         systemPromptOverride: () => input.systemPrompt,
       })
       await resourceLoader.reload()
@@ -1349,6 +1359,31 @@ export class PiAgentAdapter implements AgentProviderAdapter {
         customTools,
       })
       session.agent.toolExecution = 'sequential'
+      if (piAi && input.codexFastMode && input.provider === 'openai-codex' && isCodexFastModeSupportedModel(input.model)) {
+        // Pi 的通用 streamSimple 会丢弃 provider 专属 serviceTier；这里直接走
+        // provider stream，确保 request body 与 usage.cost 都使用 priority tier。
+        session.agent.streamFn = async (requestModel, context, options) => {
+          const auth = await registry.getApiKeyAndHeaders(requestModel)
+          if (!auth.ok) throw new Error(auth.error)
+
+          const env = auth.env || options?.env ? { ...(auth.env ?? {}), ...(options?.env ?? {}) } : undefined
+          const retrySettings = settingsManager.getProviderRetrySettings()
+          const configuredTimeoutMs = settingsManager.getHttpIdleTimeoutMs()
+          const timeoutMs = options?.timeoutMs ?? retrySettings.timeoutMs ?? (configuredTimeoutMs === 0 ? 2_147_483_647 : configuredTimeoutMs)
+          const websocketConnectTimeoutMs = options?.websocketConnectTimeoutMs ?? settingsManager.getWebSocketConnectTimeoutMs()
+
+          return piAi.stream(requestModel, context, withCodexFastModeServiceTier({
+            ...options,
+            apiKey: auth.apiKey,
+            env,
+            timeoutMs,
+            websocketConnectTimeoutMs,
+            maxRetries: options?.maxRetries ?? retrySettings.maxRetries,
+            maxRetryDelayMs: options?.maxRetryDelayMs ?? retrySettings.maxRetryDelayMs,
+            headers: { ...auth.headers, ...options?.headers },
+          }))
+        }
+      }
       installRuntimeGuardHooks(session, runtimeGuard)
       active.session = session
       resolveActiveReady(active, session)

apps/electron/src/main/lib/adapters/pi-codex-fast-mode.ts
@@ -1,0 +1,45 @@
+import type { ExtensionAPI } from '@earendil-works/pi-coding-agent'
+import { isCodexFastModeSupportedModel } from '@luxagents/shared'
+
+type ProviderPayload = Record<string, unknown>
+
+export const CODEX_FAST_MODE_SERVICE_TIER = 'priority'
+
+function isProviderPayload(payload: unknown): payload is ProviderPayload {
+  return typeof payload === 'object' && payload !== null && !Array.isArray(payload)
+}
+
+/**
+ * 为符合条件的 Codex Responses 请求附加 OpenAI priority service tier。
+ *
+ * Pi 的扩展钩子位于实际请求 payload 构建之后，因此能同时覆盖首轮、
+ * tool continuation、队列续轮与恢复会话后的全部 provider request。
+ */
+export function injectCodexFastMode(payload: unknown): unknown {
+  if (!isProviderPayload(payload)) return payload
+  const modelId = typeof payload.model === 'string' ? payload.model : undefined
+  if (!isCodexFastModeSupportedModel(modelId)) return payload
+
+  // Fast Mode 是明确的用户选择，必须覆盖先加载的用户扩展写入的 tier，
+  // 避免 UI 显示 Fast、实际请求却退化为 default/flex。
+  return { ...payload, service_tier: CODEX_FAST_MODE_SERVICE_TIER }
+}
+
+/**
+ * Pi Agent 的公开 streamFn 类型只暴露通用 SimpleStreamOptions，未包含 provider
+ * 专属 serviceTier。运行时仍会把此字段完整传给 Codex Responses provider；保留它可
+ * 让 Pi 按 priority tier 正确计算 usage.cost 与 Proma 预算守卫。
+ */
+export function withCodexFastModeServiceTier<T extends object | undefined>(options: T): T & { serviceTier: typeof CODEX_FAST_MODE_SERVICE_TIER } {
+  return { ...options, serviceTier: CODEX_FAST_MODE_SERVICE_TIER } as T & { serviceTier: typeof CODEX_FAST_MODE_SERVICE_TIER }
+}
+
+/** Pi 内联扩展：Proma 不依赖用户安装第三方 Pi extension。 */
+export function createCodexFastModeExtension(): (pi: ExtensionAPI) => void {
+  return (pi) => {
+    pi.on('before_provider_request', (event) => {
+      const updatedPayload = injectCodexFastMode(event.payload)
+      return updatedPayload === event.payload ? undefined : updatedPayload
+    })
+  }
+}

apps/electron/src/renderer/components/agent/AgentView.tsx
@@ -115,7 +115,7 @@ import { draftSessionIdsAtom } from '@/atoms/draft-session-atoms'
 import { sendWithCmdEnterAtom } from '@/atoms/shortcut-atoms'
 import { useOpenPreview } from '@/components/diff/preview-opener'
 import type { AgentRuntime, AgentSendInput, AgentPendingFile, FileDialogLargeFile, ModelOption, SDKMessage, SDKUserMessage, ProviderType } from '@luxagents/shared'
-import { inferAgentSdkContextWindow, inferContextWindow, MAX_ATTACHMENT_SIZE } from '@luxagents/shared'
+import { inferAgentSdkContextWindow, inferContextWindow, isCodexFastModeSupportedModel, MAX_ATTACHMENT_SIZE } from '@luxagents/shared'
 import { fileToBase64, formatFileNames, getFileParentPath } from '@/lib/file-utils'
 import { buildQuotedSelectionBlock } from '@/lib/quoted-selection'
 import { createClipboardPendingFile, createClipboardTextDraft, makeUniqueAttachmentName } from '@/lib/clipboard-text-attachment'
@@ -655,6 +655,11 @@ export function AgentView({ sessionId }: { sessionId: string }): React.ReactElem
     () => globalChannels.find((c) => c.id === agentChannelId)?.provider,
     [globalChannels, agentChannelId],
   )
+  const isCodexFastModeAvailable = hasSessionMeta
+    && sessionAgentRuntime === 'pi'
+    && agentChannelProvider === 'openai-codex'
+    && isCodexFastModeSupportedModel(agentModelId ?? undefined)
+  const codexFastModeEnabled = isCodexFastModeAvailable && sessionMeta?.codexFastMode === true
 
   // 检查 Agent 渠道列表中是否存在可用的模型（渠道 enabled + 模型 enabled）
   const hasAvailableModel = React.useMemo(() => {
@@ -1841,6 +1846,25 @@ export function AgentView({ sessionId }: { sessionId: string }): React.ReactElem
     streaming,
   ])
 
+  const handleCodexFastModeChange = React.useCallback(async (): Promise<void> => {
+    if (!isCodexFastModeAvailable || streaming || backgroundWaiting || !sessionMeta) return
+
+    const previousSessionMeta = sessionMeta
+    const nextEnabled = !codexFastModeEnabled
+    setAgentSessions((prev) => prev.map((item) => (
+      item.id === sessionId ? { ...item, codexFastMode: nextEnabled, updatedAt: Date.now() } : item
+    )))
+
+    try {
+      const updated = await window.electronAPI.updateSessionCodexFastMode(sessionId, nextEnabled)
+      setAgentSessions((prev) => prev.map((item) => item.id === sessionId ? updated : item))
+    } catch (error) {
+      console.error('[AgentView] 切换 Codex Fast Mode 失败:', error)
+      setAgentSessions((prev) => prev.map((item) => item.id === sessionId ? previousSessionMeta : item))
+      toast.error('快速模式切换失败', { description: getErrorMessage(error) })
+    }
+  }, [backgroundWaiting, codexFastModeEnabled, isCodexFastModeAvailable, sessionId, sessionMeta, setAgentSessions, streaming])
+
   /** 构建 externalSelectedModel 给 ModelSelector */
   const computedSelectedModel = React.useMemo(() => {
     if (!agentChannelId || !agentModelId) return null
@@ -2509,6 +2533,33 @@ export function AgentView({ sessionId }: { sessionId: string }): React.ReactElem
         />
       ),
     },
+    ...(isCodexFastModeAvailable ? [{
+      key: 'codex-fast-mode',
+      node: (
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button
+              type="button"
+              variant="ghost"
+              className={cn(
+                'h-8 min-w-10 rounded-md px-2 text-xs font-medium transition-transform active:scale-[0.96]',
+                codexFastModeEnabled
+                  ? 'bg-primary/10 text-primary hover:bg-primary/15 hover:text-primary'
+                  : 'text-foreground/60 hover:bg-muted/50 hover:text-foreground',
+              )}
+              onClick={handleCodexFastModeChange}
+              disabled={streaming || backgroundWaiting}
+              aria-pressed={codexFastModeEnabled}
+            >
+              {codexFastModeEnabled ? 'Fast' : '标准'}
+            </Button>
+          </TooltipTrigger>
+          <TooltipContent side="top">
+            <p>{codexFastModeEnabled ? '已启用快速模式：优先响应，消耗更多额度' : '开启快速模式：优先响应，消耗更多额度'}</p>
+          </TooltipContent>
+        </Tooltip>
+      ),
+    }] : []),
     {
       key: 'runtime',
       node: (
@@ -2611,6 +2662,9 @@ export function AgentView({ sessionId }: { sessionId: string }): React.ReactElem
     agentChannelId,
     planQuotaChannelId,
     planQuotaChannelUpdatedAt,
+    isCodexFastModeAvailable,
+    codexFastModeEnabled,
+    handleCodexFastModeChange,
     agentModelId,
     handleModelSelect,
     sessionAgentRuntime,