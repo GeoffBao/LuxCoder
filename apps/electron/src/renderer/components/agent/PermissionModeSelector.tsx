jacky1991/Proma

fix(agent): 同步计划阶段权限图标 (#1009)

apps/electron/src/renderer/components/agent/PermissionModeSelector.tsx
@@ -14,7 +14,7 @@ import { Button } from '@/components/ui/button'
 import { agentPermissionModeMapAtom, agentDefaultPermissionModeAtom, sessionPersistedPermissionModeAtom, sessionExistsAtom, agentPlanModeSessionsAtom } from '@/atoms/agent-atoms'
 import type { PromaPermissionMode } from '@proma/shared'
 import { PROMA_PERMISSION_MODE_CONFIG, PROMA_PERMISSION_MODE_ORDER } from '@proma/shared'
-import { updatePlanModeSessionSet } from '@/lib/agent-plan-mode'
+import { getDisplayedPermissionMode, updatePlanModeSessionSet } from '@/lib/agent-plan-mode'
 import { inputToolbarButtonClass } from '@/components/ai-elements/input-toolbar-styles'
 
 const MODE_ICONS: Record<PromaPermissionMode, React.ComponentType<{ className?: string }>> = {
@@ -30,9 +30,12 @@ interface PermissionModeSelectorProps {
 export function PermissionModeSelector({ sessionId }: PermissionModeSelectorProps): React.ReactElement | null {
   const [modeMap, setModeMap] = useAtom(agentPermissionModeMapAtom)
   const setPlanModeSessions = useSetAtom(agentPlanModeSessionsAtom)
+  const planModeSessions = useAtomValue(agentPlanModeSessionsAtom)
   const defaultMode = useAtomValue(agentDefaultPermissionModeAtom)
   const persistedSessionMode = useAtomValue(sessionPersistedPermissionModeAtom(sessionId))
   const mode = modeMap.get(sessionId) ?? persistedSessionMode ?? defaultMode
+  const planModeActive = planModeSessions.has(sessionId)
+  const displayMode = getDisplayedPermissionMode(mode, planModeActive)
   const sessionExistsInList = useAtomValue(sessionExistsAtom(sessionId))
 
   // 初始化：如果当前 session 不在 Map 中，按以下优先级读回：
@@ -52,10 +55,11 @@ export function PermissionModeSelector({ sessionId }: PermissionModeSelectorProp
 
   /** 循环切换模式 */
   const cycleMode = React.useCallback(async () => {
-    const currentIndex = PROMA_PERMISSION_MODE_ORDER.indexOf(mode)
+    const currentIndex = PROMA_PERMISSION_MODE_ORDER.indexOf(displayMode)
     const nextIndex = (currentIndex + 1) % PROMA_PERMISSION_MODE_ORDER.length
     const nextMode = PROMA_PERMISSION_MODE_ORDER[nextIndex]!
     const prevMode = mode
+    const prevPlanModeActive = planModeActive
 
     // 乐观更新当前 session 的模式
     setModeMap((prev: Map<string, PromaPermissionMode>) => {
@@ -78,13 +82,13 @@ export function PermissionModeSelector({ sessionId }: PermissionModeSelectorProp
         return next
       })
       setPlanModeSessions((prev: Set<string>) =>
-        updatePlanModeSessionSet(prev, sessionId, prevMode === 'plan')
+        updatePlanModeSessionSet(prev, sessionId, prevPlanModeActive || prevMode === 'plan')
       )
     }
-  }, [mode, sessionId, setModeMap, setPlanModeSessions])
+  }, [displayMode, mode, planModeActive, sessionId, setModeMap, setPlanModeSessions])
 
-  const config = PROMA_PERMISSION_MODE_CONFIG[mode]
-  const Icon = MODE_ICONS[mode]
+  const config = PROMA_PERMISSION_MODE_CONFIG[displayMode]
+  const Icon = MODE_ICONS[displayMode]
 
   return (
     <TooltipProvider delayDuration={300}>

apps/electron/src/renderer/lib/agent-plan-mode.ts
@@ -1,4 +1,4 @@
-import type { AgentPlanModeChangeSource } from '@proma/shared'
+import type { AgentPlanModeChangeSource, PromaPermissionMode } from '@proma/shared'
 
 export interface PlanModeChange {
   active: boolean
@@ -32,4 +32,12 @@ export function updatePlanModeSessionSet(
   const next = new Set(prev)
   next.delete(sessionId)
   return next
+}
+
+/** 输入区处于计划阶段时，权限按钮也优先展示计划模式图标。 */
+export function getDisplayedPermissionMode(
+  permissionMode: PromaPermissionMode,
+  planModeActive: boolean,
+): PromaPermissionMode {
+  return planModeActive ? 'plan' : permissionMode
 }

GeoffBao/LuxCoder

(cherry picked from commit b838b69ce22ff492c3c4447c544689da63c552a5)

apps/electron/src/renderer/components/agent/PermissionModeSelector.tsx
@@ -14,7 +14,7 @@ import { Button } from '@/components/ui/button'
 import { agentPermissionModeMapAtom, agentDefaultPermissionModeAtom, sessionPersistedPermissionModeAtom, sessionExistsAtom, agentPlanModeSessionsAtom } from '@/atoms/agent-atoms'
 import type { LuxAgentsPermissionMode } from '@luxagents/shared'
 import { LUXAGENTS_PERMISSION_MODE_CONFIG, LUXAGENTS_PERMISSION_MODE_ORDER } from '@luxagents/shared'
-import { updatePlanModeSessionSet } from '@/lib/agent-plan-mode'
+import { getDisplayedPermissionMode, updatePlanModeSessionSet } from '@/lib/agent-plan-mode'
 import { inputToolbarButtonClass } from '@/components/ai-elements/input-toolbar-styles'
 
 const MODE_ICONS: Record<LuxAgentsPermissionMode, React.ComponentType<{ className?: string }>> = {
@@ -30,9 +30,12 @@ interface PermissionModeSelectorProps {
 export function PermissionModeSelector({ sessionId }: PermissionModeSelectorProps): React.ReactElement | null {
   const [modeMap, setModeMap] = useAtom(agentPermissionModeMapAtom)
   const setPlanModeSessions = useSetAtom(agentPlanModeSessionsAtom)
+  const planModeSessions = useAtomValue(agentPlanModeSessionsAtom)
   const defaultMode = useAtomValue(agentDefaultPermissionModeAtom)
   const persistedSessionMode = useAtomValue(sessionPersistedPermissionModeAtom(sessionId))
   const mode = modeMap.get(sessionId) ?? persistedSessionMode ?? defaultMode
+  const planModeActive = planModeSessions.has(sessionId)
+  const displayMode = getDisplayedPermissionMode(mode, planModeActive)
   const sessionExistsInList = useAtomValue(sessionExistsAtom(sessionId))
 
   // 初始化：如果当前 session 不在 Map 中，按以下优先级读回：
@@ -52,10 +55,11 @@ export function PermissionModeSelector({ sessionId }: PermissionModeSelectorProp
 
   /** 循环切换模式 */
   const cycleMode = React.useCallback(async () => {
-    const currentIndex = LUXAGENTS_PERMISSION_MODE_ORDER.indexOf(mode)
+    const currentIndex = LUXAGENTS_PERMISSION_MODE_ORDER.indexOf(displayMode)
     const nextIndex = (currentIndex + 1) % LUXAGENTS_PERMISSION_MODE_ORDER.length
     const nextMode = LUXAGENTS_PERMISSION_MODE_ORDER[nextIndex]!
     const prevMode = mode
+    const prevPlanModeActive = planModeActive
 
     // 乐观更新当前 session 的模式
     setModeMap((prev: Map<string, LuxAgentsPermissionMode>) => {
@@ -78,13 +82,13 @@ export function PermissionModeSelector({ sessionId }: PermissionModeSelectorProp
         return next
       })
       setPlanModeSessions((prev: Set<string>) =>
-        updatePlanModeSessionSet(prev, sessionId, prevMode === 'plan')
+        updatePlanModeSessionSet(prev, sessionId, prevPlanModeActive || prevMode === 'plan')
       )
     }
-  }, [mode, sessionId, setModeMap, setPlanModeSessions])
+  }, [displayMode, mode, planModeActive, sessionId, setModeMap, setPlanModeSessions])
 
-  const config = LUXAGENTS_PERMISSION_MODE_CONFIG[mode]
-  const Icon = MODE_ICONS[mode]
+  const config = LUXAGENTS_PERMISSION_MODE_CONFIG[displayMode]
+  const Icon = MODE_ICONS[displayMode]
 
   return (
     <TooltipProvider delayDuration={300}>

apps/electron/src/renderer/lib/agent-plan-mode.ts
@@ -1,4 +1,4 @@
-import type { AgentPlanModeChangeSource } from '@luxagents/shared'
+import type { AgentPlanModeChangeSource, LuxAgentsPermissionMode } from '@luxagents/shared'
 
 export interface PlanModeChange {
   active: boolean
@@ -32,4 +32,12 @@ export function updatePlanModeSessionSet(
   const next = new Set(prev)
   next.delete(sessionId)
   return next
+}
+
+/** 输入区处于计划阶段时，权限按钮也优先展示计划模式图标。 */
+export function getDisplayedPermissionMode(
+  permissionMode: LuxAgentsPermissionMode,
+  planModeActive: boolean,
+): LuxAgentsPermissionMode {
+  return planModeActive ? 'plan' : permissionMode
 }