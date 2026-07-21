shanyu910/Proma

feat(sidebar): 左侧边栏支持拖拽调整宽度 (#1094)
左侧会话列表中，条目常带有工作区 badge、子任务缩进、子任务数量
标记及活跃时间等信息，导致会话标题被严重挤压、难以辨认。

本次改动允许用户向右拖拽扩展左侧边栏宽度（300~420px），拖拽手
柄在侧边栏右缘（16px 宽，hover 有微弱高亮），宽度值持久化到
localStorage。折叠态自动隐藏手柄。

实现参照右侧面板已有的 resize 逻辑（rAF 节流 + clamp 范围 +
atomWithStorage 持久化），拖拽中禁用 CSS transition 保证即时
跟手响应。

apps/electron/src/renderer/atoms/sidebar-atoms.ts
@@ -18,3 +18,9 @@ export const projectListHeightAtom = atomWithStorage<number>(
   'proma-workspace-list-height',
   120,
 )
+
+/** 左侧边栏宽度（px），用户可拖拽调整，持久化到 localStorage */
+export const leftSidebarWidthAtom = atomWithStorage<number>(
+  'proma-left-sidebar-width',
+  300,
+)

GeoffBao/LuxCoder

(cherry picked from commit 390b37f3a19c755487254614a54be320cf10db99)

apps/electron/src/renderer/atoms/sidebar-atoms.ts
@@ -18,3 +18,9 @@ export const projectListHeightAtom = atomWithStorage<number>(
   'luxagents-workspace-list-height',
   120,
 )
+
+/** 左侧边栏宽度（px），用户可拖拽调整，持久化到 localStorage */
+export const leftSidebarWidthAtom = atomWithStorage<number>(
+  'luxagents-left-sidebar-width',
+  300,
+)