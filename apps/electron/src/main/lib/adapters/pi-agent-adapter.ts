jacky1991/Proma

feat: add session OpenAI reasoning controls (#1201)
* feat: add session OpenAI reasoning controls

* fix: guard OpenAI reasoning controls

* fix: limit reasoning controls to supported models

* fix: scope session reasoning to OpenAI providers

* fix: exclude chat-only OpenAI models from reasoning

apps/electron/src/main/lib/adapters/pi-agent-adapter.ts
@@ -24,7 +24,7 @@ import type {
   SDKUserMessageInput,
   TypedError,
 } from '@proma/shared'
-import { isCodexFastModeSupportedModel } from '@proma/shared'
+import { isCodexFastModeSupportedModel, isOpenAIReasoningSupportedModel } from '@proma/shared'
 import {
   THINKING_SIGNATURE_ERROR_MESSAGE,
   THINKING_SIGNATURE_ERROR_TITLE,
@@ -50,7 +50,7 @@ import {
   type AgentRuntimeGuard,
 } from '../agent-runtime-guards'
 import { createPromaAgentsFilesOverride } from './pi-resource-loader-overrides'
-import { createCodexFastModeExtension, withCodexFastModeServiceTier } from './pi-codex-fast-mode'
+import { createCodexRequestSettingsExtension, withCodexFastModeServiceTier } from './pi-codex-request-settings'
 import { mergeRuntimeEnv, type AgentRuntimeEnv } from '../agent-runtime-env'
 import {
   convertPiMessage,
@@ -118,6 +118,8 @@ export interface PiAgentQueryOptions extends AgentQueryInput {
   compactRequest?: boolean
   /** ChatGPT Codex Fast Mode；仅 openai-codex 的受支持模型实际注入 priority service tier。 */
   codexFastMode?: boolean
+  /** 会话级 OpenAI（Codex OAuth / Responses API）思考深度。 */
+  openAIThinkingLevel?: AgentThinkingLevel
 }
 
 interface ActivePiSession {
@@ -1263,8 +1265,13 @@ export class PiAgentAdapter implements AgentProviderAdapter {
         additionalSkillPaths: input.additionalSkillPaths ?? [],
         skillsOverride: createPromaSkillsOverride(input.additionalSkillPaths),
         agentsFilesOverride: createPromaAgentsFilesOverride(),
-        ...(input.codexFastMode && input.provider === 'openai-codex' && {
-          extensionFactories: [createCodexFastModeExtension()],
+        ...((input.provider === 'openai-codex' || input.provider === 'openai-responses')
+          && model.reasoning
+          && isOpenAIReasoningSupportedModel(input.model) && {
+            extensionFactories: [createCodexRequestSettingsExtension({
+            fastMode: input.codexFastMode,
+            thinkingLevel: input.openAIThinkingLevel,
+          })],
         }),
         systemPromptOverride: () => input.systemPrompt,
       })

apps/electron/src/main/lib/adapters/pi-codex-fast-mode.test.ts
@@ -1,7 +1,21 @@
 import { describe, expect, test } from 'bun:test'
-import { injectCodexFastMode, withCodexFastModeServiceTier } from './pi-codex-fast-mode'
+import {
+  injectCodexFastMode,
+  injectOpenAIThinkingLevel,
+  withCodexFastModeServiceTier,
+} from './pi-codex-request-settings'
+import { isOpenAIReasoningSupportedModel } from '@proma/shared'
 
-describe('Pi Codex Fast Mode', () => {
+describe('Pi Codex request settings', () => {
+  test('Given OpenAI model IDs When checking reasoning support Then excludes non-reasoning GPT-4 models', () => {
+    expect(isOpenAIReasoningSupportedModel('gpt-5.6')).toBe(true)
+    expect(isOpenAIReasoningSupportedModel('o4-mini')).toBe(true)
+    expect(isOpenAIReasoningSupportedModel('gpt-4o')).toBe(false)
+    expect(isOpenAIReasoningSupportedModel('gpt-4.1')).toBe(false)
+    expect(isOpenAIReasoningSupportedModel('gpt-5-chat-latest')).toBe(false)
+    expect(isOpenAIReasoningSupportedModel('gpt-5.3-chat-latest')).toBe(false)
+  })
+
   test.each(['gpt-5.4', 'gpt-5.5', 'gpt-5.6-sol', 'gpt-5.6-terra', 'gpt-5.6-luna'])(
     'Given supported %s When injecting Then requests priority tier',
     (model) => {
@@ -25,6 +39,30 @@ describe('Pi Codex Fast Mode', () => {
     expect(withCodexFastModeServiceTier({ transport: 'websocket' })).toEqual({
       transport: 'websocket',
       serviceTier: 'priority',
+    })
+  })
+
+  test('Given thinking is disabled When injecting Then explicitly sends none', () => {
+    expect(injectOpenAIThinkingLevel({ model: 'gpt-5.5' }, { thinkingLevel: 'off' })).toEqual({
+      model: 'gpt-5.5',
+      reasoning: { effort: 'none' },
+    })
+  })
+
+  test('Given direct Codex provider stream When injecting Then fills the selected non-off effort', () => {
+    expect(injectOpenAIThinkingLevel({ model: 'gpt-5.5' }, { thinkingLevel: 'high' })).toEqual({
+      model: 'gpt-5.5',
+      reasoning: { effort: 'high' },
+    })
+  })
+
+  test('Given an upstream reasoning mode When injecting Then strips mode from the request', () => {
+    expect(injectOpenAIThinkingLevel({
+      model: 'gpt-5.6',
+      reasoning: { effort: 'high', mode: 'pro', summary: 'auto' },
+    }, { thinkingLevel: 'high' })).toEqual({
+      model: 'gpt-5.6',
+      reasoning: { effort: 'high', summary: 'auto' },
     })
   })
 

apps/electron/src/main/lib/agent-thinking-level.ts
@@ -1,0 +1,22 @@
+import type { AgentSessionMeta, AgentThinkingLevel, ProviderType } from '@proma/shared'
+import type { AppSettings } from '../../types'
+
+type ThinkingSettings = Pick<AppSettings, 'agentThinking' | 'agentEffort'>
+type ThinkingSessionMeta = Pick<AgentSessionMeta, 'openAIThinkingLevel'>
+
+function isOpenAIReasoningProvider(provider: ProviderType | undefined): boolean {
+  return provider === 'openai-codex' || provider === 'openai-responses'
+}
+
+export function resolvePiThinkingLevel(
+  settings: ThinkingSettings,
+  sessionMeta: ThinkingSessionMeta | undefined,
+  provider: ProviderType | undefined,
+): AgentThinkingLevel {
+  if (isOpenAIReasoningProvider(provider) && sessionMeta?.openAIThinkingLevel) {
+    return sessionMeta.openAIThinkingLevel
+  }
+  if (settings.agentThinking?.type === 'disabled') return 'off'
+  if (settings.agentEffort === 'max') return 'xhigh'
+  return settings.agentEffort ?? (settings.agentThinking ? 'high' : 'off')
+}

GeoffBao/LuxCoder

(cherry picked from commit f279d22ca3d572dc26ec706572b8360f6584f863)

apps/electron/src/main/lib/adapters/pi-agent-adapter.ts
@@ -24,7 +24,7 @@ import type {
   SDKUserMessageInput,
   TypedError,
 } from '@luxagents/shared'
-import { isCodexFastModeSupportedModel } from '@luxagents/shared'
+import { isCodexFastModeSupportedModel, isOpenAIReasoningSupportedModel } from '@luxagents/shared'
 import {
   THINKING_SIGNATURE_ERROR_MESSAGE,
   THINKING_SIGNATURE_ERROR_TITLE,
@@ -50,7 +50,7 @@ import {
   type AgentRuntimeGuard,
 } from '../agent-runtime-guards'
 import { createPromaAgentsFilesOverride } from './pi-resource-loader-overrides'
-import { createCodexFastModeExtension, withCodexFastModeServiceTier } from './pi-codex-fast-mode'
+import { createCodexRequestSettingsExtension, withCodexFastModeServiceTier } from './pi-codex-request-settings'
 import { mergeRuntimeEnv, type AgentRuntimeEnv } from '../agent-runtime-env'
 import {
   convertPiMessage,
@@ -118,6 +118,8 @@ export interface PiAgentQueryOptions extends AgentQueryInput {
   compactRequest?: boolean
   /** ChatGPT Codex Fast Mode；仅 openai-codex 的受支持模型实际注入 priority service tier。 */
   codexFastMode?: boolean
+  /** 会话级 OpenAI（Codex OAuth / Responses API）思考深度。 */
+  openAIThinkingLevel?: AgentThinkingLevel
 }
 
 interface ActivePiSession {
@@ -1263,8 +1265,13 @@ export class PiAgentAdapter implements AgentProviderAdapter {
         additionalSkillPaths: input.additionalSkillPaths ?? [],
         skillsOverride: createPromaSkillsOverride(input.additionalSkillPaths),
         agentsFilesOverride: createPromaAgentsFilesOverride(),
-        ...(input.codexFastMode && input.provider === 'openai-codex' && {
-          extensionFactories: [createCodexFastModeExtension()],
+        ...((input.provider === 'openai-codex' || input.provider === 'openai-responses')
+          && model.reasoning
+          && isOpenAIReasoningSupportedModel(input.model) && {
+            extensionFactories: [createCodexRequestSettingsExtension({
+            fastMode: input.codexFastMode,
+            thinkingLevel: input.openAIThinkingLevel,
+          })],
         }),
         systemPromptOverride: () => input.systemPrompt,
       })

apps/electron/src/main/lib/adapters/pi-codex-fast-mode.test.ts
@@ -1,7 +1,21 @@
 import { describe, expect, test } from 'bun:test'
-import { injectCodexFastMode, withCodexFastModeServiceTier } from './pi-codex-fast-mode'
+import {
+  injectCodexFastMode,
+  injectOpenAIThinkingLevel,
+  withCodexFastModeServiceTier,
+} from './pi-codex-request-settings'
+import { isOpenAIReasoningSupportedModel } from '@luxagents/shared'
 
-describe('Pi Codex Fast Mode', () => {
+describe('Pi Codex request settings', () => {
+  test('Given OpenAI model IDs When checking reasoning support Then excludes non-reasoning GPT-4 models', () => {
+    expect(isOpenAIReasoningSupportedModel('gpt-5.6')).toBe(true)
+    expect(isOpenAIReasoningSupportedModel('o4-mini')).toBe(true)
+    expect(isOpenAIReasoningSupportedModel('gpt-4o')).toBe(false)
+    expect(isOpenAIReasoningSupportedModel('gpt-4.1')).toBe(false)
+    expect(isOpenAIReasoningSupportedModel('gpt-5-chat-latest')).toBe(false)
+    expect(isOpenAIReasoningSupportedModel('gpt-5.3-chat-latest')).toBe(false)
+  })
+
   test.each(['gpt-5.4', 'gpt-5.5', 'gpt-5.6-sol', 'gpt-5.6-terra', 'gpt-5.6-luna'])(
     'Given supported %s When injecting Then requests priority tier',
     (model) => {
@@ -25,6 +39,30 @@ describe('Pi Codex Fast Mode', () => {
     expect(withCodexFastModeServiceTier({ transport: 'websocket' })).toEqual({
       transport: 'websocket',
       serviceTier: 'priority',
+    })
+  })
+
+  test('Given thinking is disabled When injecting Then explicitly sends none', () => {
+    expect(injectOpenAIThinkingLevel({ model: 'gpt-5.5' }, { thinkingLevel: 'off' })).toEqual({
+      model: 'gpt-5.5',
+      reasoning: { effort: 'none' },
+    })
+  })
+
+  test('Given direct Codex provider stream When injecting Then fills the selected non-off effort', () => {
+    expect(injectOpenAIThinkingLevel({ model: 'gpt-5.5' }, { thinkingLevel: 'high' })).toEqual({
+      model: 'gpt-5.5',
+      reasoning: { effort: 'high' },
+    })
+  })
+
+  test('Given an upstream reasoning mode When injecting Then strips mode from the request', () => {
+    expect(injectOpenAIThinkingLevel({
+      model: 'gpt-5.6',
+      reasoning: { effort: 'high', mode: 'pro', summary: 'auto' },
+    }, { thinkingLevel: 'high' })).toEqual({
+      model: 'gpt-5.6',
+      reasoning: { effort: 'high', summary: 'auto' },
     })
   })
 

apps/electron/src/main/lib/agent-thinking-level.ts
@@ -1,0 +1,22 @@
+import type { AgentSessionMeta, AgentThinkingLevel, ProviderType } from '@luxagents/shared'
+import type { AppSettings } from '../../types'
+
+type ThinkingSettings = Pick<AppSettings, 'agentThinking' | 'agentEffort'>
+type ThinkingSessionMeta = Pick<AgentSessionMeta, 'openAIThinkingLevel'>
+
+function isOpenAIReasoningProvider(provider: ProviderType | undefined): boolean {
+  return provider === 'openai-codex' || provider === 'openai-responses'
+}
+
+export function resolvePiThinkingLevel(
+  settings: ThinkingSettings,
+  sessionMeta: ThinkingSessionMeta | undefined,
+  provider: ProviderType | undefined,
+): AgentThinkingLevel {
+  if (isOpenAIReasoningProvider(provider) && sessionMeta?.openAIThinkingLevel) {
+    return sessionMeta.openAIThinkingLevel
+  }
+  if (settings.agentThinking?.type === 'disabled') return 'off'
+  if (settings.agentEffort === 'max') return 'xhigh'
+  return settings.agentEffort ?? (settings.agentThinking ? 'high' : 'off')
+}