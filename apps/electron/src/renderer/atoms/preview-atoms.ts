climashscape/Proma

fix(preview): add code wrap toggle (#1142)

apps/electron/src/renderer/atoms/preview-atoms.ts
@@ -53,6 +53,14 @@ export const previewModePreferenceAtom = atomWithStorage<PreviewModePreference>(
   { getOnInit: true },
 )
 
+/** 代码预览换行偏好（默认不换行，保持现有横向滚动行为） */
+export const previewCodeWrapAtom = atomWithStorage<boolean>(
+  'proma-preview-code-wrap',
+  false,
+  undefined,
+  { getOnInit: true },
+)
+
 /** 当前会话的预览面板是否打开（derived） */
 export const currentSessionPreviewOpenAtom = atom<boolean>((get) => {
   const sessionId = get(currentAgentSessionIdAtom)

GeoffBao/LuxCoder

(cherry picked from commit 2a3d9b3776e5a940474fb12d1902a965fb891f04)
Co-authored-by: Cursor <cursoragent@cursor.com>

apps/electron/src/renderer/atoms/preview-atoms.ts
@@ -53,6 +53,14 @@ export const previewModePreferenceAtom = atomWithStorage<PreviewModePreference>(
   { getOnInit: true },
 )
 
+/** 代码预览换行偏好（默认不换行，保持现有横向滚动行为） */
+export const previewCodeWrapAtom = atomWithStorage<boolean>(
+  'luxagents-preview-code-wrap',
+  false,
+  undefined,
+  { getOnInit: true },
+)
+
 /** 当前会话的预览面板是否打开（derived） */
 export const currentSessionPreviewOpenAtom = atom<boolean>((get) => {
   const sessionId = get(currentAgentSessionIdAtom)