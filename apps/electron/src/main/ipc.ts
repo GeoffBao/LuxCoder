Andreaseszhang/Proma

fix(voice): 冻结听写输出上下文并修复路由/取消/回填竞态【已审核 PR】 (#1404)
* fix(voice): 冻结听写输出上下文并修复路由/取消/回填竞态

- 主进程新增 VoiceDictationOutputContextStore，听写开始时冻结输出模式与
  是否路由到 Proma 输入框，录音期间修改设置不会改变输出去向
- 渲染端分离 UI 状态来源（sourceInputId）与文本回填目标（targetInputId），
  剪贴板模式不再向 Proma 输入框写回文本
- 新增 VoiceDictationTextDeliveryTracker 回执协议，目标输入框卸载/失效时
  不再误报「已写入」，改为复制到剪贴板并如实提示
- 修复取消/提交后迟到 transcript/state 事件重新激活会话的竞态
- ASR 重连握手期间停止可立即终止未就绪连接并正确结算 Promise
- 提交已开始后禁止取消，避免文本已插入但结果误报取消
- RichTextInput / ScratchPadView / AskUserBanner 卸载时回收临时听写预览

Made-with: Proma

* fix(voice): preserve edited dictation previews

Made-with: Proma

---------

Co-authored-by: ErlichLiu <erlichgutu@gmail.com>

apps/electron/src/main/ipc.ts
@@ -23,6 +23,8 @@ import type {
   VoiceDictationStartInput,
   VoiceDictationStopInput,
   VoiceDictationTestResult,
+  VoiceDictationTextDeliveryInput,
+  VoiceDictationToggleInput,
   MicPermissionResult,
 } from '../types'
 import type {
@@ -4462,10 +4464,13 @@ export function registerIpcHandlers(): void {
 
   ipcMain.handle(
     VOICE_DICTATION_IPC_CHANNELS.TOGGLE,
-    async (event): Promise<void> => {
+    async (event, input?: VoiceDictationToggleInput): Promise<void> => {
       const { toggleVoiceDictationWindow } = await import('./lib/voice-dictation-window')
       const sourceWindow = BrowserWindow.fromWebContents(event.sender)
-      toggleVoiceDictationWindow({ targetIsProma: !!sourceWindow })
+      const sourceInputId = typeof input?.sourceInputId === 'string' && input.sourceInputId.length > 0 && input.sourceInputId.length <= 512
+        ? input.sourceInputId
+        : undefined
+      toggleVoiceDictationWindow({ targetIsProma: !!sourceWindow, sourceInputId })
     }
   )
 
@@ -4523,7 +4528,11 @@ export function registerIpcHandlers(): void {
     async (_, input: VoiceDictationStopInput): Promise<void> => {
       const { cancelDoubaoAsrSession } = await import('./lib/doubao-asr-service')
       const { clearVoiceDictationPreview } = await import('./lib/text-output-service')
-      clearVoiceDictationPreview(input.previewSessionId ?? input.sessionId)
+      clearVoiceDictationPreview(
+        input.previewSessionId ?? input.sessionId,
+        input.targetInputId,
+        input.outputContextId,
+      )
       cancelDoubaoAsrSession(input.sessionId)
     }
   )
@@ -4536,6 +4545,18 @@ export function registerIpcHandlers(): void {
       previewVoiceDictationText(input, getVoiceDictationSettings())
     }
   )
+
+  ipcMain.on(VOICE_DICTATION_IPC_CHANNELS.ACK_INSERT_TEXT, (event, input: VoiceDictationTextDeliveryInput) => {
+    void Promise.all([
+      import('./index'),
+      import('./lib/text-output-service'),
+    ]).then(([{ getMainWindow }, { acknowledgeVoiceDictationTextDelivery }]) => {
+      const mainWindow = getMainWindow()
+      if (!mainWindow || mainWindow.isDestroyed() || event.sender.id !== mainWindow.webContents.id) return
+      if (!input || typeof input.sessionId !== 'string' || typeof input.delivered !== 'boolean') return
+      acknowledgeVoiceDictationTextDelivery(input.sessionId, input.delivered)
+    }).catch(console.error)
+  })
 
   ipcMain.handle(
     VOICE_DICTATION_IPC_CHANNELS.COMMIT,

apps/electron/src/main/lib/text-output-service.ts
@@ -9,15 +9,22 @@ import { VOICE_DICTATION_IPC_CHANNELS } from '../../types'
 import type {
   VoiceDictationCommitInput,
   VoiceDictationCommitResult,
+  VoiceDictationOutputContext,
+  VoiceDictationPreviewInput,
   VoiceDictationSettings,
   VoiceDictationTextEvent,
 } from '../../types'
 import { getMainWindow } from '../index'
 import { pasteTextAtCurrentCursor } from './text-insertion-service'
+import { VoiceDictationOutputContextStore } from './voice-dictation-output-context'
+import { VoiceDictationTextDeliveryTracker } from './voice-dictation-text-delivery'
 
 let targetWasPromaInput = false
 let activePreviewSessionId: string | null = null
 let closedPreviewSessionId: string | null = null
+const voiceDictationOutputContexts = new VoiceDictationOutputContextStore()
+const voiceDictationTextDeliveries = new VoiceDictationTextDeliveryTracker()
+const TEXT_DELIVERY_ACK_TIMEOUT_MS = 750
 
 /** 在显示语音浮窗前记录目标是否为 Proma 主窗口。 */
 export function captureVoiceDictationTarget(forcePromaInput?: boolean): boolean {
@@ -26,9 +33,35 @@ export function captureVoiceDictationTarget(forcePromaInput?: boolean): boolean 
   return targetWasPromaInput
 }
 
+/** 在主进程创建本次听写的冻结输出上下文。 */
+export function beginVoiceDictationOutputContext(
+  contextId: string,
+  context: VoiceDictationOutputContext,
+): void {
+  voiceDictationOutputContexts.begin(contextId, context)
+}
+
+/** 释放已完成、取消或隐藏的听写会话输出上下文。 */
+export function releaseVoiceDictationOutputContext(contextId?: string): void {
+  voiceDictationOutputContexts.release(contextId)
+}
+
 function shouldWriteToPromaInput(settings: VoiceDictationSettings): boolean {
   return settings.outputMode === 'proma-input' ||
     (settings.outputMode === 'auto' && targetWasPromaInput)
+}
+
+function resolveVoiceDictationOutputContext(
+  outputContextId: string | undefined,
+  settings: VoiceDictationSettings,
+): VoiceDictationOutputContext | undefined {
+  if (outputContextId !== undefined) return voiceDictationOutputContexts.get(outputContextId)
+
+  // 兼容尚未携带输出上下文 ID 的旧渲染进程。
+  return {
+    routeToPromaInput: shouldWriteToPromaInput(settings),
+    outputMode: settings.outputMode,
+  }
 }
 
 function sendTextEvent(channel: string, event: VoiceDictationTextEvent): boolean {
@@ -38,57 +71,102 @@ function sendTextEvent(channel: string, event: VoiceDictationTextEvent): boolean
   return true
 }
 
+function sendTextEventAndAwaitDelivery(event: VoiceDictationTextEvent): Promise<boolean> {
+  const delivery = voiceDictationTextDeliveries.waitFor(event.sessionId, TEXT_DELIVERY_ACK_TIMEOUT_MS)
+  if (!sendTextEvent(VOICE_DICTATION_IPC_CHANNELS.INSERT_TEXT, event)) {
+    voiceDictationTextDeliveries.acknowledge(event.sessionId, false)
+  }
+  return delivery
+}
+
+/** Receives the renderer's confirmation that the frozen input target consumed final text. */
+export function acknowledgeVoiceDictationTextDelivery(sessionId: string, delivered: boolean): void {
+  voiceDictationTextDeliveries.acknowledge(sessionId, delivered)
+}
+
 /**
  * 将 ASR 的最新完整结果预览到 Proma 输入框。
  * 外部应用只在结束时一次性写入，避免连续粘贴打断用户输入。
  */
 export function previewVoiceDictationText(
-  input: VoiceDictationTextEvent,
+  input: VoiceDictationPreviewInput,
   settings: VoiceDictationSettings,
 ): void {
   const text = input.text.trim()
-  if (!text || !shouldWriteToPromaInput(settings)) return
+  const outputContext = resolveVoiceDictationOutputContext(input.outputContextId, settings)
+  if (!text || !outputContext?.routeToPromaInput) return
   if (input.sessionId === closedPreviewSessionId) return
   if (activePreviewSessionId && activePreviewSessionId !== input.sessionId) return
   activePreviewSessionId = input.sessionId
-  sendTextEvent(VOICE_DICTATION_IPC_CHANNELS.PREVIEW_TEXT, { ...input, text })
+  sendTextEvent(VOICE_DICTATION_IPC_CHANNELS.PREVIEW_TEXT, {
+    sessionId: input.sessionId,
+    text,
+    targetInputId: input.targetInputId,
+  })
 }
 
 /** 取消录音时撤销尚未提交到 Proma 输入框的临时组合文本。 */
-export function clearVoiceDictationPreview(sessionId: string): void {
+export function clearVoiceDictationPreview(
+  sessionId: string,
+  targetInputId?: string | null,
+  outputContextId?: string,
+): void {
   if (activePreviewSessionId === sessionId) {
     activePreviewSessionId = null
-    sendTextEvent(VOICE_DICTATION_IPC_CHANNELS.CLEAR_PREVIEW_TEXT, { sessionId, text: '' })
+    sendTextEvent(VOICE_DICTATION_IPC_CHANNELS.CLEAR_PREVIEW_TEXT, { sessionId, text: '', targetInputId })
   }
   closedPreviewSessionId = sessionId
+  acknowledgeVoiceDictationTextDelivery(sessionId, false)
+  releaseVoiceDictationOutputContext(outputContextId)
 }
 
 export async function commitVoiceDictationText(
   input: VoiceDictationCommitInput,
   settings: VoiceDictationSettings,
 ): Promise<VoiceDictationCommitResult> {
-  const trimmed = input.text.trim()
-  if (!trimmed) {
-    return { mode: 'clipboard', success: false, message: '没有可输出的语音文本' }
-  }
+  const outputContext = resolveVoiceDictationOutputContext(input.outputContextId, settings)
 
-  const hasActivePreview = activePreviewSessionId === input.sessionId
-  if ((hasActivePreview || shouldWriteToPromaInput(settings)) && sendTextEvent(VOICE_DICTATION_IPC_CHANNELS.INSERT_TEXT, {
-    sessionId: input.sessionId,
-    text: trimmed,
-  })) {
-    activePreviewSessionId = null
-    closedPreviewSessionId = input.sessionId
-    return { mode: 'proma-input', success: true, message: '已写入 Proma 输入框' }
-  }
+  try {
+    if (!outputContext) {
+      return { mode: 'clipboard', success: false, message: '听写会话已结束，未输出文本' }
+    }
 
-  if (settings.outputMode === 'auto') {
-    const result = await pasteTextAtCurrentCursor(trimmed)
-    return result.success
-      ? { mode: 'cursor', success: true, message: result.message }
-      : { mode: 'clipboard', success: true, message: result.message }
-  }
+    const trimmed = input.text.trim()
+    if (!trimmed) {
+      return { mode: 'clipboard', success: false, message: '没有可输出的语音文本' }
+    }
 
-  clipboard.writeText(trimmed)
-  return { mode: 'clipboard', success: true, message: '已复制到剪贴板' }
+    const hasActivePreview = activePreviewSessionId === input.sessionId
+    const shouldSendToPromaInput = hasActivePreview || outputContext.routeToPromaInput
+    if (shouldSendToPromaInput) {
+      const delivered = await sendTextEventAndAwaitDelivery({
+        sessionId: input.sessionId,
+        text: trimmed,
+        targetInputId: input.targetInputId,
+      })
+      activePreviewSessionId = null
+      closedPreviewSessionId = input.sessionId
+      if (input.outputContextId && !voiceDictationOutputContexts.get(input.outputContextId)) {
+        return { mode: 'clipboard', success: false, message: '听写会话已取消，未输出文本' }
+      }
+      if (delivered) {
+        return { mode: 'proma-input', success: true, message: '已写入 Proma 输入框' }
+      }
+
+      clipboard.writeText(trimmed)
+      return { mode: 'clipboard', success: true, message: 'Proma 输入框未确认接收，已复制到剪贴板' }
+    }
+
+    if (outputContext.outputMode === 'auto') {
+      const result = await pasteTextAtCurrentCursor(trimmed)
+      return result.success
+        ? { mode: 'cursor', success: true, message: result.message }
+        : { mode: 'clipboard', success: true, message: result.message }
+    }
+
+    clipboard.writeText(trimmed)
+    return { mode: 'clipboard', success: true, message: '已复制到剪贴板' }
+  } finally {
+    releaseVoiceDictationOutputContext(input.outputContextId)
+  }
 }

GeoffBao/LuxCoder

apps/electron/src/main/ipc.ts
@@ -23,6 +23,8 @@ import type {
   VoiceDictationStartInput,
   VoiceDictationStopInput,
   VoiceDictationTestResult,
+  VoiceDictationTextDeliveryInput,
+  VoiceDictationToggleInput,
   MicPermissionResult,
 } from '../types'
 import type {
@@ -5066,10 +5068,13 @@ export function registerIpcHandlers(): void {
 
   ipcMain.handle(
     VOICE_DICTATION_IPC_CHANNELS.TOGGLE,
-    async (event): Promise<void> => {
+    async (event, input?: VoiceDictationToggleInput): Promise<void> => {
       const { toggleVoiceDictationWindow } = await import('./lib/voice-dictation-window')
       const sourceWindow = BrowserWindow.fromWebContents(event.sender)
-      toggleVoiceDictationWindow({ targetIsLuxCoder: !!sourceWindow })
+      const sourceInputId = typeof input?.sourceInputId === 'string' && input.sourceInputId.length > 0 && input.sourceInputId.length <= 512
+        ? input.sourceInputId
+        : undefined
+      toggleVoiceDictationWindow({ targetIsLuxCoder: !!sourceWindow, sourceInputId })
     }
   )
 
@@ -5127,7 +5132,11 @@ export function registerIpcHandlers(): void {
     async (_, input: VoiceDictationStopInput): Promise<void> => {
       const { cancelDoubaoAsrSession } = await import('./lib/doubao-asr-service')
       const { clearVoiceDictationPreview } = await import('./lib/text-output-service')
-      clearVoiceDictationPreview(input.previewSessionId ?? input.sessionId)
+      clearVoiceDictationPreview(
+        input.previewSessionId ?? input.sessionId,
+        input.targetInputId,
+        input.outputContextId,
+      )
       cancelDoubaoAsrSession(input.sessionId)
     }
   )
@@ -5140,6 +5149,18 @@ export function registerIpcHandlers(): void {
       previewVoiceDictationText(input, getVoiceDictationSettings())
     }
   )
+
+  ipcMain.on(VOICE_DICTATION_IPC_CHANNELS.ACK_INSERT_TEXT, (event, input: VoiceDictationTextDeliveryInput) => {
+    void Promise.all([
+      import('./index'),
+      import('./lib/text-output-service'),
+    ]).then(([{ getMainWindow }, { acknowledgeVoiceDictationTextDelivery }]) => {
+      const mainWindow = getMainWindow()
+      if (!mainWindow || mainWindow.isDestroyed() || event.sender.id !== mainWindow.webContents.id) return
+      if (!input || typeof input.sessionId !== 'string' || typeof input.delivered !== 'boolean') return
+      acknowledgeVoiceDictationTextDelivery(input.sessionId, input.delivered)
+    }).catch(console.error)
+  })
 
   ipcMain.handle(
     VOICE_DICTATION_IPC_CHANNELS.COMMIT,

apps/electron/src/main/lib/text-output-service.ts
@@ -9,15 +9,22 @@ import { VOICE_DICTATION_IPC_CHANNELS } from '../../types'
 import type {
   VoiceDictationCommitInput,
   VoiceDictationCommitResult,
+  VoiceDictationOutputContext,
+  VoiceDictationPreviewInput,
   VoiceDictationSettings,
   VoiceDictationTextEvent,
 } from '../../types'
 import { getMainWindow } from '../index'
 import { pasteTextAtCurrentCursor } from './text-insertion-service'
+import { VoiceDictationOutputContextStore } from './voice-dictation-output-context'
+import { VoiceDictationTextDeliveryTracker } from './voice-dictation-text-delivery'
 
 let targetWasLuxCoderInput = false
 let activePreviewSessionId: string | null = null
 let closedPreviewSessionId: string | null = null
+const voiceDictationOutputContexts = new VoiceDictationOutputContextStore()
+const voiceDictationTextDeliveries = new VoiceDictationTextDeliveryTracker()
+const TEXT_DELIVERY_ACK_TIMEOUT_MS = 750
 
 /** 在显示语音浮窗前记录目标是否为 LuxCoder 主窗口。 */
 export function captureVoiceDictationTarget(forceLuxCoderInput?: boolean): boolean {
@@ -26,9 +33,35 @@ export function captureVoiceDictationTarget(forceLuxCoderInput?: boolean): boole
   return targetWasLuxCoderInput
 }
 
+/** 在主进程创建本次听写的冻结输出上下文。 */
+export function beginVoiceDictationOutputContext(
+  contextId: string,
+  context: VoiceDictationOutputContext,
+): void {
+  voiceDictationOutputContexts.begin(contextId, context)
+}
+
+/** 释放已完成、取消或隐藏的听写会话输出上下文。 */
+export function releaseVoiceDictationOutputContext(contextId?: string): void {
+  voiceDictationOutputContexts.release(contextId)
+}
+
 function shouldWriteToLuxCoderInput(settings: VoiceDictationSettings): boolean {
   return settings.outputMode === 'luxcoder-input' ||
     (settings.outputMode === 'auto' && targetWasLuxCoderInput)
+}
+
+function resolveVoiceDictationOutputContext(
+  outputContextId: string | undefined,
+  settings: VoiceDictationSettings,
+): VoiceDictationOutputContext | undefined {
+  if (outputContextId !== undefined) return voiceDictationOutputContexts.get(outputContextId)
+
+  // 兼容尚未携带输出上下文 ID 的旧渲染进程。
+  return {
+    routeToLuxCoderInput: shouldWriteToLuxCoderInput(settings),
+    outputMode: settings.outputMode,
+  }
 }
 
 function sendTextEvent(channel: string, event: VoiceDictationTextEvent): boolean {
@@ -38,57 +71,102 @@ function sendTextEvent(channel: string, event: VoiceDictationTextEvent): boolean
   return true
 }
 
+function sendTextEventAndAwaitDelivery(event: VoiceDictationTextEvent): Promise<boolean> {
+  const delivery = voiceDictationTextDeliveries.waitFor(event.sessionId, TEXT_DELIVERY_ACK_TIMEOUT_MS)
+  if (!sendTextEvent(VOICE_DICTATION_IPC_CHANNELS.INSERT_TEXT, event)) {
+    voiceDictationTextDeliveries.acknowledge(event.sessionId, false)
+  }
+  return delivery
+}
+
+/** Receives the renderer's confirmation that the frozen input target consumed final text. */
+export function acknowledgeVoiceDictationTextDelivery(sessionId: string, delivered: boolean): void {
+  voiceDictationTextDeliveries.acknowledge(sessionId, delivered)
+}
+
 /**
  * 将 ASR 的最新完整结果预览到 LuxCoder 输入框。
  * 外部应用只在结束时一次性写入，避免连续粘贴打断用户输入。
  */
 export function previewVoiceDictationText(
-  input: VoiceDictationTextEvent,
+  input: VoiceDictationPreviewInput,
   settings: VoiceDictationSettings,
 ): void {
   const text = input.text.trim()
-  if (!text || !shouldWriteToLuxCoderInput(settings)) return
+  const outputContext = resolveVoiceDictationOutputContext(input.outputContextId, settings)
+  if (!text || !outputContext?.routeToLuxCoderInput) return
   if (input.sessionId === closedPreviewSessionId) return
   if (activePreviewSessionId && activePreviewSessionId !== input.sessionId) return
   activePreviewSessionId = input.sessionId
-  sendTextEvent(VOICE_DICTATION_IPC_CHANNELS.PREVIEW_TEXT, { ...input, text })
+  sendTextEvent(VOICE_DICTATION_IPC_CHANNELS.PREVIEW_TEXT, {
+    sessionId: input.sessionId,
+    text,
+    targetInputId: input.targetInputId,
+  })
 }
 
 /** 取消录音时撤销尚未提交到 LuxCoder 输入框的临时组合文本。 */
-export function clearVoiceDictationPreview(sessionId: string): void {
+export function clearVoiceDictationPreview(
+  sessionId: string,
+  targetInputId?: string | null,
+  outputContextId?: string,
+): void {
   if (activePreviewSessionId === sessionId) {
     activePreviewSessionId = null
-    sendTextEvent(VOICE_DICTATION_IPC_CHANNELS.CLEAR_PREVIEW_TEXT, { sessionId, text: '' })
+    sendTextEvent(VOICE_DICTATION_IPC_CHANNELS.CLEAR_PREVIEW_TEXT, { sessionId, text: '', targetInputId })
   }
   closedPreviewSessionId = sessionId
+  acknowledgeVoiceDictationTextDelivery(sessionId, false)
+  releaseVoiceDictationOutputContext(outputContextId)
 }
 
 export async function commitVoiceDictationText(
   input: VoiceDictationCommitInput,
   settings: VoiceDictationSettings,
 ): Promise<VoiceDictationCommitResult> {
-  const trimmed = input.text.trim()
-  if (!trimmed) {
-    return { mode: 'clipboard', success: false, message: '没有可输出的语音文本' }
-  }
+  const outputContext = resolveVoiceDictationOutputContext(input.outputContextId, settings)
 
-  const hasActivePreview = activePreviewSessionId === input.sessionId
-  if ((hasActivePreview || shouldWriteToLuxCoderInput(settings)) && sendTextEvent(VOICE_DICTATION_IPC_CHANNELS.INSERT_TEXT, {
-    sessionId: input.sessionId,
-    text: trimmed,
-  })) {
-    activePreviewSessionId = null
-    closedPreviewSessionId = input.sessionId
-    return { mode: 'luxcoder-input', success: true, message: '已写入 LuxCoder 输入框' }
-  }
+  try {
+    if (!outputContext) {
+      return { mode: 'clipboard', success: false, message: '听写会话已结束，未输出文本' }
+    }
 
-  if (settings.outputMode === 'auto') {
-    const result = await pasteTextAtCurrentCursor(trimmed)
-    return result.success
-      ? { mode: 'cursor', success: true, message: result.message }
-      : { mode: 'clipboard', success: true, message: result.message }
-  }
+    const trimmed = input.text.trim()
+    if (!trimmed) {
+      return { mode: 'clipboard', success: false, message: '没有可输出的语音文本' }
+    }
 
-  clipboard.writeText(trimmed)
-  return { mode: 'clipboard', success: true, message: '已复制到剪贴板' }
+    const hasActivePreview = activePreviewSessionId === input.sessionId
+    const shouldSendToLuxCoderInput = hasActivePreview || outputContext.routeToLuxCoderInput
+    if (shouldSendToLuxCoderInput) {
+      const delivered = await sendTextEventAndAwaitDelivery({
+        sessionId: input.sessionId,
+        text: trimmed,
+        targetInputId: input.targetInputId,
+      })
+      activePreviewSessionId = null
+      closedPreviewSessionId = input.sessionId
+      if (input.outputContextId && !voiceDictationOutputContexts.get(input.outputContextId)) {
+        return { mode: 'clipboard', success: false, message: '听写会话已取消，未输出文本' }
+      }
+      if (delivered) {
+        return { mode: 'luxcoder-input', success: true, message: '已写入 LuxCoder 输入框' }
+      }
+
+      clipboard.writeText(trimmed)
+      return { mode: 'clipboard', success: true, message: 'LuxCoder 输入框未确认接收，已复制到剪贴板' }
+    }
+
+    if (outputContext.outputMode === 'auto') {
+      const result = await pasteTextAtCurrentCursor(trimmed)
+      return result.success
+        ? { mode: 'cursor', success: true, message: result.message }
+        : { mode: 'clipboard', success: true, message: result.message }
+    }
+
+    clipboard.writeText(trimmed)
+    return { mode: 'clipboard', success: true, message: '已复制到剪贴板' }
+  } finally {
+    releaseVoiceDictationOutputContext(input.outputContextId)
+  }
 }