Andreaseszhang/Proma

feat: sync planning items to macOS EventKit (#1449)
* feat: sync planning items to macOS EventKit

Made-with: Proma

* fix: refine EventKit sync experience

Made-with: Proma

* chore: remove EventKit test scaffolding

Made-with: Proma

* fix: make EventKit target changes recoverable

Made-with: Proma

* fix: preserve EventKit cleanup recovery

Made-with: Proma

apps/electron/scripts/build-eventkit-native.ts
@@ -1,0 +1,44 @@
+#!/usr/bin/env bun
+/** Build the in-process macOS EventKit N-API addon for Electron main. */
+
+import { execFileSync } from 'node:child_process'
+import { chmodSync, existsSync, mkdirSync, rmSync } from 'node:fs'
+import { dirname, resolve } from 'node:path'
+import { homedir } from 'node:os'
+import { fileURLToPath } from 'node:url'
+
+const scriptDir = dirname(fileURLToPath(import.meta.url))
+const appDir = resolve(scriptDir, '..')
+const source = resolve(appDir, 'native/eventkit/macos-eventkit-addon.mm')
+const output = resolve(appDir, 'resources/eventkit/macos-eventkit.node')
+const napiHeaders = resolve(appDir, '../../node_modules/node-addon-api')
+// Electron uses stable Node-API; use the locally installed Node headers only for declarations.
+const nodeVersion = execFileSync('node', ['-p', 'process.versions.node'], { encoding: 'utf8' }).trim()
+const nodeApiHeaders = resolve(homedir(), 'Library/Caches/node-gyp', nodeVersion, 'include/node')
+const devElectronApp = resolve(appDir, '../../node_modules/electron/dist/Electron.app')
+const devElectronInfo = resolve(devElectronApp, 'Contents/Info.plist')
+const entitlements = resolve(appDir, 'resources/entitlements.mac.plist')
+
+if (process.platform !== 'darwin') {
+  console.log('[eventkit-native] skipped (macOS only)')
+  process.exit(0)
+}
+if (!existsSync(source)) throw new Error(`EventKit addon source not found: ${source}`)
+// CI 不会预热 ~/.cache/node-gyp；按当前 Node 版本拉取一次 header，随后复用缓存。
+if (!existsSync(resolve(nodeApiHeaders, 'node_api.h'))) execFileSync('npx', ['node-gyp', 'install', nodeVersion], { stdio: 'inherit' })
+if (!existsSync(resolve(napiHeaders, 'napi.h')) || !existsSync(resolve(nodeApiHeaders, 'node_api.h'))) throw new Error(`Node-API headers not found for Node ${nodeVersion}`)
+mkdirSync(dirname(output), { recursive: true })
+rmSync(output, { force: true })
+rmSync(resolve(appDir, 'resources/eventkit/macos-eventkit-helper'), { force: true })
+execFileSync('xcrun', ['clang++', '-O2', '-std=c++17', '-DNAPI_VERSION=8', '-fobjc-arc', '-bundle', '-undefined', 'dynamic_lookup', '-framework', 'EventKit', '-framework', 'Foundation', '-I', napiHeaders, '-I', nodeApiHeaders, source, '-o', output], { stdio: 'inherit' })
+chmodSync(output, 0o755)
+
+// 开发态的 responsible process 是 Electron.app，不是最终 Proma.app。为它注入相同 usage strings
+// 并重新 ad-hoc 签名，避免 bun run dev 下 TCC 因缺少 Info.plist 键而静默拒绝/不显示弹窗。
+if (existsSync(devElectronInfo)) {
+  execFileSync('plutil', ['-replace', 'NSCalendarsFullAccessUsageDescription', '-string', 'Proma 需要访问你选择的日历，以显示、创建和同步日程。', devElectronInfo])
+  execFileSync('plutil', ['-replace', 'NSRemindersFullAccessUsageDescription', '-string', 'Proma 需要访问你选择的提醒事项列表，以同步 Todo。', devElectronInfo])
+  // macOS 26+ 还会检查 EventKit entitlement；开发态 Electron 也必须带与成品相同的权限。
+  execFileSync('codesign', ['--force', '--deep', '--sign', '-', '--entitlements', entitlements, devElectronApp], { stdio: 'inherit' })
+}
+console.log(`[eventkit-native] built ${output}`)

apps/electron/src/main/lib/planning-native-sync-service.ts
@@ -1,0 +1,119 @@
+import type {
+  PlanningNativeSyncEntity,
+  PlanningNativeSyncPermission,
+  PlanningNativeSyncStatus,
+  PlanningNativeSyncTarget,
+} from '@proma/shared'
+import { callMacEventKitNativeAddon } from './mac-eventkit-native-addon'
+
+type NativePermissionResponse = {
+  entity: PlanningNativeSyncEntity
+  status: PlanningNativeSyncPermission
+  granted?: boolean
+  error?: string
+}
+
+function eventKitSupported(): boolean {
+  if (process.platform !== 'darwin') return false
+  const major = Number.parseInt(process.getSystemVersion().split('.')[0] ?? '', 10)
+  return Number.isFinite(major) && major >= 14
+}
+
+function unsupportedPermission(entity: PlanningNativeSyncEntity): NativePermissionResponse {
+  return { entity, status: 'unsupported' }
+}
+
+async function getPermission(entity: PlanningNativeSyncEntity): Promise<NativePermissionResponse> {
+  if (!eventKitSupported()) return unsupportedPermission(entity)
+  try {
+    return await callMacEventKitNativeAddon<NativePermissionResponse>('authorizationStatus', entity)
+  } catch (error) {
+    return {
+      entity,
+      status: 'unavailable',
+      error: error instanceof Error ? error.message : String(error),
+    }
+  }
+}
+
+/** 供设置页展示；不可用时不抛出，避免非 macOS 用户看到 IPC 错误。 */
+export async function getPlanningNativeSyncStatus(): Promise<PlanningNativeSyncStatus> {
+  if (!eventKitSupported()) return { supported: false, calendar: unsupportedPermission('calendar'), reminder: unsupportedPermission('reminder') }
+  const [calendar, reminder] = await Promise.all([getPermission('calendar'), getPermission('reminder')])
+  return { supported: true, calendar, reminder }
+}
+
+async function withTimeout<T>(promise: Promise<T>, milliseconds: number, message: string): Promise<T> {
+  let timer: ReturnType<typeof setTimeout> | undefined
+  try {
+    return await Promise.race([
+      promise,
+      new Promise<T>((_, reject) => { timer = setTimeout(() => reject(new Error(message)), milliseconds) }),
+    ])
+  } finally {
+    if (timer) clearTimeout(timer)
+  }
+}
+
+export async function requestPlanningNativeSyncAccess(entity: PlanningNativeSyncEntity): Promise<NativePermissionResponse> {
+  if (!eventKitSupported()) return unsupportedPermission(entity)
+  try {
+    // EventKit/TCC 在少数 macOS 版本中会既不展示 sheet 也不调用 completion；绝不能让 UI 无限 loading。
+    return await withTimeout(
+      callMacEventKitNativeAddon<NativePermissionResponse>('requestAccess', entity),
+      12_000,
+      '系统授权请求未响应。请在系统设置中手动授予完整访问权限。',
+    )
+  } catch (error) {
+    return {
+      entity,
+      status: 'unavailable',
+      error: error instanceof Error ? error.message : String(error),
+    }
+  }
+}
+
+/** 仅在已完整授权时返回可写目标；此处不自动触发系统授权弹窗。 */
+export async function listPlanningNativeSyncTargets(entity: PlanningNativeSyncEntity): Promise<PlanningNativeSyncTarget[]> {
+  if (!eventKitSupported()) return []
+  const permission = await getPermission(entity)
+  if (permission.status !== 'full-access') return []
+  try {
+    return await callMacEventKitNativeAddon<PlanningNativeSyncTarget[]>('listWritableTargets', entity)
+  } catch {
+    return []
+  }
+}
+
+export interface PlanningNativeSyncItem {
+  targetId: string
+  /** Proma UUID；仅用于新建项目的 crash-recovery marker，不覆盖用户已有 URL。 */
+  identity: string
+  calendarItemIdentifier?: string
+  title: string
+  notes?: string
+  startAt?: number
+  endAt?: number
+  allDay?: boolean
+  dueAt?: number
+  dueDateOnly?: boolean
+  priority?: 'low' | 'medium' | 'high'
+  completed?: boolean
+  completedAt?: number
+}
+
+export interface PlanningNativeSyncIdentifiers {
+  calendarItemIdentifier?: string
+  calendarItemExternalIdentifier?: string
+}
+
+export async function upsertPlanningNativeSyncItem(entity: PlanningNativeSyncEntity, item: PlanningNativeSyncItem): Promise<PlanningNativeSyncIdentifiers> {
+  if (!eventKitSupported()) throw new Error('macOS 14 或更高版本才支持系统日历与提醒事项同步')
+  return callMacEventKitNativeAddon<PlanningNativeSyncIdentifiers>('upsert', entity, item)
+}
+
+/** 删除必须带受管目标和 Proma marker；locator 缺失时 native addon 可恢复定位，避免崩溃留下孤儿项。 */
+export async function removePlanningNativeSyncItem(entity: PlanningNativeSyncEntity, item: Pick<PlanningNativeSyncItem, 'targetId' | 'identity' | 'calendarItemIdentifier' | 'startAt'>): Promise<void> {
+  if (!eventKitSupported()) return
+  await callMacEventKitNativeAddon<Record<string, never>>('remove', entity, item)
+}

GeoffBao/LuxCoder

(cherry picked from commit 293a86db1e75b8f7fa5ce83cdceb85da9898cf79)

apps/electron/scripts/build-eventkit-native.ts
@@ -1,0 +1,44 @@
+#!/usr/bin/env bun
+/** Build the in-process macOS EventKit N-API addon for Electron main. */
+
+import { execFileSync } from 'node:child_process'
+import { chmodSync, existsSync, mkdirSync, rmSync } from 'node:fs'
+import { dirname, resolve } from 'node:path'
+import { homedir } from 'node:os'
+import { fileURLToPath } from 'node:url'
+
+const scriptDir = dirname(fileURLToPath(import.meta.url))
+const appDir = resolve(scriptDir, '..')
+const source = resolve(appDir, 'native/eventkit/macos-eventkit-addon.mm')
+const output = resolve(appDir, 'resources/eventkit/macos-eventkit.node')
+const napiHeaders = resolve(appDir, '../../node_modules/node-addon-api')
+// Electron uses stable Node-API; use the locally installed Node headers only for declarations.
+const nodeVersion = execFileSync('node', ['-p', 'process.versions.node'], { encoding: 'utf8' }).trim()
+const nodeApiHeaders = resolve(homedir(), 'Library/Caches/node-gyp', nodeVersion, 'include/node')
+const devElectronApp = resolve(appDir, '../../node_modules/electron/dist/Electron.app')
+const devElectronInfo = resolve(devElectronApp, 'Contents/Info.plist')
+const entitlements = resolve(appDir, 'resources/entitlements.mac.plist')
+
+if (process.platform !== 'darwin') {
+  console.log('[eventkit-native] skipped (macOS only)')
+  process.exit(0)
+}
+if (!existsSync(source)) throw new Error(`EventKit addon source not found: ${source}`)
+// CI 不会预热 ~/.cache/node-gyp；按当前 Node 版本拉取一次 header，随后复用缓存。
+if (!existsSync(resolve(nodeApiHeaders, 'node_api.h'))) execFileSync('npx', ['node-gyp', 'install', nodeVersion], { stdio: 'inherit' })
+if (!existsSync(resolve(napiHeaders, 'napi.h')) || !existsSync(resolve(nodeApiHeaders, 'node_api.h'))) throw new Error(`Node-API headers not found for Node ${nodeVersion}`)
+mkdirSync(dirname(output), { recursive: true })
+rmSync(output, { force: true })
+rmSync(resolve(appDir, 'resources/eventkit/macos-eventkit-helper'), { force: true })
+execFileSync('xcrun', ['clang++', '-O2', '-std=c++17', '-DNAPI_VERSION=8', '-fobjc-arc', '-bundle', '-undefined', 'dynamic_lookup', '-framework', 'EventKit', '-framework', 'Foundation', '-I', napiHeaders, '-I', nodeApiHeaders, source, '-o', output], { stdio: 'inherit' })
+chmodSync(output, 0o755)
+
+// 开发态的 responsible process 是 Electron.app，不是最终 LuxCoder.app。为它注入相同 usage strings
+// 并重新 ad-hoc 签名，避免 bun run dev 下 TCC 因缺少 Info.plist 键而静默拒绝/不显示弹窗。
+if (existsSync(devElectronInfo)) {
+  execFileSync('plutil', ['-replace', 'NSCalendarsFullAccessUsageDescription', '-string', 'LuxCoder 需要访问你选择的日历，以显示、创建和同步日程。', devElectronInfo])
+  execFileSync('plutil', ['-replace', 'NSRemindersFullAccessUsageDescription', '-string', 'LuxCoder 需要访问你选择的提醒事项列表，以同步 Todo。', devElectronInfo])
+  // macOS 26+ 还会检查 EventKit entitlement；开发态 Electron 也必须带与成品相同的权限。
+  execFileSync('codesign', ['--force', '--deep', '--sign', '-', '--entitlements', entitlements, devElectronApp], { stdio: 'inherit' })
+}
+console.log(`[eventkit-native] built ${output}`)

apps/electron/src/main/lib/planning-native-sync-service.ts
@@ -1,0 +1,119 @@
+import type {
+  PlanningNativeSyncEntity,
+  PlanningNativeSyncPermission,
+  PlanningNativeSyncStatus,
+  PlanningNativeSyncTarget,
+} from '@luxcoder/shared'
+import { callMacEventKitNativeAddon } from './mac-eventkit-native-addon'
+
+type NativePermissionResponse = {
+  entity: PlanningNativeSyncEntity
+  status: PlanningNativeSyncPermission
+  granted?: boolean
+  error?: string
+}
+
+function eventKitSupported(): boolean {
+  if (process.platform !== 'darwin') return false
+  const major = Number.parseInt(process.getSystemVersion().split('.')[0] ?? '', 10)
+  return Number.isFinite(major) && major >= 14
+}
+
+function unsupportedPermission(entity: PlanningNativeSyncEntity): NativePermissionResponse {
+  return { entity, status: 'unsupported' }
+}
+
+async function getPermission(entity: PlanningNativeSyncEntity): Promise<NativePermissionResponse> {
+  if (!eventKitSupported()) return unsupportedPermission(entity)
+  try {
+    return await callMacEventKitNativeAddon<NativePermissionResponse>('authorizationStatus', entity)
+  } catch (error) {
+    return {
+      entity,
+      status: 'unavailable',
+      error: error instanceof Error ? error.message : String(error),
+    }
+  }
+}
+
+/** 供设置页展示；不可用时不抛出，避免非 macOS 用户看到 IPC 错误。 */
+export async function getPlanningNativeSyncStatus(): Promise<PlanningNativeSyncStatus> {
+  if (!eventKitSupported()) return { supported: false, calendar: unsupportedPermission('calendar'), reminder: unsupportedPermission('reminder') }
+  const [calendar, reminder] = await Promise.all([getPermission('calendar'), getPermission('reminder')])
+  return { supported: true, calendar, reminder }
+}
+
+async function withTimeout<T>(promise: Promise<T>, milliseconds: number, message: string): Promise<T> {
+  let timer: ReturnType<typeof setTimeout> | undefined
+  try {
+    return await Promise.race([
+      promise,
+      new Promise<T>((_, reject) => { timer = setTimeout(() => reject(new Error(message)), milliseconds) }),
+    ])
+  } finally {
+    if (timer) clearTimeout(timer)
+  }
+}
+
+export async function requestPlanningNativeSyncAccess(entity: PlanningNativeSyncEntity): Promise<NativePermissionResponse> {
+  if (!eventKitSupported()) return unsupportedPermission(entity)
+  try {
+    // EventKit/TCC 在少数 macOS 版本中会既不展示 sheet 也不调用 completion；绝不能让 UI 无限 loading。
+    return await withTimeout(
+      callMacEventKitNativeAddon<NativePermissionResponse>('requestAccess', entity),
+      12_000,
+      '系统授权请求未响应。请在系统设置中手动授予完整访问权限。',
+    )
+  } catch (error) {
+    return {
+      entity,
+      status: 'unavailable',
+      error: error instanceof Error ? error.message : String(error),
+    }
+  }
+}
+
+/** 仅在已完整授权时返回可写目标；此处不自动触发系统授权弹窗。 */
+export async function listPlanningNativeSyncTargets(entity: PlanningNativeSyncEntity): Promise<PlanningNativeSyncTarget[]> {
+  if (!eventKitSupported()) return []
+  const permission = await getPermission(entity)
+  if (permission.status !== 'full-access') return []
+  try {
+    return await callMacEventKitNativeAddon<PlanningNativeSyncTarget[]>('listWritableTargets', entity)
+  } catch {
+    return []
+  }
+}
+
+export interface PlanningNativeSyncItem {
+  targetId: string
+  /** LuxCoder UUID；仅用于新建项目的 crash-recovery marker，不覆盖用户已有 URL。 */
+  identity: string
+  calendarItemIdentifier?: string
+  title: string
+  notes?: string
+  startAt?: number
+  endAt?: number
+  allDay?: boolean
+  dueAt?: number
+  dueDateOnly?: boolean
+  priority?: 'low' | 'medium' | 'high'
+  completed?: boolean
+  completedAt?: number
+}
+
+export interface PlanningNativeSyncIdentifiers {
+  calendarItemIdentifier?: string
+  calendarItemExternalIdentifier?: string
+}
+
+export async function upsertPlanningNativeSyncItem(entity: PlanningNativeSyncEntity, item: PlanningNativeSyncItem): Promise<PlanningNativeSyncIdentifiers> {
+  if (!eventKitSupported()) throw new Error('macOS 14 或更高版本才支持系统日历与提醒事项同步')
+  return callMacEventKitNativeAddon<PlanningNativeSyncIdentifiers>('upsert', entity, item)
+}
+
+/** 删除必须带受管目标和 LuxCoder marker；locator 缺失时 native addon 可恢复定位，避免崩溃留下孤儿项。 */
+export async function removePlanningNativeSyncItem(entity: PlanningNativeSyncEntity, item: Pick<PlanningNativeSyncItem, 'targetId' | 'identity' | 'calendarItemIdentifier' | 'startAt'>): Promise<void> {
+  if (!eventKitSupported()) return
+  await callMacEventKitNativeAddon<Record<string, never>>('remove', entity, item)
+}